76968a9304f7c5b2f3fe144f25439754d7781b3e
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-08 20:41:19 +0530

    Use better sieve

--- snippets/sieve.py
+++ snippets/sieve.py
@@ -1,32 +1,9 @@
 from math import sqrt
+from itertools import compress
 
-def primes_less_than(n):
-    if n < 4:
-        return range(2, n + 1)
-    
-    sieve = [False] * (n + 1)
-    sieve[2:4] = (True, True)
-
-    sqrt_n = int(sqrt(n))
-
-    for x in range(1, sqrt_n):
-        for y in range(1, sqrt_n):
-            x2 = x * x
-            y2 = y * y
-
-            i = 4 * x2 + y2
-            if (i <= n) and (i % 12 in (1, 5)): sieve[i] = not sieve[i]
-
-            i = 3 * x2 + y2
-            if (i <= n) and (i % 12 == 7): sieve[i] = not sieve[i]
-
-            i = 3 * x2 - y2
-            if (x > y) and (i <= n) and (i % 12 == 11): sieve[i] = not sieve[i]
-
-
-    for p in range(5, sqrt_n):
-        if sieve[p]:
-            for i in range(p * p, n + 1, p * p):
-                sieve[i] = False
-
-    return (i for i, p in enumerate(sieve) if p)
+def get_primes(n):
+    sieve = bytearray([True]) * (n//2 + 1)
+    for i in range(1, int(sqrt(n))//2 + 1):
+        if sieve[i]:
+            sieve[2*i*(i+1)::2*i + 1] = bytearray((n//2 - 2*i*(i+1))//(2*i + 1) + 1)
+    return compress(range(3, n + 1, 2), sieve[1:])
