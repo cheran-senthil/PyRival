186b51fec895bb2393d91b49fd1f024f329a7d69
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-09 00:03:36 +0530

    Linting

--- snippets/factors.py
+++ snippets/factors.py
@@ -1,7 +1,8 @@
-from random import randint
 from math import gcd
+from random import randint
 
 _known_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+
 
 def _try_composite(a, d, n, s):
     if pow(a, d, n) == 1:
@@ -9,8 +10,9 @@ def _try_composite(a, d, n, s):
     for i in range(s):
         if pow(a, 2**i * d, n) == n-1:
             return False
-    return True # n  is definitely composite
- 
+    return True  # n  is definitely composite
+
+
 def is_prime(n, _precision_for_huge_n=16):
     if n in _known_primes or n in (0, 1):
         return True
@@ -20,35 +22,36 @@ def is_prime(n, _precision_for_huge_n=16):
     while not d % 2:
         d, s = d >> 1, s + 1
     # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
-    if n < 1373653: 
+    if n < 1373653:
         return not any(_try_composite(a, d, n, s) for a in (2, 3))
-    if n < 25326001: 
+    if n < 25326001:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
-    if n < 118670087467: 
-        if n == 3215031751: 
+    if n < 118670087467:
+        if n == 3215031751:
             return False
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
-    if n < 2152302898747: 
+    if n < 2152302898747:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
-    if n < 3474749660383: 
+    if n < 3474749660383:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
-    if n < 341550071728321: 
+    if n < 341550071728321:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
     if n < 3825123056546413051:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17, 19, 23))
     # otherwise
-    return not any(_try_composite(a, d, n, s) 
+    return not any(_try_composite(a, d, n, s)
                    for a in _known_primes[:_precision_for_huge_n])
+
 
 def factor(N):
     for i in _known_primes:
         if N % i == 0:
             return i
-    
+
     y, c, m = randint(1, N-1), randint(1, N-1), randint(1, N-1)
     g, r, q = 1, 1, 1
-    
-    while g == 1:             
+
+    while g == 1:
         x = y
         for i in range(r):
             y = ((y * y) % N + c) % N
@@ -61,7 +64,7 @@ def factor(N):
             g = gcd(q, N)
             k = k + m
         r = r * 2
-    
+
     if g == N:
         while True:
             ys = ((ys * ys) % N + c) % N
@@ -70,6 +73,7 @@ def factor(N):
                 break
 
     return g
+
 
 def factors(N):
     if is_prime(N):
@@ -84,5 +88,3 @@ def factors(N):
                 yield i
             for i in factors(N//f):
                 yield i
-
-    
