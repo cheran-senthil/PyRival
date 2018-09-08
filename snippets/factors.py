3ce671dad2522e38ac91516e4c9e95bc3b447845
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-08 23:29:35 +0530

    Improve factorization

---
+++ snippets/factors.py
@@ -0,0 +1,88 @@
+from random import randint
+from math import gcd
+
+_known_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+
+def _try_composite(a, d, n, s):
+    if pow(a, d, n) == 1:
+        return False
+    for i in range(s):
+        if pow(a, 2**i * d, n) == n-1:
+            return False
+    return True # n  is definitely composite
+ 
+def is_prime(n, _precision_for_huge_n=16):
+    if n in _known_primes or n in (0, 1):
+        return True
+    if any((n % p) == 0 for p in _known_primes):
+        return False
+    d, s = n - 1, 0
+    while not d % 2:
+        d, s = d >> 1, s + 1
+    # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
+    if n < 1373653: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3))
+    if n < 25326001: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
+    if n < 118670087467: 
+        if n == 3215031751: 
+            return False
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
+    if n < 2152302898747: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
+    if n < 3474749660383: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
+    if n < 341550071728321: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
+    if n < 3825123056546413051:
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17, 19, 23))
+    # otherwise
+    return not any(_try_composite(a, d, n, s) 
+                   for a in _known_primes[:_precision_for_huge_n])
+
+def factor(N):
+    for i in _known_primes:
+        if N % i == 0:
+            return i
+    
+    y, c, m = randint(1, N-1), randint(1, N-1), randint(1, N-1)
+    g, r, q = 1, 1, 1
+    
+    while g == 1:             
+        x = y
+        for i in range(r):
+            y = ((y * y) % N + c) % N
+        k = 0
+        while (k < r) and (g == 1):
+            ys = y
+            for i in range(min(m, r - k)):
+                y = ((y * y) % N + c) % N
+                q = q * (abs(x - y)) % N
+            g = gcd(q, N)
+            k = k + m
+        r = r * 2
+    
+    if g == N:
+        while True:
+            ys = ((ys * ys) % N + c) % N
+            g = gcd(abs(x - ys), N)
+            if g > 1:
+                break
+
+    return g
+
+def factors(N):
+    if is_prime(N):
+        yield N
+    else:
+        f = factor(N)
+        if f == N:
+            for i in factors(N):
+                yield i
+        else:
+            for i in factors(f):
+                yield i
+            for i in factors(N//f):
+                yield i
+
+    
