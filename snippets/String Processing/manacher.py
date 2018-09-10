44d1d657535d767df9fd133993248a17e86a3a31
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-11 05:08:38 +0530

    Add manacher

---
+++ snippets/String Processing/manacher.py
@@ -0,0 +1,18 @@
+def longestPalindrome(s):
+    T = '#'.join('^{}$'.format(s))
+    n = len(T)
+    P = [0] * n
+    C = R = 0
+    for i in range(1, n-1):
+        P[i] = (R > i) and min(R - i, P[2*C - i]) # equals to i' = C - (i-C)
+        # Attempt to expand palindrome centered at i
+        while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
+            P[i] += 1
+
+        # If palindrome centered at i expand past R, adjust center based on expanded palindrome.
+        if i + P[i] > R:
+            C, R = i, i + P[i]
+
+    # Find the maximum element in P.
+    maxLen, centerIndex = max((n, i) for i, n in enumerate(P))
+    return s[(centerIndex  - maxLen)//2: (centerIndex  + maxLen)//2]
