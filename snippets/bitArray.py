6d5045b76657cb79d330b201818fa0b3020b1744
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-10 19:12:12 +0530

    Optimize code

--- snippets/bitArray.py
+++ snippets/bitArray.py
@@ -1,12 +1,14 @@
+from operator import rshift
+
 class bitArray:
     def __init__(self, size):
-        self.bytes = bytearray((size >> 3) + 1)
+        self.bytes = bytearray(rshift(size, 3) + 1)
 
     def __getitem__(self, index):
-        return (self.bytes[index >> 3] >> (index & 0b111)) & 1
+        return rshift(self.bytes[rshift(index, 3)], (index & 0b111)) & 1
 
     def __setitem__(self, index, value):
         if value:
-            self.bytes[index >> 3] |= 1 << (index & 0b111)
+            self.bytes[rshift(index, 3)] |= 1 << (index & 0b111)
         else:
-            self.bytes[index >> 3] &= ~(1 << (index & 0b111))
+            self.bytes[rshift(index, 3)] &= ~(1 << (index & 0b111))
--- snippets/sieve.py
+++ snippets/sieve.py
@@ -1,28 +1,15 @@
-from math import sqrt
-
-
-class bitArray:
-    def __init__(self, size):
-        self.bytes = bytearray((size >> 3) + 1)
-
-    def __getitem__(self, index):
-        return (self.bytes[index >> 3] >> (index & 0b111)) & 1
-
-    def __setitem__(self, index, value):
-        if value:
-            self.bytes[index >> 3] |= 1 << (index & 0b111)
-        else:
-            self.bytes[index >> 3] &= ~(1 << (index & 0b111))
+from operator import rshift
 
 
 def get_primes(n):
-    # does not return 2 and 3
-    m = n + 6 - (n % 6)
-    sieve = bitArray(m//3)
-    for i in range(1, int(sqrt(m) // 3 + 1)):
-        if not sieve[i]:
-            k = (3 * i + 1) | 1
-            for j in range(k * k // 3, m // 3, 2 * k): sieve[j] = 1
-            for j in range(k * (k - 2 * (i & 1) + 4) // 3, m // 3, 2 * k): sieve[j] = 1
+    """ Input n>=6, Returns a list of primes, 2 <= p < n """
+    m = (n+1) + 6 - ((n+1) % 6)
+    sieve = bytearray(rshift(m//3, 3) + 1)
 
-    return ((3*i + 1) | 1 for i in range(1, m//3 - (n % 6 > 1)) if not sieve[i])
+    for i in range(1, int(m**0.5) // 3 + 1):
+        if not rshift(sieve[rshift(i, 3)], (i & 0b111)) & 1:
+            k = (3 * i + 1) | 1
+            for j in range(k * k // 3, m // 3, 2 * k): sieve[rshift(j, 3)] |= 1 << (j & 0b111)
+            for j in range(k * (k - 2 * (i & 1) + 4) // 3, m // 3, 2 * k): sieve[rshift(j, 3)] |= 1 << (j & 0b111)
+    
+    return (3*i + 1|1 for i in range(1, m // 3 - (n % 6 > 1)) if not rshift(sieve[rshift(i, 3)], (i & 0b111)) & 1)
