0e6e622f82845c5e8610330445bf2d48b4c4810c
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-11 05:05:35 +0530

    Add CRT

---
+++ snippets/Mathematics/CRT.py
@@ -0,0 +1,13 @@
+from operator import mul
+from functools import reduce
+
+def chinese_remainder(a, n):
+    # solve for x = a_i (mod n_i) where n_i is prime
+    x = 0
+    prod = reduce(mul, n)
+ 
+    for a_i, n_i in zip(a, n):
+        p = prod // n_i
+        x += a_i * pow(p, n_i - 2,n_i) * p
+    
+    return x % prod
