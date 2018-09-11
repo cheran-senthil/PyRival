4a6816bb6d064e1275213473dcb0130b3fcbc5fc
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-11 21:07:21 +0530

    Use tuple instead of list

--- snippets/Mathematics/misc.py
+++ snippets/Mathematics/misc.py
@@ -2,7 +2,7 @@ from functools import reduce
 from math import factorial, gcd
 from operator import mul
 
-fib = lambda n: reduce(lambda x, n: [x[1], x[0] + x[1]], range(n), [0, 1])[0]
+fib = lambda n: reduce(lambda x, n: (x[1], x[0] + x[1]), range(n), (0, 1))[0]
 
 nCr = lambda n, r: reduce(mul, range(n-r+1, n+1), 1)//factorial(r)
 
