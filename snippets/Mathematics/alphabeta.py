af465ca666868dc1a151ac0004a7f5120e021a46
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-11 07:31:41 +0530

    Add alphabeta

---
+++ snippets/Mathematics/alphabeta.py
@@ -0,0 +1,25 @@
+class node:
+    def __init__(self, value, children=None):
+        self.value = value
+        self.children = children
+
+
+def alphabeta(node, depth, alpha=-float('+inf'), beta=float('+inf'), maximizingPlayer=True):
+    if (depth == 0) or (node.children is None):
+        return node.value
+    if maximizingPlayer:
+        value = -float('+inf')
+        for child in node.children:
+            value = max(value, alphabeta(child, depth - 1, alpha, beta, False))
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return value
+    else:
+        value = float('+inf')
+        for child in node.children:
+            value = min(value, alphabeta(child, depth - 1, alpha, beta, True))
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return value
