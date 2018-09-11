886a01f136b898673ceeae6f90a873b08c9e4fd1
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-11 17:39:44 +0530

    Add string algorithms

---
+++ snippets/String Processing/kmp.py
@@ -0,0 +1,21 @@
+def kmpSearch(T, P):
+    m = len(P)
+    b = [-1] * (m + 1)
+
+    j = -1
+    for i in range(m):
+        while (j >= 0) and (P[i] != P[j]):
+            j = b[j]
+        j += 1
+        b[i+1] = j
+
+    print(b)
+
+    j = 0
+    for i in range(len(T)):
+        while (j >= 0) and (T[i] != P[j]):
+            j = b[j]
+        j += 1
+        if j == m:
+            yield i + 1 - j
+            j = b[j]
