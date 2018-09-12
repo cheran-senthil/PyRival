1d59147865e4c4d45168f77c1514f69e2b4d7421
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-12 20:01:01 +0530

    Add more functions

--- snippets/Geometry/lines.py
+++ snippets/Geometry/lines.py
@@ -10,14 +10,25 @@ from itertools import combinations
 
 pointsToLine2d = lambda p1, p2: (p2[1] - p1[1], p1[0] - p2[0], p1[1]*p2[0] - p1[0]*p2[1])
 
-pointsToLine = lambda p1, p2: map(pointsToLine2d, combinations(p1, 2), combinations(p2, 2))
+pointsToLines = lambda p1, p2: map(pointsToLine2d, combinations(p1, 2), combinations(p2, 2))
 
 areParallel = lambda l1, l2: l1[0] * l2[1] == l2[0] * l1[1]
 
-toVec = lambda a, b: (i - j for i, j in zip(a, b))
+areSame = lambda l1, l2: areParallel(l1, l2) and (l1[1] * l2[2] == l2[1] * l1[2])
 
-dot = lambda A, B: sum(map(mul, A, B))
+intersect = lambda l1, l2: None if areParallel(l1, l2) else ((l2[1] * l1[2] - l1[1] * l2[2]) / (l2[0] * l1[1] - l1[0] * l2[1]),
+                                                             (l1[0] * l2[2] - l1[2] * l2[0]) / (l2[0] * l1[1] - l1[0] * l2[1]))
+
+toVec = lambda p1, p2: (i - j for i, j in zip(p1, p2))
+
+scale = lambda v, s: (i*s for i in v)
+
+translate = lambda p, v: (pi + vi for pi, vi in zip(p, v))
+
+dot = lambda v1, v2: sum(map(mul, v1, v2))
 
 norm_sq = lambda v: sum(i*i for i in v)
 
 angle = lambda oa, ob: acos(dot(oa, ob) / (norm_sq(oa) * norm_sq(ob))**0.5)
+
+collinear = lambda p1, p2, p3: areSame(pointsToLine2d(p1, p2), pointsToLine2d(p2, p3))
