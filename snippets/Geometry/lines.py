80ebe11c2bbab132f9197b02a8a124894e5d9aaf
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-12 18:53:45 +0530

    Improve pointsToLine

--- snippets/Geometry/lines.py
+++ snippets/Geometry/lines.py
@@ -1,9 +1,16 @@
 from math import acos
 from operator import mul
+from itertools import combinations
 
-# line: ax + by + c = 0 is (a, b, c)
+# 2d line: ax + by + c = 0  is  (a, b, c)
 
-pointsToLine = lambda p1, p2: (p2[1] - p1[1], p1[0] - p2[0], p1[1]*p2[0] - p1[0]*p2[1])
+# 3d line: ax + by + c = 0     ((a, b, c),
+#          dx + ez + f = 0  is  (d, e, f),
+#          gy + hz + i = 0      (g, h, i))
+
+pointsToLine2d = lambda p1, p2: (p2[1] - p1[1], p1[0] - p2[0], p1[1]*p2[0] - p1[0]*p2[1])
+
+pointsToLine = lambda p1, p2: map(pointsToLine2d, combinations(p1, 2), combinations(p2, 2))
 
 areParallel = lambda l1, l2: l1[0] * l2[1] == l2[0] * l1[1]
 
