616a1cf54ae486bcd62708e86b5367fa0e5a8e87
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-22 22:28:19 +0530

    Linting

--- template/template.py
+++ template/template.py
@@ -6,43 +6,42 @@ Copyright (c) 2018 Cheran Senthilkumar
 """
 # IMPORTS---------------------------------------------------------------------#
 from __future__ import division, print_function
-from functools import reduce
-from itertools import ifilter, imap, izip
 
-# from atexit import register
-# from io import BytesIO
-# from sys import __stdout__, stdin, stdout
-
+import itertools
 # import math
 # import operator as op
 # import random
-
 # from collections import Counter, defaultdict, deque
 # from fractions import Fraction, gcd
-# from decimal import Decimal, getcontext
+# from itertools import combinations, permutations, product
 # from Queue import PriorityQueue, Queue
+
 
 # PYTHON3---------------------------------------------------------------------#
 class dict(dict):
     def keys(self):
         return dict.iterkeys(self)
+
     def items(self):
         return dict.iteritems(self)
+
     def values(self):
         return dict.itervalues(self)
 
-filter = ifilter
-map = imap
-zip = izip
+
+filter = itertools.ifilter
+map = itertools.imap
+zip = itertools.izip
 
 range = xrange
 input = raw_input
 
-# SETTINGS--------------------------------------------------------------------#
-# getcontext().prec = 100
-# setrecursionlimit(100000)
 
 # FASTIO----------------------------------------------------------------------#
+# from atexit import register
+# from io import BytesIO
+# from sys import __stdout__, stdin, stdout
+
 # stdout = BytesIO()
 # register(lambda: __stdout__.write(stdout.getvalue()))
 # stdin = BytesIO(stdin.read())
@@ -50,6 +49,15 @@ input = raw_input
 # input = lambda: stdin.readline().rstrip()
 # print = lambda *args: stdout.write(' '.join(str(x) for x in args) + '\n')
 # flush = stdout.flush()
+
+
+# SETTINGS--------------------------------------------------------------------#
+# from decimal import Decimal, getcontext
+# from sys import setrecursionlimit
+
+# getcontext().prec = 100
+# setrecursionlimit(100000)
+
 
 # CONSTANTS-------------------------------------------------------------------#
 MOD = 1000000007
