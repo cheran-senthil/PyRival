b6ba016d3e9ee7f613604b14503d7419a8039db6
mukundan314 <mukundan314@gmail.com>
2018-09-24 19:44:21 +0530

    Create phi.py

---
+++ snippets/Mathematics/phi.py
@@ -0,0 +1,9 @@
+def phi(n):
+    sieve = [i if i & 1 else i // 2 for i in range(n + 1)]
+    for i in range(3, n + 1, 2):
+        if sieve[i] == i:
+            for j in range(i, n + 1, i):
+                sieve[j] = (sieve[j] // i) * (i - 1)
+
+    return sieve
+
