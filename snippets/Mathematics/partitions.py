a2cf795489b68ca36a016eae551765a78a190d10
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-25 00:42:04 +0530

    Add partition function

---
+++ snippets/Mathematics/partitions.py
@@ -0,0 +1,21 @@
+def partitions(n):
+    a = [0] * (n + 1)
+    k = 1
+    y = n - 1
+    while k != 0:
+        x = a[k - 1] + 1
+        k -= 1
+        while 2 * x <= y:
+            a[k] = x
+            y -= x
+            k += 1
+        j = k + 1
+        while x <= y:
+            a[k] = x
+            a[j] = y
+            yield a[:k + 2]
+            x += 1
+            y -= 1
+        a[k] = x + y
+        y = x + y - 1
+        yield a[:k + 1]
