7c6b81fe083007c04d3a1e6e6e968060ccda7903
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-26 17:27:12 +0530

    Add binary search

---
+++ snippets/Mathematics/binary_search.py
@@ -0,0 +1,14 @@
+def binary_search(l, value):
+    low = 0
+    high = len(l) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if l[mid] > value:
+            high = mid - 1
+        elif l[mid] < value:
+            low = mid + 1
+        else:
+            return mid
+
+    return -1
