0674d4792db84876863f29e3c70ce0c4eb8bb76f
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-28 10:37:19 +0530

    Add lis

--- snippets/String Processing/lcs.py
+++ snippets/String Processing/lcs.py
@@ -16,11 +16,15 @@ class MemoizeMutable:
 
 @MemoizeMutable
 def lcs(s1, s2):
-    if not s1 or not s2:
-        return type(s1)()
+    if (not s1) or (not s2):
+        return []
 
     return (
-        lcs(s1[:-1], s2[:-1]) + type(s1)(s1[-1])
+        lcs(s1[:-1], s2[:-1]) + [s1[-1]]
         if s1[-1] == s2[-1] else
         max(lcs(s1[:-1], s2), lcs(s1, s2[:-1]), key=len)
     )
+
+
+def lis(arr):
+    return lcs(arr, sorted(arr))
