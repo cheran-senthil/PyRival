6a150ca774ab561bfc2848fff4eef8699237b011
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-29 09:04:00 +0530

    Add range_query.py

---
+++ snippets/Data Structures/range_query.py
@@ -0,0 +1,15 @@
+from itertools import product
+
+
+class RangeQuery(object):
+    def __init__(self, items, func=min):
+        self.rq = rq = {(i, 0): item for i, item in enumerate(items)}
+        self.func = func
+        n = len(items)
+        for step, i in product(range(1, n.bit_length()), range(n)):
+            j = i + (1 << (step - 1))
+            rq[i, step] = func(rq[i, step - 1], rq[j, step - 1]) if j < n else rq[i, step - 1]
+
+    def query(self, start, stop):
+        j = (stop - start).bit_length() - 1
+        return self.func(self.rq[start, j], self.rq[stop - (1 << j), j])
