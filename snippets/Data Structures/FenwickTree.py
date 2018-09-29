94e955a6f5b74b52e0c38e79ea9127335e8b860a
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-09-29 15:17:36 +0530

    Add FenwickTree2d

--- snippets/Data Structures/FenwickTree.py
+++ snippets/Data Structures/FenwickTree.py
@@ -1,3 +1,6 @@
+from bisect import bisect_left
+
+
 class FenwickTree:
     def __init__(self, n):
         self.s = [0] * n
@@ -29,3 +32,33 @@ class FenwickTree:
             pw >>= 1
 
         return pos
+
+
+class FenwickTree2d:
+    def __init__(self, points, difs, n=100000):
+        self.ys = [[] for _ in range(n)]
+        self.ft = []
+
+        for point in points:
+            x, y = point
+            while x < len(self.ys):
+                self.ys[x].append(y)
+                x |= x + 1
+
+        for v in self.ys:
+            v.sort()
+            self.ft.append(FenwickTree(len(v)))
+
+        for point, dif in zip(points, difs):
+            x, y = point
+            while x < len(self.ys):
+                self.ft[x].update(bisect_left(self.ys[x], y), dif)
+                x |= x + 1
+
+    def query(self, point):
+        x, y = point
+        res = 0
+        while x != 0:
+            res += self.ft[x - 1].query(bisect_left(self.ys[x - 1], y))
+            x &= x - 1
+        return res
