438db65651ffcdd94aeda08d68dab0fceca00dc7
Cheran-Senthil <cheran.v.senthil@gmail.com>
2018-10-01 15:07:10 +0530

    Move Gaussian Elimination

--- snippets/Numerical/matrix.py
+++ snippets/Numerical/matrix.py
@@ -1,5 +1,4 @@
 import operator as op
-from copy import deepcopy
 
 transpose = lambda mat: list(map(list, zip(*mat)))
 
@@ -55,41 +54,3 @@ def inverse(m):
                 [-1 * m[1][0] / determinant, m[0][0] / determinant]]
 
     return transpose([[(pow(-1, i + j) * det(minor(m, i, j))) / determinant for j in range(len(m))] for i in range(len(m))])
-
-
-def gauss(A, b):
-    # returns det(A), x s.t. Ax = b
-    a = deepcopy(A)
-    x = deepcopy(b)
-    n = len(a)
-    p = len(x[0])
-
-    determinant = 1
-    for i in range(n - 1):
-        k = i
-        for j in range(i + 1, n):
-            if abs(a[j][i]) > abs(a[k][i]):
-                k = j
-        if k != i:
-            a[i], a[k] = a[k], a[i]
-            x[i], x[k] = x[k], x[i]
-            determinant = -determinant
-
-        for j in range(i + 1, n):
-            t = a[j][i] / a[i][i]
-            for k in range(i + 1, n):
-                a[j][k] -= t * a[i][k]
-            for k in range(p):
-                x[j][k] -= t * x[i][k]
-
-    for i in range(n - 1, -1, -1):
-        for j in range(i + 1, n):
-            t = a[i][j]
-            for k in range(p):
-                x[i][k] -= t * x[j][k]
-        t = 1 / a[i][i]
-        determinant *= a[i][i]
-        for j in range(p):
-            x[i][j] *= t
-
-    return determinant, x
