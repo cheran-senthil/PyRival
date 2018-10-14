ef616aa8b5b523e899fe9d248d82343916d8139d
cheran-senthil <cheran.v.senthil@gmail.com>
2018-10-14 11:22:52 +0530

    Remove unicode

--- snippets/Mathematics/cycle_finding.py
+++ snippets/Mathematics/cycle_finding.py
@@ -11,12 +11,12 @@ def cycle_finding(f, x0):
         hare = f(hare)
         lam += 1
 
-    # Find the position of the first repetition of length λ
+    # Find the position of the first repetition of length lam
     mu = 0
     tortoise = hare = x0
     for _ in range(lam):
         hare = f(hare)
-    # The distance between the hare and tortoise is now λ.
+    # The distance between the hare and tortoise is now lam.
 
     # Next, the hare and tortoise move at same speed until they agree
     while tortoise != hare:
