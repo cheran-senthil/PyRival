7a8edd83be4fd6f27e53c81ee5e41242931d3640
cheran-senthil <cheran.v.senthil@gmail.com>
2018-10-25 20:07:13 +0530

    Remove unneeded variable

--- snippets/String Processing/kmp.py
+++ snippets/String Processing/kmp.py
@@ -1,9 +1,8 @@
 def kmpSearch(T, P):
-    m = len(P)
-    b = [-1] * (m + 1)
+    b = [-1] * (len(P) + 1)
 
     j = -1
-    for i in range(m):
+    for i in range(len(P)):
         while (j >= 0) and (P[i] != P[j]):
             j = b[j]
         j += 1
@@ -14,6 +13,6 @@ def kmpSearch(T, P):
         while (j >= 0) and (T[i] != P[j]):
             j = b[j]
         j += 1
-        if j == m:
+        if j == len(P):
             yield i + 1 - j
             j = b[j]
