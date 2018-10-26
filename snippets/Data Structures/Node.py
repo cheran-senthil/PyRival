f6c21d3c5e908b19383653ede6588e806dd059e0
cheran-senthil <cheran.v.senthil@gmail.com>
2018-10-26 20:04:46 +0530

    Add Node Data Structure

---
+++ snippets/Data Structures/Node.py
@@ -0,0 +1,10 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, self.value)
