7e4c77785d3e7d454ee9d4d07ce961f3f52fb114
cheran-senthil <cheran.v.senthil@gmail.com>
2018-10-30 00:33:08 +0530

    Add docstring

--- snippets/Mathematics/is_prime.py
+++ snippets/Mathematics/is_prime.py
@@ -2,15 +2,29 @@ def _try_composite(a, d, n, s):
     if pow(a, d, n) == 1:
         return False
     for i in range(s):
-        if pow(a, 2**i * d, n) == n-1:
+        if pow(a, 2**i * d, n) == n - 1:
             return False
     return True
 
 
-def is_prime(n, _precision_for_huge_n=16):
-    if n in _known_primes or n in (0, 1):
+def is_prime(n):
+    """
+    Deterministic variant of the Miller-Rabin primality test to determine
+    whether a given number is prime.
+
+    Parameters
+    ----------
+    n : int
+        n > 1, an integer to be tested for primality.
+
+    Returns
+    -------
+    bool
+        False if n is composite, otherwise True.
+    """
+    if n in [0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]:
         return True
-    if any((n % p) == 0 for p in _known_primes):
+    if any((n % p) == 0 for p in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]):
         return False
     d, s = n - 1, 0
     while not d % 2:
@@ -19,23 +33,19 @@ def is_prime(n, _precision_for_huge_n=16):
     if n < 2047:
         return not _try_composite(2, d, n, s)
     if n < 1373653:
-        return not any(_try_composite(a, d, n, s) for a in (2, 3))
+        return not any(_try_composite(a, d, n, s) for a in [2, 3])
     if n < 25326001:
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
+        return not any(_try_composite(a, d, n, s) for a in [2, 3, 5])
     if n < 118670087467:
         if n == 3215031751:
             return False
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
+        return not any(_try_composite(a, d, n, s) for a in [2, 3, 5, 7])
     if n < 2152302898747:
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
+        return not any(_try_composite(a, d, n, s) for a in [2, 3, 5, 7, 11])
     if n < 3474749660383:
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
+        return not any(_try_composite(a, d, n, s) for a in [2, 3, 5, 7, 11, 13])
     if n < 341550071728321:
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
+        return not any(_try_composite(a, d, n, s) for a in [2, 3, 5, 7, 11, 13, 17])
     if n < 3825123056546413051:
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17, 19, 23))
-    return not any(_try_composite(a, d, n, s) for a in _known_primes[:_precision_for_huge_n])
-
-
-_known_primes = [2, 3]
-_known_primes += [x for x in range(5, 54, 2) if is_prime(x)]
+        return not any(_try_composite(a, d, n, s) for a in [2, 3, 5, 7, 11, 13, 17, 19, 23])
+    return not any(_try_composite(a, d, n, s) for a in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37])
