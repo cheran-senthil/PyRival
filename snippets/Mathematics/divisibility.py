c05e6e66cb8cbe9ce3debf3c8e524ce99ed02c82
cheran-senthil <cheran.v.senthil@gmail.com>
2018-11-06 00:09:02 +0530

    Major speed boost

--- snippets/Mathematics/divisibility.py
+++ snippets/Mathematics/divisibility.py
@@ -1,10 +1,19 @@
-from fractions import gcd
-from functools import reduce
+from math import gcd
+
 
 lcm = lambda a, b: a * b // gcd(a, b)
 
-lcmm = lambda *args: reduce(lcm, args)
-
-gcdm = lambda *args: reduce(gcd, args)
 
 mul_inv = lambda a, p: pow(a, p-2, p)
+
+
+def gcdm(a, *r):
+    for b in r:
+        a = gcd(a, b)
+    return a
+
+
+def lcmm(a, *r):
+    for b in r:
+        a = lcm(a, b)
+    return a
