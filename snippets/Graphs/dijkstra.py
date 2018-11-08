352d27448931808814ba98e8f68274c736bc5e9c
cheran-senthil <cheran.v.senthil@gmail.com>
2018-11-08 23:33:15 +0530

    Move find_path to seperate file

--- snippets/Graphs/dijkstra.py
+++ snippets/Graphs/dijkstra.py
@@ -37,29 +37,3 @@ def dijkstra(graph, start):
             visited[v] = True
 
     return dist, parents
-
-
-def find_path(start, end, parents):
-    """
-    Constructs a path between two vertices, given the parents of all vertices.
-
-    Parameters
-    ----------
-    start : int
-        The first verex in the path
-    end : int
-        The last vertex in the path
-    parents : list[int]
-        The parent of a vertex in its path from start to end.
-
-    Returns
-    -------
-    path : list[int]
-        The path from start to end.
-    """
-    path = []
-    parent = end
-    while parent != parents[start]:
-        path.append(parent)
-        parent = parents[parent]
-    return path[::-1]
---
+++ snippets/Graphs/find_path.py
@@ -0,0 +1,24 @@
+def find_path(start, end, parents):
+    """
+    Constructs a path between two vertices, given the parents of all vertices.
+
+    Parameters
+    ----------
+    start : int
+        The first verex in the path
+    end : int
+        The last vertex in the path
+    parents : list[int]
+        The parent of a vertex in its path from start to end.
+
+    Returns
+    -------
+    path : list[int]
+        The path from start to end.
+    """
+    path = []
+    parent = end
+    while parent != parents[start]:
+        path.append(parent)
+        parent = parents[parent]
+    return path[::-1]
