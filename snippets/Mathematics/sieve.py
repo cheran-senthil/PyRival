c8158151d973f5bc3a976b3f142a0cd2db4ad2d4
cheran-senthil <cheran.v.senthil@gmail.com>
2018-11-08 23:32:57 +0530

    Use 7 instead of 0b111

--- snippets/Mathematics/sieve.py
+++ snippets/Mathematics/sieve.py
@@ -3,14 +3,14 @@ def get_primes(n):
     sieve = bytearray((n//3 + (n % 6 == 2) >> 3) + 1)
 
     for i in range(1, int(n**0.5) // 3 + 1):
-        if not (sieve[i >> 3] >> (i & 0b111)) & 1:
+        if not (sieve[i >> 3] >> (i & 7)) & 1:
             k = (3 * i + 1) | 1
             for j in range(k * k // 3, n // 3 + (n % 6 == 2), 2 * k):
-                sieve[j >> 3] |= 1 << (j & 0b111)
+                sieve[j >> 3] |= 1 << (j & 7)
             for j in range(k * (k - 2 * (i & 1) + 4) // 3, n // 3 + (n % 6 == 2), 2 * k):
-                sieve[j >> 3] |= 1 << (j & 0b111)
+                sieve[j >> 3] |= 1 << (j & 7)
 
     return (
         3*i + 1 | 1 for i in range(1, n // 3 + (n % 6 == 2))
-        if not (sieve[i >> 3] >> (i & 0b111)) & 1
+        if not (sieve[i >> 3] >> (i & 7)) & 1
     )
