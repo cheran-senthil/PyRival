466cdcff217b2b3d650776cfdd343e55a8490183
cheran-senthil <cheran.v.senthil@gmail.com>
2018-11-10 06:54:44 +0530

    Add composite CRT

--- snippets/Mathematics/chinese_remainder.py
+++ snippets/Mathematics/chinese_remainder.py
@@ -12,3 +12,36 @@ def chinese_remainder(a, n):
         x += a_i * pow(p, n_i - 2, n_i) * p
 
     return x % prod
+
+
+def egcd(a, m):
+    if a == 0:
+        return (m, 0, 1)
+
+    g, y, x = egcd(m % a, a)
+    return (g, x - (m // a) * y, y)
+
+
+def modinv(a, m):
+    amodm = a % m
+    g, x, _ = egcd(amodm, m)
+
+    """Sanity Check
+    if g != 1:
+        return None
+    """
+    return x % m
+
+
+def composite_crt(b, m):
+    # solve for x = b_i (mod m_i)
+    x = 0
+    m_prod = 1
+
+    for bi, mi in zip(b, m):
+        tot = x
+        tmp = (modinv(m_prod, mi) * (bi - tot)) % mi
+        x += tmp * m_prod
+        m_prod *= mi
+
+    return x % m_prod
