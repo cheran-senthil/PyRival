62d01e19d43a17caa75f6440d0c3f1d7b465bfeb
cheran-senthil <cheran.v.senthil@gmail.com>
2018-11-18 21:00:04 +0530

    Add Factorial with Mod

--- snippets/Mathematics/nCr_p.py
+++ snippets/Mathematics/nCr_p.py
@@ -1,13 +1,27 @@
-import math
+def factorial_p(n, p):
+    ans = 1
+    if n <= p // 2:
+        for i in range(1, n + 1):
+            ans = (ans * i) % p
+    else:
+        for i in range(1, p - n):
+            ans = (ans * i) % p
+        ans = pow(ans, p - 2, p)
+
+        if n % 2 == 0:
+            ans = p - ans
+
+    return ans
 
 
 def nCr_p(n, r, p):
-    c = 1
+    ans = 1
     while (n != 0) or (r != 0):
         a, b = n % p, r % p
         if a < b:
             return 0
-        c = (c * math.factorial(a) * pow(math.factorial(b), p-2, p) * pow(math.factorial(a - b), p - 2, p)) % p
+        ans = (ans * factorial_p(a, p) * pow(factorial_p(b, p), p - 2, p) * pow(factorial_p(a - b, p), p - 2, p)) % p
         n //= p
         r //= p
-    return c
+
+    return ans
