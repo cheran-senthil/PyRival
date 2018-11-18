3e4bb1ae5dfc849bc24cb5430fe7b0eeee1ae19d
cheran-senthil <cheran.v.senthil@gmail.com>
2018-11-18 21:04:48 +0530

    Add all_factors

--- snippets/Mathematics/factors.py
+++ snippets/Mathematics/factors.py
@@ -1,5 +1,6 @@
 import random
 from collections import Counter
+from functools import reduce
 from math import gcd
 
 
@@ -122,3 +123,7 @@ def factors(n):
             return factors(n)
         else:
             return factors(f) + factors(n//f)
+
+
+all_factors = lambda n: set(reduce(list.__add__,
+                            ([i, n//i] for i in range(1, int(n**0.5) + 1, 2 if n % 2 else 1) if n % i == 0)))
