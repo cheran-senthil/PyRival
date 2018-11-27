fbd840205a5b0e0324e3a6c7e1916bf705b4b72e
mukundan314 <mukundan314@gmail.com>
2018-11-27 21:55:43 +0530

    Add bfs_depth

--- snippets/Graphs/bfs.py
+++ snippets/Graphs/bfs.py
@@ -9,3 +9,11 @@ def bfs(tree, start=0):
         out.append(cur_node)
         queue.extend(tree[cur_node])
     return out
+
+
+def bfs_depth(tree, start=0):
+    q, ret = [start], []
+    while q:
+        ret.append(q)
+        q = [i for node in q for i in tree[node]]
+    return ret
