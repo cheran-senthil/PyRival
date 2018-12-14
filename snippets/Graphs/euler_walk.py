89a46a90157059dd736e42d2599af4385f2ad987
cheran-senthil <cheran.v.senthil@gmail.com>
2018-12-14 05:34:55 +0530

    Add Eulerian Path

---
+++ snippets/Graphs/euler_walk.py
@@ -0,0 +1,61 @@
+def euler_walk(n, g):
+    deg = [0] * n
+
+    for i in range(n):
+        for j in range(n):
+            deg[i] += g[i][j]
+
+    first = 0
+    while deg[first] == 0:
+        first += 1
+
+    v1, v2 = -1, -1
+    bad = False
+
+    for i in range(n):
+        if deg[i] % 2 == 1:
+            if v1 == -1:
+                v1 = i
+            elif v2 == -1:
+                v2 = i
+            else:
+                bad = True
+
+    if v1 != -1:
+        g[v1][v2] += 1
+        g[v2][v1] += 1
+
+    st, res = [first], []
+
+    while st:
+        v = st[-1]
+        flag = False
+
+        for i in range(n):
+            if g[v][i]:
+                flag = True
+                break
+
+        if flag:
+            g[v][i] -= 1
+            g[i][v] -= 1
+            st.append(i)
+        else:
+            res.append(v)
+            st.pop()
+
+    if v1 != -1:
+        for i in range(len(res) - 1):
+            if ((res[i] == v1) and (res[i + 1] == v2)) or ((res[i] == v2) and (res[i + 1] == v1)):
+                res = [res[j] for j in range(i + 1, len(res))] + [res[j] for j in range(1, i + 1)]
+                break
+
+    for i in range(n):
+        for j in range(n):
+            if g[i][j]:
+                bad = True
+
+    if bad:
+        return None
+
+    return res
