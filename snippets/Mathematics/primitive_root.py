dcb6bf3eabb182a92ac94d367c2c0cb1de9df6e7
cheran-senthil <cheran.v.senthil@gmail.com>
2019-01-07 13:12:08 +0530

    Create primitive_root.py

---
+++ snippets/Mathematics/primitive_root.py
@@ -0,0 +1,164 @@
+import random
+from collections import Counter
+from math import gcd
+
+
+def memodict(f):
+    """ Memoization decorator for a function taking a single argument. """
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+    return memodict().__getitem__
+
+
+def is_prime(n):
+    """
+    Deterministic variant of the Miller-Rabin primality test to determine
+    whether a given number (upto 2**64) is prime.
+
+    Parameters
+    ----------
+    n : int
+        n >= 0, an integer to be tested for primality.
+
+    Returns
+    -------
+    bool
+        False if n is composite, otherwise True.
+    """
+    if n in [2, 3, 5, 13, 19, 73, 193, 407521, 299210837]:
+        return True
+
+    if (n in [0, 1]) or (any((n % p) == 0 for p in [2, 3, 5, 13, 19, 73, 193, 407521, 299210837])):
+        return False
+
+    d, s = n - 1, 0
+    while not d % 2:
+        d, s = d >> 1, s + 1
+
+    def try_composite(a):
+        if pow(a, d, n) == 1:
+            return False
+        for i in range(s):
+            if pow(a, 2**i * d, n) == n - 1:
+                return False
+        return True
+
+    return not any(try_composite(w) for w in [2, 325, 9375, 28178, 450775, 9780504, 1795265022])
+
+
+@memodict
+def pollard_rho(n):
+    if n <= 1:
+        return Counter()
+
+    if is_prime(n):
+        return Counter({n: 1})
+
+    y, c, m = random.randint(1, n - 1), random.randint(1, n - 1), random.randint(1, n - 1)
+    g, r, q = 1, 1, 1
+
+    while g == 1:
+        x, k = y, 0
+
+        for _ in range(r):
+            y = (y*y + c) % n
+
+        while (k < r) and (g == 1):
+            ys = y
+
+            for _ in range(min(m, r - k)):
+                y = (y*y + c) % n
+                q = (q * abs(x - y)) % n
+
+            g = gcd(q, n)
+            k += m
+
+        r *= 2
+
+    if g == n:
+        while True:
+            ys = (ys*ys + c) % n
+            g = gcd(abs(x - ys), n)
+            if g > 1:
+                break
+
+    return pollard_rho(g) + pollard_rho(n // g)
+
+
+def factors(n):
+    """
+    Prime factorization using Pollard's rho algorithm.
+
+    Parameters
+    ----------
+    n : int
+        n > 0, an integer to be factorized.
+
+    Returns
+    -------
+    prime_factors : Counter
+        Counter of the prime factors of n.
+    """
+    prime_factors = Counter()
+
+    def ilog(n, p):
+        cnt = 0
+        while not n % p:
+            n, cnt = n // p, cnt + 1
+        return n, cnt
+
+    if n % 2 == 0:
+        n, prime_factors[2] = ilog(n, 2)
+    if n % 3 == 0:
+        n, prime_factors[3] = ilog(n, 3)
+
+    i = 5
+    while i * i <= min(n, 4294967295):
+        if n % i == 0:
+            n, prime_factors[i] = ilog(n, i)
+        i += 2 if i % 3 == 2 else 4
+
+    if n <= 1:
+        return prime_factors
+
+    if n <= 4294967295:
+        prime_factors[n] = 1
+        return prime_factors
+
+    return prime_factors + pollard_rho(n)
+
+
+def perfect_base(n):
+    b = n.bit_length()
+    for a in range(b, 0, -1):
+        lo, hi = 1, 1 << (b//a + 1)
+        while lo < hi:
+            mi = (lo + hi) // 2
+
+            a_b = mi**a
+            if a_b == n:
+                return mi
+
+            if a_b > n:
+                hi = mi
+            else:
+                lo = mi + 1
+
+
+def primitive_root(p):
+    if p == 4:
+        return 3
+    if -p & p == p:
+        return None
+
+    factor = perfect_base(p if p & 1 else p >> 1)
+    if not is_prime(factor):
+        return None
+
+    phi = (factor - 1) * ((p if p & 1 else p >> 1) // factor)
+
+    for res in range(2 + (1 ^ (p & 1)), factor, 1 + (1 ^ (p & 1))):
+        if all(pow(res, phi // i, p) != 1 for i in factors(phi).keys()):
+            return res
