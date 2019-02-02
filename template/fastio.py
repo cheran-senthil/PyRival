89318d28962ebde595a39e464170bdb318d75756
cheran-senthil <cheran.v.senthil@gmail.com>
2019-02-02 23:42:15 +0530

    Update fastio

---
+++ template/fastio.py
@@ -0,0 +1,18 @@
+import sys
+from atexit import register
+from io import BytesIO, FileIO, StringIO
+
+
+INP_FILE = 0
+OUT_FILE = 1
+
+if sys.version_info[0] < 3:
+    input = iter(FileIO(INP_FILE).read().splitlines()).next
+
+    sys.stdout = BytesIO()
+    register(lambda: FileIO(OUT_FILE, 'w').write(sys.stdout.getvalue()))
+else:
+    input = iter(FileIO(INP_FILE).read().splitlines()).__next__
+
+    sys.stdout = StringIO()
+    register(lambda: FileIO(OUT_FILE, 'w').write(sys.stdout.getvalue().encode()))
