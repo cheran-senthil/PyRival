8b27cb84590e044a1c14545337492fa180d60386
cheran-senthil <cheran.v.senthil@gmail.com>
2019-02-09 23:03:57 +0530

    Use @pajenegod's SegmentTree

--- snippets/Data Structures/SegmentTree.py
+++ snippets/Data Structures/SegmentTree.py
@@ -1,35 +1,115 @@
-import operator as op
+# A very nice implementation of a maximum segment tree with
+# some inspiration taken from https://codeforces.com/blog/entry/18051
+# This implementation should be able to be modified to do pretty
+# much anything one would want to do with segment trees apart from
+# persistance.
+# Note that especially in python this implementation is much much better
+# than most other approches because how slow python can be with function
+# calls.
+
+# Currently it allows for two operations, both running in O(log n),
+# 'add(l,r,value)' adds value to [l,r)
+# 'maxi(l,r)' returns the biggest value on l:r
 
 
 class SegmentTree:
-    def __init__(self, m, d=0, func=op.add, low=0):
-        self.n = 1 << m.bit_length()
-        self.s = [d if i < self.n + m else low for i in range(2*self.n)]
-        self.func = func
-        self.low = low
+    def __init__(self, data):
+        n = len(data)
+        m = 1
+        while m < n:
+            m *= 2
 
-        for i in range(self.n - 1, 0, -1):
-            self.s[i] = func(self.s[2*i], self.s[2*i + 1])
+        self.n = n
+        self.m = m
+        self.data = [0] * (2 * m)
+        for i in range(n):
+            self.data[i + m] = data[i]
+        for i in reversed(range(m)):
+            self.data[i] = max(self.data[2 * i], self.data[2 * i + 1])
+        self.query = [0] * (2 * m)
 
-    def __setitem__(self, pos, val):
-        pos += self.n
-        self.s[pos] = val
-        pos //= 2
-        while pos >= 1:
-            self.s[pos] = self.func(self.s[2*pos], self.s[2*pos + 1])
-            pos //= 2
+    # Push the query on seg_ind to its children
+    def push(self, seg_ind):
+        # Let the children know of the queries
+        q = self.query[seg_ind]
 
-    def __getitem__(self, pos):
-        return self.query(pos, pos + 1)
+        self.query[2 * seg_ind] += q
+        self.query[2 * seg_ind + 1] += q
 
-    def que(self, pos, left, right, lo, hi):
-        if (right <= lo) or (hi <= left):
-            return self.low
-        if (left <= lo) and (hi <= right):
-            return self.s[pos]
+        self.data[2 * seg_ind] += q
+        self.data[2 * seg_ind + 1] += q
 
-        return self.func(self.que(2 * pos, left, right, lo, (lo + hi) / 2),
-                         self.que(2*pos + 1, left, right, (lo + hi) / 2, hi))
+        # Remove queries from seg_ind
+        self.data[seg_ind] = max(self.data[2 * seg_ind],
+                                 self.data[2 * seg_ind + 1])
+        self.query[seg_ind] = 0
 
-    def query(self, left, right):
-        return self.que(1, left, right, 0, self.n)
+    # Updates the node seg_ind to know of all queries
+    # applied to it via its ancestors
+    def update(self, seg_ind):
+        # Find all indecies to be updated
+        seg_ind //= 2
+        inds = []
+        while seg_ind > 0:
+            inds.append(seg_ind)
+            seg_ind //= 2
+
+        # Push the queries down the segment tree
+        for ind in reversed(inds):
+            self.push(ind)
+
+    # Make the changes to seg_ind be known to its ancestors
+    def build(self, seg_ind):
+        seg_ind //= 2
+        while seg_ind > 0:
+            self.data[seg_ind] = max(
+                self.data[2 * seg_ind],
+                self.data[2 * seg_ind + 1]) + self.query[seg_ind]
+            seg_ind //= 2
+
+    # Lazily add value to [l,r)
+    def add(self, l, r, value):
+        l += self.m
+        r += self.m
+
+        l0 = l
+        r0 = r
+
+        while l < r:
+            if l % 2 == 1:
+                self.query[l] += value
+                self.data[l] += value
+                l += 1
+            if r % 2 == 1:
+                r -= 1
+                self.query[r] += value
+                self.data[r] += value
+            l //= 2
+            r //= 2
+
+        # Tell all nodes above of the updated
+        # area of the updates
+        self.build(l0)
+        self.build(r0 - 1)
+
+    # Max of data[l,r)
+    def maxi(self, l, r):
+        l += self.m
+        r += self.m
+
+        # Apply all the lazily stored queries
+        self.update(l)
+        self.update(r - 1)
+
+        segs = []
+        while l < r:
+            if l % 2 == 1:
+                segs.append(l)
+                l += 1
+            if r % 2 == 1:
+                r -= 1
+                segs.append(r)
+            l //= 2
+            r //= 2
+
+        return max(self.data[ind] for ind in segs)
