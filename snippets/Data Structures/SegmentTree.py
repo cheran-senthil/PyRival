fca51dcb6367d6ad2bd7c99d354d29f3bc0cd10f
cheran-senthil <cheran.v.senthil@gmail.com>
2019-02-11 19:30:39 +0530

    Improved formatting

--- snippets/Data Structures/SegmentTree.py
+++ snippets/Data Structures/SegmentTree.py
@@ -1,35 +1,36 @@
-# A very nice implementation of a maximum segment tree with
-# some inspiration taken from https://codeforces.com/blog/entry/18051
-# This implementation should be able to be modified to do pretty
-# much anything one would want to do with segment trees apart from
-# persistance.
-# Note that especially in python this implementation is much much better
-# than most other approches because how slow python can be with function
-# calls.
+"""
+A very nice implementation of a maximum segment tree with
+some inspiration taken from https://codeforces.com/blog/entry/18051
 
-# Currently it allows for two operations, both running in O(log n),
-# 'add(l,r,value)' adds value to [l,r)
-# 'maxi(l,r)' returns the biggest value on l:r
+This implementation should be able to be modified to do pretty
+much anything one would want to do with segment trees apart from
+persistance.
+
+Note that especially in python this implementation is much much better
+than most other approches because how slow python can be with function
+calls.
+
+Currently it allows for two operations, both running in O(log n),
+'add(l,r,value)' adds value to [l,r)
+'maxi(l,r)' returns the biggest value on l:r
+"""
 
 
 class SegmentTree:
     def __init__(self, data):
-        n = len(data)
-        m = 1
-        while m < n:
-            m *= 2
+        self.n = len(data)
+        self.m = 1 << self.n.bit_length()
+        self.data = [0] * (2 * self.m)
 
-        self.n = n
-        self.m = m
-        self.data = [0] * (2 * m)
-        for i in range(n):
-            self.data[i + m] = data[i]
-        for i in reversed(range(m)):
+        for i in range(len(data)):
+            self.data[i + self.m] = data[i]
+        for i in reversed(range(self.m)):
             self.data[i] = max(self.data[2 * i], self.data[2 * i + 1])
-        self.query = [0] * (2 * m)
 
-    # Push the query on seg_ind to its children
+        self.query = [0] * (2 * self.m)
+
     def push(self, seg_ind):
+        """Push the query on seg_ind to its children"""
         # Let the children know of the queries
         q = self.query[seg_ind]
 
@@ -43,70 +44,70 @@ class SegmentTree:
         self.data[seg_ind] = max(self.data[2 * seg_ind], self.data[2 * seg_ind + 1])
         self.query[seg_ind] = 0
 
-    # Updates the node seg_ind to know of all queries
-    # applied to it via its ancestors
     def update(self, seg_ind):
+        """Updates the node seg_ind to know of all queries applied to it via its ancestors"""
         # Find all indecies to be updated
-        seg_ind //= 2
+        seg_ind >>= 1
         inds = []
         while seg_ind > 0:
             inds.append(seg_ind)
-            seg_ind //= 2
+            seg_ind >>= 1
 
         # Push the queries down the segment tree
         for ind in reversed(inds):
             self.push(ind)
 
-    # Make the changes to seg_ind be known to its ancestors
     def build(self, seg_ind):
-        seg_ind //= 2
+        """Make the changes to seg_ind be known to its ancestors"""
+        seg_ind >>= 1
         while seg_ind > 0:
             self.data[seg_ind] = max(self.data[2 * seg_ind], self.data[2 * seg_ind + 1]) + self.query[seg_ind]
-            seg_ind //= 2
+            seg_ind >>= 1
 
-    # Lazily add value to [l,r)
-    def add(self, l, r, value):
-        l += self.m
-        r += self.m
+    def add(self, start, end, value):
+        """Lazily add value to [start, end)"""
+        start += self.m
+        end += self.m
+        _start, _end = start, end
 
-        l0 = l
-        r0 = r
+        while _start < _end:
+            if _start & 1:
+                self.query[_start] += value
+                self.data[_start] += value
+                _start += 1
 
-        while l < r:
-            if l % 2 == 1:
-                self.query[l] += value
-                self.data[l] += value
-                l += 1
-            if r % 2 == 1:
-                r -= 1
-                self.query[r] += value
-                self.data[r] += value
-            l //= 2
-            r //= 2
+            if _end & 1:
+                _end -= 1
+                self.query[_end] += value
+                self.data[_end] += value
 
-        # Tell all nodes above of the updated
-        # area of the updates
-        self.build(l0)
-        self.build(r0 - 1)
+            _start >>= 1
+            _end >>= 1
 
-    # Max of data[l,r)
-    def maxi(self, l, r):
-        l += self.m
-        r += self.m
+        # Tell all nodes above of the updated area of the updates
+        self.build(start)
+        self.build(end - 1)
+
+    def maxi(self, start, end):
+        """Max of data[start, end)"""
+        start += self.m
+        end += self.m
 
         # Apply all the lazily stored queries
-        self.update(l)
-        self.update(r - 1)
-
+        self.update(start)
+        self.update(end - 1)
         segs = []
-        while l < r:
-            if l % 2 == 1:
-                segs.append(l)
-                l += 1
-            if r % 2 == 1:
-                r -= 1
-                segs.append(r)
-            l //= 2
-            r //= 2
 
-        return max(self.data[ind] for ind in segs)
+        while start < end:
+            if start & 1:
+                segs.append(start)
+                start += 1
+
+            if end & 1:
+                end -= 1
+                segs.append(end)
+
+            start >>= 1
+            end >>= 1
+
+        return max(self.data[i] for i in segs)
