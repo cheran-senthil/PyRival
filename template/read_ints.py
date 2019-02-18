30234cb425586635eed1323507dced828bd9a5ab
cheran-senthil <cheran.v.senthil@gmail.com>
2019-02-19 04:48:50 +0530

    remove function overhead

--- template/read_ints.py
+++ template/read_ints.py
@@ -1,23 +1,18 @@
 import os
 
+numb, sign = 0, 1
+res = []
 
-def read_ints():
-    """Read integers from underlying buffer until we hit EOF."""
-    numb, sign = 0, 1
-    res = []
-
-    s = os.read(0, os.fstat(0).st_size)
-    for i in range(len(s)):
-        if s[i] >= '0':
-            numb = 10 * numb + ord(s[i]) - 48
+s = os.read(0, os.fstat(0).st_size)
+for i in range(len(s)):
+    if s[i] >= '0':
+        numb = 10 * numb + ord(s[i]) - 48
+    else:
+        if s[i] == '-':
+            sign = -1
         else:
-            if s[i] == '-':
-                sign = -1
-            else:
-                res.append(sign * numb)
-                numb, sign = 0, 1
+            res.append(sign * numb)
+            numb, sign = 0, 1
 
-    if s[-1] >= '0':
-        res.append(sign * numb)
-
-    return res
+if s[-1] >= '0':
+    res.append(sign * numb)
