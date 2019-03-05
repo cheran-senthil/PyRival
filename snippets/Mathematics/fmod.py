6aff2cf3d3dec40ae6a97daf88ebd469bbcf8eff
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-05 07:50:37 +0530

    Add fpow

--- snippets/Mathematics/fmod.py
+++ snippets/Mathematics/fmod.py
@@ -5,4 +5,15 @@ SHRT = 65536.0
 
 fmod = lambda x: x - FMOD * math.trunc(x / FMOD)
 
-mod_prod = lambda a, b, c=0: fmod(math.trunc(a / SHRT) * fmod(b * SHRT) + (a - SHRT * math.trunc(a / SHRT)) * b + c)
+fmul = lambda a, b, c=0: fmod(math.trunc(a / SHRT) * fmod(b * SHRT) + (a - SHRT * math.trunc(a / SHRT)) * b + c)
+
+
+def fpow(x, y):
+    res = 1.0
+    while y > 0:
+        if y & 1 == 1:
+            res = fmul(res, x)
+        x = fmul(x, x)
+        y >>= 1
+
+    return res
