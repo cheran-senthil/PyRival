2db8e1ca160c18b917dd14385730c80016d8c091
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-09 08:43:24 +0530

    Avoid formatting

--- snippets/String Processing/LPSubstr.py
+++ snippets/String Processing/LPSubstr.py
@@ -1,15 +1,22 @@
 def LPSubstr(s):
-    T = '#'.join('^{}$'.format(s))
-    n = len(T)
-    P = [0] * n
-    C = R = 0
-    for i in range(1, n - 1):
-        P[i] = (R > i) and min(R - i, P[2 * C - i])
-        while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
-            P[i] += 1
+    n = len(s)
+    p = [[0] * (n + 1), [0] * n]
 
-        if i + P[i] > R:
-            C, R = i, i + P[i]
+    for z, p_z in enumerate(p):
+        left, right = 0, 0
+        for i in range(n):
+            t = right - i + 1 - z
+            if i < right:
+                p_z[i] = min(t, p_z[left + t])
+            L, R = i - p_z[i], i + p_z[i] - 1 + z
+            while (L >= 1) and (R + 1 < n) and (s[L - 1] == s[R + 1]):
+                p_z[i] += 1
+                L -= 1
+                R += 1
+            if R > right:
+                left, right = L, R
 
-    maxLen, centerIndex = max((n, i) for i, n in enumerate(P))
-    return s[(centerIndex - maxLen) // 2:(centerIndex + maxLen) // 2]
+    i1, x1 = max(enumerate(p[0]), key=lambda x: x[1])
+    i2, x2 = max(enumerate(p[1]), key=lambda x: x[1])
+
+    return s[i1 - x1:i1 + x1], s[i2 - x2:i2 + x2 + 1]
