b54e44aa75ba0a4ab799151a48eed4c0d6e726cf
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-09 08:44:54 +0530

    Simplify least rotation

--- snippets/String Processing/min_rotation.py
+++ snippets/String Processing/min_rotation.py
@@ -1,19 +1,14 @@
 def least_rotation(s):
+    a, n = 0, len(s)
     s += s
-    i, ans = 0, 0
 
-    while 2 * i < len(s):
-        ans = i
-        j, k = i + 1, i
+    for b in range(n):
+        for i in range(n):
+            if (a + i == b) or (s[a + 1] < s[b + i]):
+                b += max(0, i - 1)
+                break
+            if s[a + i] > s[b + i]:
+                a = b
+                break
 
-        while (2 * j < len(s)) and (s[k] <= s[j]):
-            if s[k] < s[j]:
-                k = i
-            else:
-                k += 1
-            j += 1
-
-        while i <= k:
-            i += j - k
-
-    return s[ans:ans + (len(s) // 2)]
+    return s[a:a + n]
