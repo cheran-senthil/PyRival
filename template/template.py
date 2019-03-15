dac994f5275499fdd000cbecb464d7899653d8e2
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-15 08:31:29 +0530

    Bump Fast IO

--- template/template.py
+++ template/template.py
@@ -10,24 +10,29 @@ import itertools
 import os
 import sys
 from atexit import register
-from io import BytesIO, StringIO
 
 if sys.version_info[0] < 3:
+    from cStringIO import StringIO
+
     range = xrange
 
     filter = itertools.ifilter
     map = itertools.imap
     zip = itertools.izip
 
-    sys.stdin = BytesIO(os.read(0, os.fstat(0).st_size))
-    sys.stdout = BytesIO()
+    sys.stdin = StringIO(os.read(0, os.fstat(0).st_size))
+    input = lambda: sys.stdin.readline().rstrip('\r\n')
+
+    sys.stdout = StringIO()
     register(lambda: os.write(1, sys.stdout.getvalue()))
 else:
-    sys.stdin = StringIO(os.read(0, os.fstat(0).st_size).decode())
+    from io import BytesIO, StringIO
+
+    sys.stdin = BytesIO(os.read(0, os.fstat(0).st_size))
+    input = lambda: sys.stdin.readline().rstrip(b'\r\n')
+
     sys.stdout = StringIO()
     register(lambda: os.write(1, sys.stdout.getvalue().encode()))
-
-input = lambda: sys.stdin.readline().rstrip('\r\n')
 
 
 def main():
