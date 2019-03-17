a3804e5e720d7e560b6cbd01ab17ca23712bc212
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-18 04:04:59 +0530

    restructure

--- template/fastio.py
+++ template/fastio.py
@@ -7,10 +7,10 @@ if sys.version_info[0] < 3:
 else:
     from io import BytesIO as StringIO
 
-input = StringIO(os.read(0, os.fstat(0).st_size)).readline
 sys.stdout = StringIO()
-register(lambda: os.write(1, sys.stdout.getvalue()))
-
 if sys.version_info[0] >= 3:
     _write = sys.stdout.write
     sys.stdout.write = lambda s: _write(s.encode())
+
+register(lambda: os.write(1, sys.stdout.getvalue()))
+input = StringIO(os.read(0, os.fstat(0).st_size)).readline
