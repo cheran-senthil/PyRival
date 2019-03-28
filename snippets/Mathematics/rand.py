e687f5e2079dde468cb674dcc4ff61a46e30690e
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-28 20:03:21 +0530

    Initialize seed to time

--- snippets/Mathematics/rand.py
+++ snippets/Mathematics/rand.py
@@ -1,7 +1,9 @@
 from _random import Random
 
-random = Random().random
-randint = lambda a, b: a + int(random() * (b - a + 1))
+Random = Random()
+Random.seed()
+
+randint = lambda a, b: a + int(Random.random() * (b - a + 1))
 
 
 def shuffle(x):
