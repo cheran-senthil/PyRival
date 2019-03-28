2f700e14f0a3ff8532673150e34d69f861d6f749
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-28 23:45:42 +0530

    Simplify compatability tools

--- template/py3k.py
+++ template/py3k.py
@@ -4,21 +4,5 @@ from __future__ import division, print_function
 import sys
 
 if sys.version_info[0] < 3:
-    from cStringIO import StringIO
-    from itertools import ifilter, imap, izip
-    from Queue import Queue
-else:
-    from functools import reduce
-    from io import BytesIO as StringIO
-    from queue import Queue
-
-if sys.version_info[0] < 3:
-    input, range = raw_input, xrange
-    filter, map, zip = ifilter, imap, izip
-
-
-def gcd(x, y):
-    """ greatest common divisor of x and y """
-    while y:
-        x, y = y, x % y
-    return x
+    from __builtin__ import xrange as range
+    from future_builtins import ascii, filter, hex, map, oct, zip
