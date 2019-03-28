6d1d0423ef2acd921c5f1122061d3e1b2a9906fc
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-28 19:06:55 +0530

    Improve IO class

--- snippets/Data Structures/FastIO.py
+++ snippets/Data Structures/FastIO.py
@@ -2,31 +2,56 @@ import os
 import sys
 from io import BytesIO, IOBase
 
+sys.stdout, stream = IOBase(), BytesIO()
+sys.stdout.flush = lambda: os.write(1, stream.getvalue()) and not stream.truncate(0) and stream.seek(0)
+sys.stdout.write = stream.write if sys.version_info[0] < 3 else lambda s: stream.write(s.encode())
 
-class FastI(BytesIO):
+
+class FastI():
+    """ FastIO for PyPy2 and PyPy3 by Pajenegod """
+    stream = BytesIO()
     newlines = 0
-    read = lambda self: super(FastI, self).read() if self.read1() or self.tell() else self.getvalue()
 
-    def read1(self, getline=False, s=b'\n'):
-        while s and not (getline and self.newlines):
-            s, ptr = os.read(0, (1 << 13) + os.fstat(0).st_size), self.tell()
-            self.newlines += s.count(b'\n')
+    def _read(self):
+        s, ptr = os.read(0, (1 << 13) + os.fstat(0).st_size), self.stream.tell()
+        self.stream.seek(0, 2)
+        self.stream.write(s)
+        self.stream.seek(ptr)
 
-            self.seek(0, 2)
-            self.write(s)
-            self.seek(ptr)
+        return s
+
+    def read(self):
+        while self._read():
+            pass
+        return self.stream.read() if self.stream.tell() else self.stream.getvalue()
 
     def readline(self):
-        self.read1(True)
+        while self.newlines == 0:
+            s = self._read()
+            self.newlines += s.count(b'\n') + (not s)
         self.newlines -= 1
-        return super(FastI, self).readline()
+
+        return self.stream.readline()
+
+    def readnumbers(self, var=int):
+        """ Read numbers till EOF. Use var to change type. """
+        numbers, buffer = [], self.read()
+
+        numb, sign = var(0), 1
+        for char in buffer:
+            if char >= b'0' [0]:
+                numb = 10 * numb + ((ord(char) if sys.version_info[0] < 3 else char) - 48)
+            elif char == b'-' [0]:
+                sign = -1
+            elif char != b'\r' [0]:
+                numbers.append(sign * numb)
+                numb, sign = var(0), 1
+
+        if buffer and buffer[-1] >= b'0' [0]:
+            numbers.append(sign * numb)
+
+        return numbers
 
 
-class FastO(IOBase):
-    stream = BytesIO()
-    flush = lambda self: os.write(1, self.stream.getvalue()) + self.stream.truncate(0)
-    write = lambda self, s: self.stream.write(s) if sys.version_info[0] < 3 else self.stream.write(s.encode())
-
-
-sys.stdin, sys.stdout = FastI(), FastO()
+sys.stdin = FastI()
 input = sys.stdin.readline
