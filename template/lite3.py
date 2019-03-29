a24f3f099f49589cb6c467a50b2f211960a562c6
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-30 00:07:28 +0530

    compress further

--- template/lite3.py
+++ template/lite3.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-import io
 import os
 import sys
+from io import BytesIO, IOBase
 
-sys.stdout, stream = io.IOBase(), io.BytesIO()
+sys.stdout, stream = IOBase(), BytesIO()
 sys.stdout.flush = lambda: os.write(1, stream.getvalue()) and not stream.truncate(0) and stream.seek(0)
 sys.stdout.write = lambda s: stream.write(s.encode())
 
-input, flush = sys.stdin.readline, sys.stdout.flush
-input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+input = BytesIO(os.read(0, os.fstat(0).st_size)).readline
 
 
 def main():
