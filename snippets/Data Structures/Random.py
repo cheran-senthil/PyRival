68376c3b334289434c944922db40dd2d3ca19d02
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-29 15:47:58 +0530

    Improve random

---
+++ snippets/Data Structures/Random.py
@@ -0,0 +1,15 @@
+import _random
+
+
+class Random(_random.Random):
+    def shuffle(self, x):
+        for i in range(len(x) - 1, 0, -1):
+            j = int(self.random() * (i + 1))
+            x[i], x[j] = x[j], x[i]
+
+    randrange = lambda self, a, b, step=1: a + step * int(self.random() * ((b - a + step + [1, -1][step < 0]) // step))
+    randint = lambda self, a, b: a + int(self.random() * (b - a + 1))
+    choice = lambda self, seq: seq[int(self.random() * len(seq))]
+
+
+random = Random()
