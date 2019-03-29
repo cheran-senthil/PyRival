71233aeab8a964d89fade388c4c1446c9c0a8b10
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-29 16:49:37 +0530

    Remove unreachable if

--- template/lite3.py
+++ template/lite3.py
@@ -5,7 +5,7 @@ import sys
 
 sys.stdout, stream = io.IOBase(), io.BytesIO()
 sys.stdout.flush = lambda: os.write(1, stream.getvalue()) and not stream.truncate(0) and stream.seek(0)
-sys.stdout.write = stream.write if sys.version_info[0] < 3 else lambda s: stream.write(s.encode())
+sys.stdout.write = lambda s: stream.write(s.encode())
 
 input, flush = sys.stdin.readline, sys.stdout.flush
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
