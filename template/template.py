ee23a713bffd09391d5f0ebcbd472adf129286c9
cheran-senthil <cheran.v.senthil@gmail.com>
2019-03-29 16:09:58 +0530

    Improve usability for interactive

--- template/template.py
+++ template/template.py
@@ -16,6 +16,7 @@ sys.stdout, stream = io.IOBase(), StringIO()
 sys.stdout.flush = lambda: os.write(1, stream.getvalue()) and not stream.truncate(0) and stream.seek(0)
 sys.stdout.write = stream.write if sys.version_info[0] < 3 else lambda s: stream.write(s.encode())
 
+input, flush = sys.stdin.readline, sys.stdout.flush
 input = StringIO(os.read(0, os.fstat(0).st_size)).readline
 
 
