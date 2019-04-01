392d0394a6842c3a57da21d98a634813d4088777
Mukundan Senthil <mukundan314@gmail.com>
2019-04-01 19:01:26 +0530

    Remove use of deque in is_bipartite (#8)
    * Update is_bipartite
    
    * Fix bug in is_bipartite

--- snippets/Graphs/is_bipartite.py
+++ snippets/Graphs/is_bipartite.py
@@ -1,22 +1,20 @@
-from collections import deque
+def is_bipartite(graph):
+    n = len(graph)
+    color = [-1] * n
 
+    for start in range(n):
+        if color[start] == -1:
+            color[start] = 0
+            stack = [start]
 
-def is_bipartite(n, graph):
-    bipartite = True
-    side = [-1] * n
-    q = deque()
+            while stack:
+                parent = stack.pop()
 
-    for st in range(n):
-        if side[st] == -1:
-            q.append(st)
-            side[st] = 0
-            while len(q):
-                v = q.popleft()
-                for u in graph[v]:
-                    if side[u] == -1:
-                        side[u] = side[v] ^ 1
-                        q.append(u)
-                    else:
-                        bipartite &= side[u] != side[v]
+                for child in graph[parent]:
+                    if color[child] == -1:
+                        color[child] = 1 - color[parent]
+                        stack.append(child)
+                    elif color[parent] == color[child]:
+                        return False, color
 
-    return bipartite
+    return True, color
