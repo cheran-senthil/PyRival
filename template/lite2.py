a19e088339089c057d28f0dc42a83eb0afe36305
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-07 03:50:07 +0530

    Use improved FastIO

--- template/lite2.py
+++ template/lite2.py
@@ -8,12 +8,41 @@ from cStringIO import StringIO
 from future_builtins import ascii, filter, hex, map, oct, zip
 from io import IOBase
 
-sys.stdout, stream = IOBase(), StringIO()
-sys.stdout.flush = lambda: os.write(1, stream.getvalue()) and not stream.truncate(0) and stream.seek(0)
-sys.stdout.write = stream.write
 
+class FastI:
+    stream = StringIO()
+    newlines = 0
+
+    def readline(self):
+        while self.newlines == 0:
+            b, ptr = os.read(0, (1 << 13) + os.fstat(0).st_size), self.stream.tell()
+            self.stream.seek(0, 2), self.stream.write(b), self.stream.seek(ptr)
+            self.newlines += b.count('\n') + (not b)
+
+        self.newlines -= 1
+        return self.stream.readline()
+
+
+class FastO(IOBase):
+    def __init__(self):
+        stream = StringIO()
+        self.flush = lambda: os.write(1, stream.getvalue()) and not stream.truncate(0) and stream.seek(0)
+        self.write = stream.write
+
+
+class ostream:
+    def __lshift__(self, a):
+        if a == endl:
+            sys.stdout.write('\n')
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(str(a))
+        return self
+
+
+sys.stdin, sys.stdout = FastI(), FastO()
 input, flush = sys.stdin.readline, sys.stdout.flush
-input = StringIO(os.read(0, os.fstat(0).st_size)).readline
+cout, endl = ostream(), object()
 
 
 def main():
--- template/lite3.py
+++ template/lite3.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python3
 import os
 import sys
-from io import BytesIO, IOBase
+from io import BytesIO
 
-sys.stdout, stream = IOBase(), BytesIO()
-sys.stdout.flush = lambda: os.write(1, stream.getvalue()) and not stream.truncate(0) and stream.seek(0)
-sys.stdout.write = lambda s: stream.write(s.encode())
 
-input = BytesIO(os.read(0, os.fstat(0).st_size)).readline
+class FastI:
+    stream = BytesIO()
+    newlines = 0
+
+    def readline(self):
+        while self.newlines == 0:
+            b, ptr = os.read(0, (1 << 13) + os.fstat(0).st_size), self.stream.tell()
+            self.stream.seek(0, 2), self.stream.write(b), self.stream.seek(ptr)
+            self.newlines += b.count(b'\n') + (not b)
+
+        self.newlines -= 1
+        return self.stream.readline()
+
+
+class FastO:
+    def __init__(self):
+        stream = BytesIO()
+        self.flush = lambda: os.write(1, stream.getvalue()) and not stream.truncate(0) and stream.seek(0)
+        self.write = lambda b: stream.write(b.encode())
+
+
+class ostream:
+    def __lshift__(self, a):
+        if a == endl:
+            sys.stdout.write('\n')
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(str(a))
+        return self
+
+
+sys.stdin, sys.stdout = FastI(), FastO()
+input, flush = sys.stdin.readline, sys.stdout.flush
+cout, endl = ostream(), object()
 
 
 def main():
