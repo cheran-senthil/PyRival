cb0cd6c2d4fe496d107b5059cc657acb2e0ebbdc
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-12 21:36:03 +0530

    Add print function

--- snippets/Data Structures/FastIO.py
+++ snippets/Data Structures/FastIO.py
@@ -1,39 +1,42 @@
 import os
 import sys
-from io import BytesIO, IOBase
+from builtins import str as __str__
+from io import BytesIO
+
+str = lambda x=b'': x if type(x) is bytes else __str__(x).encode()
 
 BUFSIZE = 8192
 
 
-class FastIO(IOBase):
-    stream = BytesIO()
+class FastIO:
     newlines = 0
 
-    def __init__(self, fd):
-        self.fd = fd
-        self.flush = lambda: os.write(fd, self.stream.getvalue()) and not self.stream.truncate(0) and self.stream.seek(0)
-        self.write = self.stream.write if sys.version_info[0] < 3 else lambda b: self.stream.write(b.encode())
+    def __init__(self, file):
+        self._buffer = BytesIO()
+        self._fd = file.fileno()
+        self.flush = lambda: os.write(self._fd, self._buffer.getvalue()) and not self._buffer.truncate(0) and self._buffer.seek(0)
+        self.write = self._buffer.write
 
     def readline(self):
         while self.newlines == 0:
-            b, ptr = os.read(self.fd, max(os.fstat(self.fd).st_size, BUFSIZE)), self.stream.tell()
-            self.stream.seek(0, 2), self.stream.write(b), self.stream.seek(ptr)
+            b, ptr = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE)), self._buffer.tell()
+            self._buffer.seek(0, 2), self._buffer.write(b), self._buffer.seek(ptr)
             self.newlines += b.count(b'\n') + (not b)
-
         self.newlines -= 1
-        return self.stream.readline()
+        return self._buffer.readline()
 
 
-class ostream:
-    def __lshift__(self, a):
-        if a == endl:
-            sys.stdout.write('\n')
-            sys.stdout.flush()
-        else:
-            sys.stdout.write(str(a))
-        return self
+sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip(b'\r\n')
 
 
-sys.stdin, sys.stdout = FastIO(0), FastIO(1)
-input, flush = sys.stdin.readline, sys.stdout.flush
-cout, endl = ostream(), object()
+def print(*args, sep=b' ', end=b'\n', file=sys.stdout, flush=False):
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(end)
+    if flush:
+        file.flush()
--- template/lite3.py
+++ template/lite3.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python3
 import os
 import sys
+from builtins import str as __str__
 from io import BytesIO
+
+str = lambda x=b'': x if type(x) is bytes else __str__(x).encode()
 
 BUFSIZE = 8192
 
 
 class FastIO:
-    stream = BytesIO()
     newlines = 0
 
-    def __init__(self, fd):
-        self.fd = fd
-        self.flush = lambda: os.write(fd, self.stream.getvalue()) and not self.stream.truncate(0) and self.stream.seek(0)
-        self.write = lambda b: self.stream.write(b.encode())
+    def __init__(self, file):
+        self._buffer = BytesIO()
+        self._fd = file.fileno()
+        self.flush = lambda: os.write(self._fd, self._buffer.getvalue()) and not self._buffer.truncate(0) and self._buffer.seek(0)
+        self.write = self._buffer.write
 
     def readline(self):
         while self.newlines == 0:
-            b, ptr = os.read(self.fd, max(os.fstat(self.fd).st_size, BUFSIZE)), self.stream.tell()
-            self.stream.seek(0, 2), self.stream.write(b), self.stream.seek(ptr)
+            b, ptr = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE)), self._buffer.tell()
+            self._buffer.seek(0, 2), self._buffer.write(b), self._buffer.seek(ptr)
             self.newlines += b.count(b'\n') + (not b)
-
         self.newlines -= 1
-        return self.stream.readline()
+        return self._buffer.readline()
 
 
-sys.stdin, sys.stdout = FastIO(0), FastIO(1)
-input = sys.stdin.readline
+sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip(b'\r\n')
+
+
+def print(*args, sep=b' ', end=b'\n', file=sys.stdout, flush=False):
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(end)
+    if flush:
+        file.flush()
 
 
 def main():
