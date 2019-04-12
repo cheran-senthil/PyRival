58a186a3f80f3a8a5b5c07c138fcdefdc334bb1a
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-12 21:36:18 +0530

    Move ostream to separate file

---
+++ snippets/Data Structures/ostream.py
@@ -0,0 +1,14 @@
+import sys
+
+
+class ostream:
+    def __lshift__(self, a):
+        if a == endl:
+            sys.stdout.write('\n')
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(str(a))
+        return self
+
+
+cout, endl = ostream(), object()
