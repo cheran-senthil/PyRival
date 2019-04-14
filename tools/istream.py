8a0bcbcea4492d422131b287a356329e52c4a843
Cheran Senthil <cheran.v.senthil@gmail.com>
2019-04-14 23:40:07 +0530

    reorder loop

--- tools/istream.py
+++ tools/istream.py
@@ -2,15 +2,13 @@ import sys
 
 istream = []
 i, b = 0, sys.stdin.read()
-while True:
-    while i < len(b) and b[i].isspace():
-        i += 1
-    if i == len(b):
-        break
+while i < len(b):
     j = i
     while i < len(b) and not b[i].isspace():
         i += 1
     istream.append(int(b[j:i]))
+    while i < len(b) and b[i].isspace():
+        i += 1
 
 istream.reverse()
 getint = istream.pop
