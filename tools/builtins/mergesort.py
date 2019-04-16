643b6c414ca4211e94df4ab87e7502a3eb22b51a
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-16 19:20:15 +0530

    Improve mergesort

--- tools/builtins/mergesort.py
+++ tools/builtins/mergesort.py
@@ -1,17 +1,38 @@
-def mergesort(A):
+def mergesort(A, key=lambda x: x, reverse=False):
     B, n = A[:], len(A)
-    width = 1
+
+    for i in range(0, n - 1, 2):
+        if key(A[i]) > key(A[i ^ 1]):
+            A[i], A[i ^ 1] = A[i ^ 1], A[i]
+
+    width = 2
     while width < n:
         for i in range(0, n, 2 * width):
-            right, end = min(i + width, n), min(i + 2 * width, n)
-            j = right
-            for k in range(i, end):
-                if i < right and (j >= end or A[i] <= A[j]):
-                    B[k] = A[i]
-                    i += 1
-                else:
+            R1, R2 = min(i + width, n), min(i + 2 * width, n)
+            j, k = R1, i
+
+            while i < R1 and j < R2:
+                if key(A[i]) > key(A[j]):
                     B[k] = A[j]
                     j += 1
-        A = B[:]
+                else:
+                    B[k] = A[i]
+                    i += 1
+                k += 1
+
+            while i < R1:
+                B[k] = A[i]
+                k += 1
+                i += 1
+
+            while k < R2:
+                B[k] = A[k]
+                k += 1
+
+        A, B = B, A
         width *= 2
+
+    if reverse:
+        A.reverse()
+
     return A
