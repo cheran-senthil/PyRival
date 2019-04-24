0fe1dae596b9238e0371c80706b57cd28fb3a4b1
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-25 03:33:11 +0530

    speed up factors

--- snippets/algebra/factors.py
+++ snippets/algebra/factors.py
@@ -1,17 +1,11 @@
-from functools import reduce
-
-
-def memodict(f):
-    """ Memoization decorator for a function taking a single argument. """
-
-    class memodict(dict):
-        def __missing__(self, key):
-            ret = self[key] = f(key)
-            return ret
-
-    return memodict().__getitem__
-
-
-@memodict
 def factors(n):
-    return set(reduce(list.__add__, ([i, n // i] for i in range(1, int(n**0.5) + 1, 2 if n & 1 else 1) if n % i == 0)))
+    small, large = [], []
+    for i in range(1, int(n ** 0.5) + 1, 2 if n & 1 else 1):
+        if not n % i:
+            small.append(i)
+            large.append(n // i)
+    if small[-1] == large[-1]:
+        large.pop()
+    large.reverse()
+    small.extend(large)
+    return small
