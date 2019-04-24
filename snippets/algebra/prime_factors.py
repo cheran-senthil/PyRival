2bc1d35ac3c0de3019db262d18433599b8bf9132
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-25 04:05:33 +0530

    embed Miller-Rabin

--- snippets/algebra/prime_factors.py
+++ snippets/algebra/prime_factors.py
@@ -13,34 +13,31 @@ def memodict(f):
     return memodict().__getitem__
 
 
-def is_prime(n):
-    """Deterministic variant of the Miller-Rabin primality test"""
-    if n < 2 or n % 6 % 4 != 1:
-        return n - 2 < 2
+def pollard_rho(n):
+    if not n & 1:
+        return 2
+
     s = ((n - 1) & (1 - n)).bit_length() - 1
     d = n >> s
     for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
         p, i = pow(a, d, n), s
         while p != 1 and p != n - 1 and a % n and i:
             i -= 1
-            p = (p * p) % n
+            q = (p * p) % n
+            if q == 1:
+                return gcd(p - 1, n)
+            p = q
         if p != n - 1 and i != s:
-            return False
-    return True
-
-
-def pollard_rho(n):
-    if not n & 1:
-        return 2
-    for i in range(2, n):
-        x, y = i, (i * i + 1) % n
-        f = gcd(abs(x - y), n)
-        while f == 1:
-            x, y = (x * x + 1) % n, (y * y + 1) % n
-            y = (y * y + 1) % n
-            f = gcd(abs(x - y), n)
-        if f != n:
-            return f
+            for i in range(2, n):
+                x, y = i, (i * i + 1) % n
+                f = gcd(abs(x - y), n)
+                while f == 1:
+                    x, y = (x * x + 1) % n, (y * y + 1) % n
+                    y = (y * y + 1) % n
+                    f = gcd(abs(x - y), n)
+                if f != n:
+                    return f
+    return n
 
 
 @memodict
@@ -48,7 +45,5 @@ def prime_factors(n):
     """Prime factorization using Pollard's rho algorithm"""
     if n <= 1:
         return Counter()
-    if is_prime(n):
-        return Counter([n])
     f = pollard_rho(n)
-    return prime_factors(f) + prime_factors(n // f)
+    return Counter([n]) if f == n else prime_factors(f) + prime_factors(n // f)
