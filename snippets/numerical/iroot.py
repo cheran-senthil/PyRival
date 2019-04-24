9d49d1aeb769536087d0a573ae693e5070000566
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-24 11:31:01 +0530

    bug fix

--- snippets/numerical/iroot.py
+++ snippets/numerical/iroot.py
@@ -1,12 +1,11 @@
-def iroot(a, n=2):
-    if a < 2:
-        return a
-
-    c = 1
-    d = ((n - 1) * c + a // (c**(n - 1))) // n
-    e = ((n - 1) * d + a // (d**(n - 1))) // n
-
-    while (c != d) and (c != e):
-        c, d, e = d, e, ((n - 1) * e + a // (e**(n - 1))) // n
-
-    return min(d, e)
+def iroot(n, k=2):
+    if n == 0:
+        return 0
+    if n < 0:
+        return -iroot(-n, k) if k & 1 else None
+    u, s = n, n + 1
+    while u < s:
+        s = u
+        t = (k - 1) * s + n // s ** (k - 1)
+        u = t // k
+    return s
