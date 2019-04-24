5ae300b08af25946924125dfd0f14444277b80a8
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-24 10:31:08 +0530

    Remove newlines

--- snippets/algebra/prime_factors.py
+++ snippets/algebra/prime_factors.py
@@ -10,9 +10,11 @@ class Random(_random.Random):
             j = int(self.random() * (i + 1))
             x[i], x[j] = x[j], x[i]
 
-    randrange = lambda self, a, b, step=1: a + step * int(self.random() * ((b - a + step + [1, -1][step < 0]) // step))
     randint = lambda self, a, b: a + int(self.random() * (b - a + 1))
     choice = lambda self, seq: seq[int(self.random() * len(seq))]
+    randrange = lambda self, a, b, step=1: a + step * int(
+        self.random() * ((b - a + step + [1, -1][step < 0]) // step)
+    )
 
 
 randint = Random().randint
@@ -47,7 +49,6 @@ def pollard_rho(n):
         if p != n - 1 and i != d:
             flag = False
             break
-
     if flag:
         return Counter([n])
 
@@ -56,20 +57,15 @@ def pollard_rho(n):
 
     while g == 1:
         x, k = y, 0
-
         for _ in range(r):
             y = (y * y + c) % n
-
         while k < r and g == 1:
             ys = y
-
             for _ in range(min(m, r - k)):
                 y = (y * y + c) % n
                 q = (q * abs(x - y)) % n
-
             g = gcd(q, n)
             k += m
-
         r *= 2
 
     if g == n:
@@ -86,12 +82,10 @@ def pollard_rho(n):
 def prime_factors(n):
     """ Prime factorization using Pollard's rho algorithm. """
     factors = Counter()
-
     for p in [2, 3, 5, 13, 19, 73, 193, 407521, 299210837]:
         if n % p == 0:
             cnt = 0
             while n % p == 0:
                 n, cnt = n // p, cnt + 1
             factors[p] = cnt
-
     return factors + pollard_rho(n)
