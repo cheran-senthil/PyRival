df51bcf531a8c37a93b4b23cf36cd9a974479c1f
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-25 03:02:36 +0530

    Simplify miller-rabin

--- snippets/algebra/is_prime.py
+++ snippets/algebra/is_prime.py
@@ -1,22 +1,14 @@
 def is_prime(n):
     """ Deterministic variant of the Miller-Rabin primality test. """
-    if n <= 1:
-        return False
-
-    for p in [2, 3, 5, 13, 19, 73, 193, 407521, 299210837]:
-        if n % p == 0:
-            return n == p
-
-    d = n - 1
-    while not d & 1:
-        d >>= 1
-
+    if n < 2 or n % 6 % 4 != 1:
+        return n - 2 < 2
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
     for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
-        p, i = pow(a, d, n), d
-        while p != 1 and p != n - 1 and i != n - 1:
-            i <<= 1
+        p, i = pow(a, d, n), s
+        while p != 1 and p != n - 1 and a % n and i:
+            i -= 1
             p = (p * p) % n
-        if p != n - 1 and i != d:
+        if p != n - 1 and i != s:
             return False
-
     return True
