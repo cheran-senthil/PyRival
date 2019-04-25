6da0d4c089286c9a7262ace71cc37e5e73e7f87b
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-26 02:28:41 +0530

    Remove some blank lines

--- tools/fmod.py
+++ tools/fmod.py
@@ -15,12 +15,10 @@ fmul = lambda a, b, c=0.0: fmod(fmod(a * SHRT) * fround(SHRT_INV * b) + a * (b -
 def fpow(x, y):
     if y == 0:
         return 1.0
-
     res = 1.0
     while y > 1:
         if y & 1 == 1:
             res = fmul(res, x)
         x = fmul(x, x)
         y >>= 1
-
     return fmul(res, x)
