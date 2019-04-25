4527e5da4ea4b06cff8711c7de87a00c2c2d43ce
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-26 02:28:51 +0530

    Simplify discrete log

--- snippets/algebra/discrete_log.py
+++ snippets/algebra/discrete_log.py
@@ -1,61 +1,19 @@
-from math import gcd
-
-
-def egcd(a, m):
-    if a == 0:
-        return (m, 0, 1)
-
-    g, y, x = egcd(m % a, a)
-    return (g, x - (m // a) * y, y)
-
-
-def mod_inv(a, m):
-    amodm = a % m
-    g, x, _ = egcd(amodm, m)
-    """Sanity Check
-    if g != 1:
-        return None
-    """
-    return x % m
-
-
 def discrete_log(a, b, m):
-    if a == 0:
-        return 1
+    a %= m
+    b %= m
+    if b == 1:
+        return 0
 
-    g, x = gcd(a, m), 0
-    while 1 not in [a, b, m, g]:
-        m //= g
-        b = (b // g) * mod_inv(a // g, m)
-        g = gcd(a, m)
-        x += 1
-
-    if 1 in [a, b, m]:
-        return x
-
-    n = int(m**0.5) + 1
-
-    an = 1
+    n, vals = int(m ** 0.5) + 1, dict()
+    mult, cur = pow(a, n, m), 1
     for i in range(n):
-        an = (an * a) % m
-
-    vals = dict()
-
-    cur = an
-    for i in range(1, n + 1):
-        if cur not in vals:
-            vals[cur] = i
-        cur = (cur * an) % m
+        cur = (cur * mult) % m
+        vals[cur] = i + 1
 
     cur = b
     for i in range(n + 1):
-        try:
-            ans = vals[cur] * n - i
-            if ans < m:
-                return x + ans
-        except KeyError:
-            pass
-
+        it = vals.get(cur, None)
+        if it is not None:
+            return it * n - i  # % phi(m)
         cur = (cur * a) % m
-
     return -1
