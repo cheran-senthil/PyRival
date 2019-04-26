3b001bfc252cfb12b149fde7c8dffd0a3b107b51
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-26 16:18:22 +0530

    separate sieve and prime list

--- snippets/algebra/sieve.py
+++ snippets/algebra/sieve.py
@@ -1,8 +1,7 @@
-def get_primes(n):
-    """ Input n>=6, Returns a generator of primes, 5 <= p < n """
+def prime_sieve(n):
+    """returns a sieve of primes >= 5 and < n"""
     flag = n % 6 == 2
     sieve = bytearray((n // 3 + flag >> 3) + 1)
-
     for i in range(1, int(n**0.5) // 3 + 1):
         if not (sieve[i >> 3] >> (i & 7)) & 1:
             k = (3 * i + 1) | 1
@@ -10,5 +9,17 @@ def get_primes(n):
                 sieve[j >> 3] |= 1 << (j & 7)
             for j in range(k * (k - 2 * (i & 1) + 4) // 3, n // 3 + flag, 2 * k):
                 sieve[j >> 3] |= 1 << (j & 7)
+    return sieve
 
-    return (3 * i + 1 | 1 for i in range(1, n // 3 + flag) if not (sieve[i >> 3] >> (i & 7)) & 1)
+
+def prime_list(n):
+    """returns a list of primes <= n"""
+    res = []
+    if n > 1:
+        res.append(2)
+    if n > 2:
+        res.append(3)
+    if n > 4:
+        sieve = prime_sieve(n + 1)
+        res.extend(3 * i + 1 | 1 for i in range(1, (n + 1) // 3 + (n % 6 == 1)) if not (sieve[i >> 3] >> (i & 7)) & 1)
+    return res
