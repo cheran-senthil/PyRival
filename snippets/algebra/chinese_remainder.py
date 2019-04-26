ffce7c66cbc19d64ea416a1fef0599899f9ef239
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-26 14:40:47 +0530

    simplify code

--- snippets/algebra/chinese_remainder.py
+++ snippets/algebra/chinese_remainder.py
@@ -2,44 +2,34 @@ import operator as op
 from functools import reduce
 
 
-def chinese_remainder(a, n):
-    # solve for x = a_i (mod n_i) where n_i is prime
-    x = 0
-    prod = reduce(op.mul, n)
-
-    for a_i, n_i in zip(a, n):
-        p = prod // n_i
-        x += a_i * pow(p, n_i - 2, n_i) * p
-
-    return x % prod
+def chinese_remainder(a, p):
+    """returns x s.t. x = a[i] (mod p[i]) where p[i] is prime for all i"""
+    prod = reduce(op.mul, p, 1)
+    x = [prod // pi for pi in p]
+    return sum(a[i] * pow(x[i], p[i] - 2, p[i]) * x[i] for i in range(len(a))) % prod
 
 
-def egcd(a, m):
-    if a == 0:
-        return (m, 0, 1)
-
-    g, y, x = egcd(m % a, a)
-    return (g, x - (m // a) * y, y)
+def extended_gcd(a, b):
+    """returns gcd(a, b), s, r s.t. a * s + b * r == gcd(a, b)"""
+    s, old_s = 0, 1
+    r, old_r = b, a
+    while r:
+        q = old_r // r
+        old_r, r = r, old_r - q * r
+        old_s, s = s, old_s - q * s
+    return old_r, old_s, (old_r - old_s * a) // b
 
 
 def modinv(a, m):
-    g, x, _ = egcd(a % m, m)
-    """Sanity Check
-    if g != 1:
-        return None
-    """
-    return x % m
+    """returns the modular inverse of a w.r.t. to m"""
+    g, x, _ = extended_gcd(a % m, m)
+    return x % m if g == 1 else None
 
 
 def composite_crt(b, m):
-    # solve for x = b_i (mod m_i)
-    x = 0
-    m_prod = 1
-
+    """returns x s.t. x = a[i] (mod m[i]) for all i"""
+    x, m_prod = 0, 1
     for bi, mi in zip(b, m):
-        tot = x
-        tmp = (modinv(m_prod, mi) * (bi - tot)) % mi
-        x += tmp * m_prod
+        x += ((modinv(m_prod, mi) * (bi - x)) % mi) * m_prod
         m_prod *= mi
-
     return x % m_prod
