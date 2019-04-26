43ebffddca960350dda660300817b48197d2d085
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-26 13:54:18 +0530

    minor style changes

--- snippets/linear_algebra/multivariable_crt.py
+++ snippets/linear_algebra/multivariable_crt.py
@@ -6,55 +6,47 @@ mat_mul = lambda A, B: [[sum(i * j for i, j in zip(row, col)) for col in zip(*B)
 
 
 def egcd(a, m):
-    """ Extended GCD """
-    if a == 0:
-        return (m, 0, 1)
-
+    """extended gcd"""
+    if not a:
+        return m, 0, 1
     g, y, x = egcd(m % a, a)
-    return (g, x - (m // a) * y, y)
+    return g, x - (m // a) * y, y
 
 
 def modinv(a, m):
-    """ Find Modular Inverse """
+    """returns the modular inverse of a w.r.t. to m"""
     amodm = a % m
     g, x, _ = egcd(amodm, m)
-    """
-    if g != 1:
-        return None
-    """
-    return x % m
+    return x % m if g == 1 else None
 
 
 def pivot(A, m):
-    """ Finds the pivot of A and m """
+    """returns the pivot of A and m"""
     result = [0] * len(A)
     for i, Ai in enumerate(A):
         for j, Aij in enumerate(Ai):
             if gcd(Aij, m[i]) == 1:
                 result[i] = j
-
     return result
 
 
 def is_sol(A, x, b, m):
-    """ Checks if Ax = b mod m """
+    """checks if Ax = b mod m"""
     ax_b = mat_sub(mat_mul(A, x), b)
     for i, mod in enumerate(m):
-        if ax_b[i] % mod != 0:
+        if ax_b[i] % mod:
             return False
     return True
 
 
 def mcrt(A, b, m):
-    """ Returns for x in Ax = b mod m """
+    """returns x s.t. Ax = b mod m"""
     piv = pivot(A, m)
     x = [0] * len(A)
     m_prod = 1
-
     for i, Ai in enumerate(A):
         tot = sum(Ai[k] * x[k] for k in range(len(A)))
         tmp = (modinv(m_prod * Ai[piv[i]], m[i]) * (b[i] - tot)) % m[i]
         x[piv[i]] += tmp * m_prod
         m_prod *= m[i]
-
     return x
