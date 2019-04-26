bd896dfca34d89d79074a64dc6a50ea7350f16f1
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-26 16:06:03 +0530

    Faster primitive root

--- snippets/algebra/primitive_root.py
+++ snippets/algebra/primitive_root.py
@@ -1,64 +1,81 @@
 from collections import Counter
+from math import gcd
+
+
+def memodict(f):
+    """memoization decorator for a function taking a single argument"""
+
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
 
 
 def is_prime(n):
-    if n in [2, 3, 5, 13, 19, 73, 193, 407521, 299210837]:
-        return True
-
-    if (n in [0, 1]) or (any((n % p) == 0 for p in [2, 3, 5, 13, 19, 73, 193, 407521, 299210837])):
-        return False
-
-    d, s = n - 1, 0
-    while not d % 2:
-        d, s = d >> 1, s + 1
-
-    def try_composite(a):
-        if pow(a, d, n) == 1:
+    """returns True if n is prime else False"""
+    if n < 2 or n % 6 % 4 != 1:
+        return n - 2 < 2
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        p, i = pow(a, d, n), s
+        while p != 1 and p != n - 1 and a % n and i:
+            i -= 1
+            p = (p * p) % n
+        if p != n - 1 and i != s:
             return False
-        for i in range(s):
-            if pow(a, 2**i * d, n) == n - 1:
-                return False
-        return True
-
-    return not any(try_composite(w) for w in [2, 325, 9375, 28178, 450775, 9780504, 1795265022])
+    return True
 
 
-def factors(n):
-    prime_factors = Counter()
+def pollard_rho(n):
+    """returns a random factor of n"""
+    if not n & 1:
+        return 2
 
-    def ilog(n, p):
-        cnt = 0
-        while not n % p:
-            n, cnt = n // p, cnt + 1
-        return n, cnt
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        p, i = pow(a, d, n), s
+        while p != 1 and p != n - 1 and a % n and i:
+            i -= 1
+            q = (p * p) % n
+            if q == 1:
+                return gcd(p - 1, n)
+            p = q
+        if p != n - 1 and i != s:
+            for i in range(2, n):
+                x, y = i, (i * i + 1) % n
+                f = gcd(abs(x - y), n)
+                while f == 1:
+                    x, y = (x * x + 1) % n, (y * y + 1) % n
+                    y = (y * y + 1) % n
+                    f = gcd(abs(x - y), n)
+                if f != n:
+                    return f
+    return n
 
-    if n % 2 == 0:
-        n, prime_factors[2] = ilog(n, 2)
-    if n % 3 == 0:
-        n, prime_factors[3] = ilog(n, 3)
 
-    i = 5
-    while i * i <= n:
-        if n % i == 0:
-            n, prime_factors[i] = ilog(n, i)
-        i += 2 if i % 3 == 2 else 4
-
-    if n > 1:
-        prime_factors[n] = 1
-    return prime_factors
+@memodict
+def prime_factors(n):
+    """returns a Counter of the prime factorization of n"""
+    if n <= 1:
+        return Counter()
+    f = pollard_rho(n)
+    return Counter([n]) if f == n else prime_factors(f) + prime_factors(n // f)
 
 
-def perfect_base(n):
-    b = n.bit_length()
-    for a in range(b, 0, -1):
-        lo, hi = 1, 1 << (b // a + 1)
+def ilog(n):
+    """returns the smallest a, b s.t. a**b = n for integer a, b"""
+    a = n.bit_length()
+    for b in range(a, 0, -1):
+        lo, hi = 1, 1 << (a // b + 1)
         while lo < hi:
             mi = (lo + hi) // 2
-
-            a_b = mi**a
+            a_b = mi**b
             if a_b == n:
-                return mi
-
+                return mi, b
             if a_b > n:
                 hi = mi
             else:
@@ -66,17 +83,13 @@ def perfect_base(n):
 
 
 def primitive_root(p):
+    """returns a primitive root of p"""
     if p <= 4:
         return p - 1
-    if -p & p == p:
-        return None
-
-    factor = perfect_base(p if p & 1 else p >> 1)
-    if not is_prime(factor):
-        return None
-
+    assert -p & p == p
+    factor, _ = ilog(p if p & 1 else p >> 1)
+    assert not is_prime(factor)
     phi = (factor - 1) * ((p if p & 1 else p >> 1) // factor)
-
     for res in range(2 + (1 ^ (p & 1)), factor, 1 + (1 ^ (p & 1))):
-        if all(pow(res, phi // i, p) != 1 for i in factors(phi).keys()):
+        if all(pow(res, phi // i, p) != 1 for i in prime_factors(phi)):
             return res
