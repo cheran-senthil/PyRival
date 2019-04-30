2e352b4e848729151494cb5f2546ffabb8870f1f
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-30 14:55:21 +0530

    Improved variable names and iteration

--- snippets/data_structures/Treap.py
+++ snippets/data_structures/Treap.py
@@ -2,32 +2,47 @@ import random
 
 left_child = [0]
 right_child = [0]
-keys = [0]
-priority = [0]
+_keys = [0]
+_prior = [0.0]
 
 
 def create_node(key):
-    keys.append(key)
-    priority.append(random.random())
+    _keys.append(key)
+    _prior.append(random.random())
     left_child.append(0)
     right_child.append(0)
-    return len(keys) - 1
+    return len(_keys) - 1
 
 
 def split(root, key):
-    tmp = [0, 0]
-    wherel, wherer, posl, posr = tmp, tmp, 0, 1
+    left_pos = right_pos = 0
     while root:
-        if key < keys[root]:
-            wherer[posr] = root
-            wherer, posr = left_child, root
+        if key < _keys[root]:
+            left_child[right_pos] = right_pos = root
             root = left_child[root]
         else:
-            wherel[posl] = root
-            wherel, posl = right_child, root
+            right_child[left_pos] = left_pos = root
             root = right_child[root]
-    wherel[posl] = wherer[posr] = 0
-    return tmp
+    left, right = right_child[0], left_child[0]
+    right_child[left_pos] = left_child[right_pos] = right_child[0] = left_child[0] = 0
+    return left, right
+
+
+def merge(left, right):
+    where, pos = left_child, 0
+    while left and right:
+        if _prior[left] > _prior[right]:
+            where[pos] = pos = left
+            where = right_child
+            left = right_child[left]
+        else:
+            where[pos] = pos = right
+            where = left_child
+            right = left_child[right]
+    where[pos] = left or right
+    node = left_child[0]
+    left_child[0] = 0
+    return node
 
 
 def insert(root, key):
@@ -35,29 +50,13 @@ def insert(root, key):
     return merge(merge(left, create_node(key)), right)
 
 
-def merge(left, right):
-    tmp = [0]
-    where, pos = tmp, 0
-    while left and right:
-        if priority[left] > priority[right]:
-            where[pos] = left
-            where, pos = right_child, left
-            left = right_child[left]
-        else:
-            where[pos] = right
-            where, pos = left_child, right
-            right = left_child[right]
-    where[pos] = left or right
-    return tmp[0]
-
-
 def erase(root, key):
-    if keys[root] == key:
+    if _keys[root] == key:
         return merge(left_child[root], right_child[root])
     node = root
-    while keys[root] != key:
+    while _keys[root] != key:
         parent = root
-        root = left_child[root] if key < keys[root] else right_child[root]
+        root = left_child[root] if key < _keys[root] else right_child[root]
     if root == left_child[parent]:
         left_child[parent] = merge(left_child[root], right_child[root])
     else:
@@ -66,24 +65,25 @@ def erase(root, key):
 
 
 def lower_bound(root, key):
-    while root and keys[root] < key:
+    while root and _keys[root] < key:
         root = right_child[root]
     if not root:
         return 0
-    max_node = root
-    max_key = keys[root]
+    min_node = root
+    min_key = _keys[root]
     while root:
-        if keys[root] < key:
+        if _keys[root] < key:
             root = right_child[root]
         else:
-            if keys[root] < max_key:
-                max_key = keys[root]
-                max_node = root
+            if _keys[root] < min_key:
+                min_key = _keys[root]
+                min_node = root
             root = left_child[root]
-    return max_node
+    return min_node
 
 
 values = sorted()
+node_cnt = 1
 
 
 def build(begin, end):
@@ -93,8 +93,8 @@ def build(begin, end):
     mid = (begin + end) // 2
     root = node_cnt
     node_cnt += 1
-    priority[root] = random.random()
-    keys[root] = values[mid]
+    _prior[root] = random.random()
+    _keys[root] = values[mid]
     left_child[root] = build(begin, mid)
     right_child[root] = build(mid + 1, end)
     return root
