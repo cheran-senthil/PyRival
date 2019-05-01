b1dda661ef6186c89dbd65169f09dadbf13f1164
cheran-senthil <cheran.v.senthil@gmail.com>
2019-05-01 16:54:58 +0530

    Add Segment Tree

--- snippets/data_structures/SegmentTree.py
+++ snippets/data_structures/SegmentTree.py
@@ -1,92 +1,84 @@
 class SegmentTree:
-    def __init__(self, data):
-        """maximum segment tree"""
-        self.m = m = 1 << (len(data) - 1).bit_length()
-        self.data = [0] * (2 * m)
-        self.data[m:m + len(data)] = data
-        for i in reversed(range(m)):
-            self.data[i] = max(self.data[2 * i], self.data[2 * i + 1])
-        self.query = [0] * (2 * m)
+    def __init__(self, data, default=0, func=lambda x, y: max(x, y)):
+        """initialize the segment tree with data"""
+        self._len = _len = len(data)
+        self._size = _size = 1 << (_len - 1).bit_length()
+        self._data = _data = [default] * (2 * _size)
+        self._default = default
+        self._func = func
+        _data[_size:_size + _len] = data
+        for i in reversed(range(_size)):
+            _data[i] = func(_data[2 * i], _data[2 * i + 1])
 
-    def push(self, seg_ind):
-        """push the query on seg_ind to its children"""
-        # Let the children know of the queries
-        q = self.query[seg_ind]
+    def __delitem__(self, key):
+        self[key] = self._default
 
-        self.query[2 * seg_ind] += q
-        self.query[2 * seg_ind + 1] += q
+    def __getitem__(self, key):
+        return self._data[key + self._size]
 
-        self.data[2 * seg_ind] += q
-        self.data[2 * seg_ind + 1] += q
+    def __setitem__(self, key, value):
+        key += self._size
+        self._data[key] = value
+        key >>= 1
+        while key:
+            self._data[key] = self._func(self._data[2 * key], self._data[2 * key + 1])
+            key >>= 1
 
-        # Remove queries from seg_ind
-        self.data[seg_ind] = max(self.data[2 * seg_ind], self.data[2 * seg_ind + 1])
-        self.query[seg_ind] = 0
+    def __len__(self):
+        return self._len
 
-    def update(self, seg_ind):
-        """udates the node seg_ind to know of all queries applied to it via its ancestors"""
-        # Find all indecies to be updated
-        seg_ind >>= 1
-        inds = []
-        while seg_ind > 0:
-            inds.append(seg_ind)
-            seg_ind >>= 1
+    def bisect_left(self, value):
+        i = 1
+        while i < self._size:
+            i = 2 * i + 1 if value > self._data[2 * i] else 2 * i
+        return i - self._size
 
-        # Push the queries down the segment tree
-        for ind in reversed(inds):
-            self.push(ind)
+    def bisect_right(self, value):
+        i = 1
+        while i < self._size:
+            i = 2 * i + 1 if value >= self._data[2 * i] else 2 * i
+        return i - self._size
 
-    def build(self, seg_ind):
-        """make the changes to seg_ind be known to its ancestors"""
-        seg_ind >>= 1
-        while seg_ind > 0:
-            self.data[seg_ind] = max(self.data[2 * seg_ind], self.data[2 * seg_ind + 1]) + self.query[seg_ind]
-            seg_ind >>= 1
+    bisect = bisect_right
 
-    def add(self, start, end, value):
-        """lazily add value to [start, end)"""
-        start += self.m
-        end += self.m
-        _start, _end = start, end
+    def query(self, begin, end):
+        begin += self._size
+        end += self._size
+        res = self._default
 
-        while _start < _end:
-            if _start & 1:
-                self.query[_start] += value
-                self.data[_start] += value
-                _start += 1
-
-            if _end & 1:
-                _end -= 1
-                self.query[_end] += value
-                self.data[_end] += value
-
-            _start >>= 1
-            _end >>= 1
-
-        # Tell all nodes above of the updated area of the updates
-        self.build(start)
-        self.build(end - 1)
-
-    def maxi(self, start, end):
-        """max of data[start, end)"""
-        start += self.m
-        end += self.m
-
-        # Apply all the lazily stored queries
-        self.update(start)
-        self.update(end - 1)
-        segs = []
-
-        while start < end:
-            if start & 1:
-                segs.append(start)
-                start += 1
-
+        while begin < end:
+            if begin & 1:
+                res = self._func(res, self._data[begin])
+                begin += 1
             if end & 1:
                 end -= 1
-                segs.append(end)
-
-            start >>= 1
+                res = self._func(res, self._data[end])
+            begin >>= 1
             end >>= 1
 
-        return max(self.data[i] for i in segs)
+        return res
+
+    def __repr__(self):
+        def recursive_repr(i):
+            if i >= self._size:
+                return [str(self._data[i])]
+
+            left = recursive_repr(2 * i)
+            right = recursive_repr(2 * i + 1)
+            lines = ['{}   {}'.format(l, r) for l, r in zip(left, right)]
+
+            width = len(lines[0])
+            left_width = len(left[0]) // 2
+            right_width = len(right[0]) // 2
+            stem_width = width - left_width - right_width - 2
+
+            branches = ' ' * left_width + '/' + ' ' * stem_width + '\\' + ' ' * right_width
+            stem = [' '] * (left_width + 1) + ['_'] * stem_width + [' '] * (right_width + 1)
+            stem[width // 2] = '^'
+
+            lines.append(branches)
+            lines.append(''.join(stem))
+            lines.append(str(self._data[i]).center(width))
+            return lines
+
+        return '\n'.join(reversed(recursive_repr(1)))
