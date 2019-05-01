091afbc0bd8af3da31618b274150892d9ce8a204
cheran-senthil <cheran.v.senthil@gmail.com>
2019-05-01 16:55:19 +0530

    Remove Binary Node

--- snippets/data_structures/Node.py
+++ snippets/data_structures/Node.py
@@ -8,10 +8,3 @@ class Node(object):
         if not self:
             return '%s()' % (self.__class__.__name__, )
         return '%s(%r)' % (self.__class__.__name__, self.value)
-
-
-class BinaryNode():
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
