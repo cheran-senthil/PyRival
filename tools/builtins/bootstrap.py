4b7bc6a67f07cc90b97d49e5517eaa311fdfac00
cheran-senthil <cheran.v.senthil@gmail.com>
2019-05-07 18:47:29 +0530

    Use as decorator

--- tools/builtins/bootstrap.py
+++ tools/builtins/bootstrap.py
@@ -1,15 +1,21 @@
 from types import GeneratorType
 
 
-def bootstrap(call):
-    stack = []
-    while True:
-        if type(call) is GeneratorType:
-            stack.append(call)
-            call = next(call)
+def bootstrap(func, stack=[]):
+    def wrapped_function(*args, **kwargs):
+        if stack:
+            return func(*args, **kwargs)
         else:
-            stack.pop()
-            if not stack:
-                break
-            call = stack[-1].send(call)
-    return call
+            call = func(*args, **kwargs)
+            while True:
+                if type(call) is GeneratorType:
+                    stack.append(call)
+                    call = next(call)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    call = stack[-1].send(call)
+            return call
+
+    return wrapped_function
