1127dac5d079a46350f067692493f4462136fa32
cheran-senthil <cheran.v.senthil@gmail.com>
2019-05-12 17:53:03 +0530

    Improved hashing methods

--- snippets/strings/hashing.py
+++ snippets/strings/hashing.py
@@ -1,37 +1,33 @@
 import random
 
-MAXVAL = 10**9 + 1
-MOD1 = 2147483629
-MOD2 = 2147483647
-# BASE = random.choice(numbers not in s)
-BASE1 = random.randint(MAXVAL, MOD1 - 1)
-BASE2 = random.randint(MAXVAL, MOD2 - 1)
+MOD = 2147483647
+BASE1 = random.randrange(MOD)
+BASE2 = random.randrange(MOD)
 
 
 class Hashing:
     def __init__(self, s):
-        self.n = n = len(s)
-        hash1, hash2 = [0] * (n + 1), [0] * (n + 1)
-        for i in range(n):
-            hash1[i + 1] = (BASE1 * hash1[i] + s[i]) % MOD1
-            hash2[i + 1] = (BASE2 * hash2[i] + s[i]) % MOD2
-        self.hash1, self.hash2 = hash1, hash2
+        self._len = _len = len(s)
+        f_hash, s_hash = [0] * (_len + 1), [0] * (_len + 1)
+        for i in range(_len):
+            f_hash[i + 1] = (BASE1 * f_hash[i] + s[i]) % MOD
+            s_hash[i + 1] = (BASE2 * s_hash[i] + s[i]) % MOD
+        self.f_hash, self.s_hash = f_hash, s_hash
 
     def hashed(self, start, stop):
-        return ((self.hash1[stop] - pow(BASE1, stop - start, MOD1) * self.hash1[start]) % MOD1,
-                (self.hash2[stop] - pow(BASE2, stop - start, MOD2) * self.hash2[start]) % MOD2)
+        return ((self.f_hash[stop] - pow(BASE1, stop - start, MOD) * self.f_hash[start]) % MOD,
+                (self.s_hash[stop] - pow(BASE2, stop - start, MOD) * self.s_hash[start]) % MOD)
 
     def get_hashes(self, length):
-        shift1, shift2 = pow(BASE1, length, MOD1), pow(BASE2, length, MOD2)
-        hash1 = [(self.hash1[i + length] - shift1 * self.hash1[i]) % MOD1 for i in range(self.n - length + 1)]
-        hash2 = [(self.hash2[i + length] - shift2 * self.hash2[i]) % MOD2 for i in range(self.n - length + 1)]
-        return hash1, hash2
+        pow_base1, pow_base2 = pow(BASE1, length, MOD), pow(BASE2, length, MOD)
+        f_hash = [(self.f_hash[i + length] - pow_base1 * self.f_hash[i]) % MOD for i in range(self._len - length + 1)]
+        s_hash = [(self.s_hash[i + length] - pow_base2 * self.s_hash[i]) % MOD for i in range(self._len - length + 1)]
+        return f_hash, s_hash
 
 
 def hashed(s):
-    n = len(s)
-    hash1, hash2 = 0, 0
-    for i in range(n):
-        hash1 = (BASE1 * hash1 + s[i]) % MOD1
-        hash2 = (BASE2 * hash2 + s[i]) % MOD2
-    return hash1, hash2
+    f_hash, s_hash = 0, 0
+    for si in s:
+        f_hash = (BASE1 * f_hash + si) % MOD
+        s_hash = (BASE2 * s_hash + si) % MOD
+    return (f_hash, s_hash)
