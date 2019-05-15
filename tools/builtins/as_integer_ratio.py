8a1bae8e7df07dc72f079866ee148a926fa589a8
cheran-senthil <cheran.v.senthil@gmail.com>
2019-05-15 18:55:49 +0530

    minor improvement

--- tools/builtins/as_integer_ratio.py
+++ tools/builtins/as_integer_ratio.py
@@ -6,7 +6,10 @@ def as_integer_ratio(x, prec=53):
         return 0, 1
     a, b = math.frexp(x)
     a = int(a * (1 << prec))
-    g = ((~a + 1) & a).bit_length() - 1
+    g = (a & -a).bit_length() - 1
     a >>= g
     b += g - prec
     return (a << b, 1) if b > 0 else (a, 1 << -b)
+
+
+print(as_integer_ratio(-1.0 / 3))
