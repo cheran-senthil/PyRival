08924adcaa778eb977f550c5693b982805a1e357
Eduard Netsajev <eduard.netsajev93@gmail.com>
2019-06-02 14:42:03 +0300

    improved Treap.py - added functions + wrapper set class

--- snippets/data_structures/Treap.py
+++ snippets/data_structures/Treap.py
@@ -64,7 +64,7 @@ def erase(root, key):
     return node
 
 
-def lower_bound(root, key):
+def ceiling_key(root, key):
     while root and _keys[root] < key:
         root = right_child[root]
     if not root:
@@ -82,7 +82,7 @@ def lower_bound(root, key):
     return min_node
 
 
-def upper_bound(root, key):
+def higher_key(root, key):
     while root and _keys[root] <= key:
         root = right_child[root]
     if not root:
@@ -93,11 +93,47 @@ def upper_bound(root, key):
         if _keys[root] <= key:
             root = right_child[root]
         else:
-            if _keys[root] <= min_key:
+            if _keys[root] < min_key:
                 min_key = _keys[root]
                 min_node = root
             root = left_child[root]
     return min_node
+
+
+def floor_key(root, key):
+    while root and _keys[root] > key:
+        root = left_child[root]
+    if not root:
+        return 0
+    max_node = root
+    max_key = _keys[root]
+    while root:
+        if _keys[root] > key:
+            root = left_child[root]
+        else:
+            if _keys[root] > max_key:
+                max_key = _keys[root]
+                max_node = root
+            root = right_child[root]
+    return max_node
+
+
+def lower_key(root, key):
+    while root and _keys[root] >= key:
+        root = left_child[root]
+    if not root:
+        return 0
+    max_node = root
+    max_key = _keys[root]
+    while root:
+        if _keys[root] >= key:
+            root = left_child[root]
+        else:
+            if _keys[root] > max_key:
+                max_key = _keys[root]
+                max_node = root
+            root = right_child[root]
+    return max_node
 
 
 values = sorted([])
@@ -116,3 +152,33 @@ def build(begin, end):
     left_child[root] = build(begin, mid)
     right_child[root] = build(mid + 1, end)
     return root
+
+
+class myset(object):
+    def __init__(self):
+        self.root = 0
+
+    def insert(self, key):
+        if not self.root:
+            self.root = create_node(key)
+        else:
+            self.root = insert(self.root, key)
+
+    def erase(self, key):
+        self.root = erase(self.root, key)
+
+    def ceiling_key(self, key):
+        x = ceiling_key(self.root, key) if self.root else 0
+        return _keys[x] if x else None
+
+    def higher_key(self, key):
+        x = higher_key(self.root, key) if self.root else 0
+        return _keys[x] if x else None
+
+    def floor_key(self, key):
+        x = floor_key(self.root, key) if self.root else 0
+        return _keys[x] if x else None
+
+    def lower_key(self, key):
+        x = lower_key(self.root, key) if self.root else 0
+        return _keys[x] if x else None
