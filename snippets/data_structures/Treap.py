778a80a3674141863bf6a858a4b8574d846a1a17
Bjorn <pajenegod@gmail.com>
2019-06-03 02:52:45 +0200

    Added TreeSet and TreeMultiSet. Also improved the treap.

--- snippets/data_structures/Treap.py
+++ snippets/data_structures/Treap.py
@@ -1,23 +1,159 @@
 import random
 
+class TreeSet(object):
+    root = 0
+    size = 0
+    def __init__(self, data=None):
+        if data:
+            data = iter(sorted(data))
+            tmp = [next(data)]
+            for d in data:
+                if tmp[-1] != d:
+                    tmp.append(d)
+            data = tmp
+            self.root = treap_builder(data)
+            self.size = len(data)
+
+    def add(self, key, check_unique=True):
+        if not self.root:
+            self.size += 1
+            self.root = treap_create_node(key)
+        elif not (check_unique and key in self):
+            self.size += 1
+            self.root = treap_insert(self.root, key)
+
+    def remove(self, key):
+        self.root = treap_erase(self.root, key)
+        self.size -= 1
+    
+    def discard(self, key):
+        try:
+            self.root = treap_erase(self.root, key)
+            self.size -= 1
+        except KeyError:
+            pass
+
+    def ceiling(self, key):
+        x = treap_ceiling(self.root, key) if self.root else 0
+        return treap_keys[x] if x else None
+
+    def higher(self, key):
+        x = treap_higher(self.root, key) if self.root else 0
+        return treap_keys[x] if x else None
+
+    def floor(self, key):
+        x = treap_floor(self.root, key) if self.root else 0
+        return treap_keys[x] if x else None
+
+    def lower(self, key):
+        x = treap_lower(self.root, key) if self.root else 0
+        return treap_keys[x] if x else None
+
+    def max(self):
+        if not self.root:
+            raise ValueError('.max() on empty TreeSet')
+        return treap_keys[treap_max(self.root)]
+    
+    def min(self):
+        if not self.root:
+            raise ValueError('.min() on empty TreeSet')
+        return treap_keys[treap_min(self.root)]
+
+    def __len__(self):
+        return self.size
+
+    def __nonzero__(self):
+        return bool(self.root)
+    __bool__ = __nonzero__
+
+    def __contains__(self, key):
+        return treap_keys[treap_floor(self.root, key)] == key if self.root else False
+
+    def __str__(self):
+        return 'TreeSet([%s])' % ', '.join(str(key) for key in self)
+    __repr__ = __str__
+
+    def __iter__(self):
+        if not self.root:
+            return iter()
+        out = []
+        stack = [self.root]
+        while stack:
+            node = stack.pop()
+            if node>0:
+                if right_child[node]:
+                    stack.append(right_child[node])
+                stack.append(~node)
+                if left_child[node]:
+                    stack.append(left_child[node])
+            else:
+                out.append(treap_keys[~node])
+        return iter(out)
+
+
+class TreeMultiSet(TreeSet):
+    def __init__(self, data=None):
+        if data:
+            data = sorted(data)
+            self.root = treap_builder(data)
+            self.size = len(data)
+
+    def add(self, key):
+        return super(TreeMultiSet, self).add(key, False)
+    
+    def __str__(self):
+        return 'TreeMultiSet([%s])' % ', '.join(str(key) for key in self)
+    __repr__ = __str__
+
+
+# Build a treap in O(n) time using sorted data
+def treap_builder(sorted_data):
+    def build(begin, end):
+        if begin == end:
+            return 0
+        mid = (begin + end) // 2
+        root = treap_create_node(sorted_data[mid])
+        left_child[root] = build(begin, mid)
+        right_child[root] = build(mid + 1, end)
+        
+        # sift down the priorities
+        ind = root
+        while True:
+            lc = left_child[ind]
+            rc = right_child[ind]
+
+            if lc and treap_prior[lc] > treap_prior[ind]:
+                if rc and treap_prior[rc] > treap_prior[rc]:
+                    treap_prior[ind], treap_prior[rc], ind = \
+                    treap_prior[rc], treap_prior[ind], rc
+                else:
+                    treap_prior[ind], treap_prior[lc], ind = \
+                    treap_prior[lc], treap_prior[ind], lc
+            elif rc and treap_prior[rc] > treap_prior[ind]:
+                treap_prior[ind], treap_prior[rc], ind = \
+                treap_prior[rc], treap_prior[ind], rc
+            else:
+                break
+        return root 
+    return build(0, len(sorted_data))
+
+
 left_child = [0]
 right_child = [0]
-_keys = [0]
-_prior = [0.0]
+treap_keys = [0]
+treap_prior = [0.0]
 
-
-def create_node(key):
-    _keys.append(key)
-    _prior.append(random.random())
+def treap_create_node(key):
+    treap_keys.append(key)
+    treap_prior.append(random.random())
     left_child.append(0)
     right_child.append(0)
-    return len(_keys) - 1
+    return len(treap_keys) - 1
 
-
-def split(root, key):
+def treap_split(root, key):
     left_pos = right_pos = 0
     while root:
-        if key < _keys[root]:
+        if key < treap_keys[root]:
             left_child[right_pos] = right_pos = root
             root = left_child[root]
         else:
@@ -27,11 +163,10 @@ def split(root, key):
     right_child[left_pos] = left_child[right_pos] = right_child[0] = left_child[0] = 0
     return left, right
 
-
-def merge(left, right):
+def treap_merge(left, right):
     where, pos = left_child, 0
     while left and right:
-        if _prior[left] > _prior[right]:
+        if treap_prior[left] > treap_prior[right]:
             where[pos] = pos = left
             where = right_child
             left = right_child[left]
@@ -44,75 +179,104 @@ def merge(left, right):
     left_child[0] = 0
     return node
 
+def treap_insert(root, key):
+    left, right = treap_split(root, key)
+    return treap_merge(treap_merge(left, treap_create_node(key)), right)
 
-def insert(root, key):
-    left, right = split(root, key)
-    return merge(merge(left, create_node(key)), right)
-
-
-def erase(root, key):
-    if _keys[root] == key:
-        return merge(left_child[root], right_child[root])
+def treap_erase(root, key):
+    if treap_keys[root] == key:
+        return treap_merge(left_child[root], right_child[root])
     node = root
-    while _keys[root] != key:
+    while root and treap_keys[root] != key:
         parent = root
-        root = left_child[root] if key < _keys[root] else right_child[root]
+        root = left_child[root] if key < treap_keys[root] else right_child[root]
+    if not root:
+        raise KeyError(str(key))
     if root == left_child[parent]:
-        left_child[parent] = merge(left_child[root], right_child[root])
+        left_child[parent] = treap_merge(left_child[root], right_child[root])
     else:
-        right_child[parent] = merge(left_child[root], right_child[root])
+        right_child[parent] = treap_merge(left_child[root], right_child[root])
+    
     return node
 
-
-def lower_bound(root, key):
-    while root and _keys[root] < key:
+def treap_ceiling(root, key):
+    while root and treap_keys[root] < key:
         root = right_child[root]
     if not root:
         return 0
     min_node = root
-    min_key = _keys[root]
+    min_key = treap_keys[root]
     while root:
-        if _keys[root] < key:
+        if treap_keys[root] < key:
             root = right_child[root]
         else:
-            if _keys[root] < min_key:
-                min_key = _keys[root]
+            if treap_keys[root] < min_key:
+                min_key = treap_keys[root]
                 min_node = root
             root = left_child[root]
     return min_node
 
-
-def upper_bound(root, key):
-    while root and _keys[root] <= key:
+def treap_higher(root, key):
+    while root and treap_keys[root] <= key:
         root = right_child[root]
     if not root:
         return 0
     min_node = root
-    min_key = _keys[root]
+    min_key = treap_keys[root]
     while root:
-        if _keys[root] <= key:
+        if treap_keys[root] <= key:
             root = right_child[root]
         else:
-            if _keys[root] <= min_key:
-                min_key = _keys[root]
+            if treap_keys[root] < min_key:
+                min_key = treap_keys[root]
                 min_node = root
             root = left_child[root]
     return min_node
 
-
-values = sorted([])
-node_cnt = 1
-
-
-def build(begin, end):
-    global node_cnt
-    if begin == end:
+def treap_floor(root, key):
+    while root and treap_keys[root] > key:
+        root = left_child[root]
+    if not root:
         return 0
-    mid = (begin + end) // 2
-    root = node_cnt
-    node_cnt += 1
-    _prior[root] = random.random()
-    _keys[root] = values[mid]
-    left_child[root] = build(begin, mid)
-    right_child[root] = build(mid + 1, end)
+    max_node = root
+    max_key = treap_keys[root]
+    while root:
+        if treap_keys[root] > key:
+            root = left_child[root]
+        else:
+            if treap_keys[root] > max_key:
+                max_key = treap_keys[root]
+                max_node = root
+            root = right_child[root]
+    return max_node
+
+def treap_lower(root, key):
+    while root and treap_keys[root] >= key:
+        root = left_child[root]
+    if not root:
+        return 0
+    max_node = root
+    max_key = treap_keys[root]
+    while root:
+        if treap_keys[root] >= key:
+            root = left_child[root]
+        else:
+            if treap_keys[root] > max_key:
+                max_key = treap_keys[root]
+                max_node = root
+            root = right_child[root]
+    return max_node
+
+def treap_min(root):
+    if not root:
+        return 0
+    while left_child[root]:
+        root = left_child[root]
+    return root
+
+def treap_max(root):
+    if not root:
+        return 0
+    while right_child[root]:
+        root = right_child[root]
     return root
