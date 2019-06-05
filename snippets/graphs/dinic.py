f3f4176e1dd6d7967d8a031c9bd822123f6d9066
Mukundan Senthil <mukundan314@gmail.com>
2019-06-05 18:52:27 +0530

    Fix Dinic

--- snippets/graphs/dinic.py
+++ snippets/graphs/dinic.py
@@ -1,61 +1,61 @@
-from collections import deque
-
 INF = float("inf")
-
-SCALING = False
 
 
 class Dinic:
-    def __init__(self, maxv, var=int):
-        self.lim = 1
-        self.s = maxv - 2
-        self.t = maxv - 1
+    def __init__(self, n):
+        self.lvl = [0] * n
+        self.ptr = [0] * n
+        self.q = [0] * n
+        self.adj = [[] for _ in range(n)]
 
-        self.level = [0] * maxv
-        self.ptr = [0] * maxv
-        self.adj = [[] for _ in range(maxv)]
+    def add_edge(self, a, b, c, rcap=0):
+        self.adj[a].append([b, len(self.adj[b]), c, 0])
+        self.adj[b].append([a, len(self.adj[a]) - 1, rcap, 0])
 
-    def add_edge(self, a, b, cap, is_directed=True):
-        self.adj[a].append([b, len(self.adj[b]), cap, 0])
-        self.adj[b].append([a, len(self.adj[a]) - 1, 0 if is_directed else cap, 0])
+    def dfs(self, v, t, f):
+        if v == t or not f:
+            return f
 
-    def bfs(self):
-        q = deque([self.s])
-        self.level = [-1] * len(self.level)
+        for i in range(self.ptr[v], len(self.adj[v])):
+            e = self.adj[v][i]
 
-        while q and self.level[self.t] == -1:
-            v = q.popleft()
-            for e in self.adj[v]:
-                if (self.level[e[0]] == -1 and e[3] < e[2] and ((not SCALING) or (e[2] - e[3] >= self.lim))):
-                    q.append(e[0])
-                    self.level[e[0]] = self.level[v] + 1
+            if self.lvl[e[0]] == self.lvl[v] + 1:
+                p = self.dfs(e[0], t, min(f, e[2] - e[3]))
+                if p:
+                    self.adj[v][i][3] += p
+                    self.adj[e[0]][e[1]][3] -= p
+                    return p
 
-        return self.level[self.t] != -1
+        return 0
 
-    def dfs(self, v, flow):
-        if v == self.t or not flow:
-            return flow
-        while self.ptr[v] < len(self.adj[v]):
-            e = self.adj[v][self.ptr[v]]
-            if self.level[e.to] != self.level[v] + 1:
-                continue
-            pushed = self.dfs(e.to, min(flow, e.cap - e.flow))
-            if pushed:
-                e.flow += pushed
-                self.adj[e.to][e.rev].flow -= pushed
-                return pushed
-            return 0
-
-    def calc(self):
+    def calc(self, s, t):
         flow = 0
-        self.lim = 1 << 30 if SCALING else 1
-        while self.lim > 0:
-            while self.bfs():
-                self.ptr = [0] * len(self.ptr)
-                pushed = self.dfs(self.s, INF)
-                while pushed:
-                    flow += pushed
-                    pushed = self.dfs(self.s, INF)
-            self.lim >>= 1
+        self.q[0] = s
+
+        while True:
+            for l in range(0, 31):
+                self.lvl = [0] * len(self.q)
+                self.ptr = [0] * len(self.q)
+
+                qi = 0
+                qe = self.lvl[s] = 1
+
+                while qi < qe and not self.lvl[t]:
+                    v = self.q[qi]
+                    qi += 1
+
+                    for e in self.adj[v]:
+                        if not self.lvl[e[0]] and (e[2] - e[3]) >> (30 - l):
+                            self.q[qe] = e[0]
+                            qe += 1
+                            self.lvl[e[0]] = self.lvl[v] + 1
+
+                p = self.dfs(s, t, INF)
+                while p:
+                    flow += p
+                    p = self.dfs(s, t, INF)
+
+            if not self.lvl[t]:
+                break
 
         return flow
