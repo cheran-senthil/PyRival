e5a293243e2f22d9d9ee375a95ad12d891b5813b
Cheran <cheran.v.senthil@gmail.com>
2019-06-14 01:51:32 +0530

    bug fix and improvement

--- snippets/data_structures/RangeQuery.py
+++ snippets/data_structures/RangeQuery.py
@@ -3,11 +3,10 @@ class RangeQuery:
         n = len(data)
         depth = n.bit_length() + 1
         self.func = func
-        self._data = _data = [data[:] for _ in range(depth)]
+        self._data = _data = [data[:]]
         for i in range(depth - 1):
-            _data_prev, _data_next = _data[i], _data[i + 1]
-            for j in range(n - (1 << i)):
-                _data_next[j] = func(_data_prev[j], _data_prev[j + (1 << i)])
+            _data_prev = _data[-1]
+            _data.append([func(_data_prev[j], _data_prev[min(n - 1, j + (1 << i))]) for j in range(n)])
 
     def query(self, begin, end):
         depth = (end - begin).bit_length() - 1
