af86b77b3a4c96c6e0beb00fbe603a90b00c63e0
Cheran <cheran.v.senthil@gmail.com>
2019-06-17 15:49:52 +0530

    remove whitespace

--- snippets/graphs/bfs.py
+++ snippets/graphs/bfs.py
@@ -1,7 +1,6 @@
 def bfs(graph, start=0):
     used = [False] * len(graph)
     used[start] = True
-
     q = [start]
     for v in q:
         for w in graph[v]:
@@ -13,19 +12,14 @@ def bfs(graph, start=0):
 def layers(graph, start=0):
     used = [False] * len(graph)
     used[start] = True
-
     q, ret = [start], []
-
     while q:
         nq = []
         ret.append(q)
-
         for v in q:
             for w in graph[v]:
                 if not used[w]:
                     used[w] = True
                     nq.append(w)
-
         q = nq
-
     return ret
