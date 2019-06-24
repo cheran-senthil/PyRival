0b7bc1e4e24f8d075cce46b43a88fb40d56108f3
Cheran <cheran.v.senthil@gmail.com>
2019-06-25 02:41:43 +0530

    make data public

--- snippets/data_structures/SegmentTree.py
+++ snippets/data_structures/SegmentTree.py
@@ -1,43 +1,32 @@
 class SegmentTree:
     def __init__(self, data, default=0, func=max):
         """initialize the segment tree with data"""
-        self._len = _len = len(data)
-        self._size = _size = 1 << (_len - 1).bit_length()
-        self._data = _data = [default] * (2 * _size)
         self._default = default
         self._func = func
+        self._len = len(data)
+        self._size = _size = 1 << (self._len - 1).bit_length()
 
-        _data[_size:_size + _len] = data
+        self.data = [default] * (2 * _size)
+        self.data[_size:_size + self._len] = data
         for i in reversed(range(_size)):
-            _data[i] = func(_data[i + i], _data[i + i + 1])
+            self.data[i] = func(self.data[i + i], self.data[i + i + 1])
 
     def __delitem__(self, idx):
         self[idx] = self._default
 
     def __getitem__(self, idx):
-        return self._data[idx + self._size]
+        return self.data[idx + self._size]
 
     def __setitem__(self, idx, value):
         idx += self._size
-        self._data[idx] = value
+        self.data[idx] = value
         idx >>= 1
         while idx:
-            self._data[idx] = self._func(self._data[2 * idx], self._data[2 * idx + 1])
+            self.data[idx] = self._func(self.data[2 * idx], self.data[2 * idx + 1])
             idx >>= 1
 
     def __len__(self):
         return self._len
-
-    def bisect(self, value, cmp):
-        if not cmp(value, self._data[1]):
-            return -1
-
-        idx = 1
-        while idx < self._size:
-            idx <<= 1
-            if cmp(value, self._data[idx + 1]):
-                idx += 1
-        return idx - self._size
 
     def query(self, start, stop):
         start += self._size
@@ -46,11 +35,11 @@ class SegmentTree:
         res = self._default
         while start < stop:
             if start & 1:
-                res = self._func(res, self._data[start])
+                res = self._func(res, self.data[start])
                 start += 1
             if stop & 1:
                 stop -= 1
-                res = self._func(res, self._data[stop])
+                res = self._func(res, self.data[stop])
             start >>= 1
             stop >>= 1
         return res
@@ -58,7 +47,7 @@ class SegmentTree:
     def __repr__(self):
         def recursive_repr(i):
             if i >= self._size:
-                return [str(self._data[i])]
+                return [str(self.data[i])]
 
             left = recursive_repr(2 * i)
             right = recursive_repr(2 * i + 1)
@@ -75,7 +64,7 @@ class SegmentTree:
 
             lines.appstop(branches)
             lines.appstop(''.join(stem))
-            lines.appstop(str(self._data[i]).center(width))
+            lines.appstop(str(self.data[i]).center(width))
             return lines
 
         return '\n'.join(reversed(recursive_repr(1)))
