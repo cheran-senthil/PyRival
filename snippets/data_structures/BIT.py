f58ef073e076e8a409a60d0837ba06e9fe41269e
Eduard Netšajev <eduard.netsajev93@gmail.com>
2019-07-04 02:26:32 +0300

    Make Fenwick Tree a proper class (#20)
    * Make Fenwick Tree a proper class
    
    * small refactoring
    
    * minor formatting

--- snippets/data_structures/BIT.py
+++ snippets/data_structures/BIT.py
@@ -1,33 +1,32 @@
-def bitify(x):
-    """transform list into BIT"""
-    for i in range(len(x)):
-        j = i | (i + 1)
-        if j < len(x):
-            x[j] += x[i]
+class FenwickTree:
+    def __init__(self, x):
+        """transform list into BIT"""
+        self.bit = x
+        for i in range(len(x)):
+            j = i | (i + 1)
+            if j < len(x):
+                x[j] += x[i]
 
+    def update(self, idx, x):
+        """updates bit[idx] += x"""
+        while idx < len(self.bit):
+            self.bit[idx] += x
+            idx |= idx + 1
 
-def bitupdate(bit, idx, x):
-    """updates bit[idx] += x"""
-    while idx < len(bit):
-        bit[idx] += x
-        idx |= idx + 1
+    def query(self, end):
+        """calc sum(bit[:end])"""
+        x = 0
+        while end:
+            x += self.bit[end - 1]
+            end &= end - 1
+        return x
 
-
-def bitquery(bit, end):
-    """calc sum(bit[:end])"""
-    x = 0
-    while end:
-        x += bit[end - 1]
-        end &= end - 1
-    return x
-
-
-def bitkth(bit, k):
-    """Find largest idx such that sum(bit[:idx]) <= k"""
-    idx = -1
-    for d in reversed(range(len(bit).bit_length())):
-        right_idx = idx + (1 << d)
-        if right_idx < len(bit) and k >= bit[right_idx]:
-            idx = right_idx
-            k -= bit[idx]
-    return idx + 1
+    def findkth(self, k):
+        """Find largest idx such that sum(bit[:idx]) <= k"""
+        idx = -1
+        for d in reversed(range(len(self.bit).bit_length())):
+            right_idx = idx + (1 << d)
+            if right_idx < len(self.bit) and k >= self.bit[right_idx]:
+                idx = right_idx
+                k -= self.bit[idx]
+        return idx + 1
