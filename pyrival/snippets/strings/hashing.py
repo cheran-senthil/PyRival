63c37327052de4a2385d79bc838986c0190bb803
Cheran <cheran.v.senthil@gmail.com>
2019-07-18 00:26:57 +0530

    allow general mod

--- pyrival/snippets/strings/hashing.py
+++ pyrival/snippets/strings/hashing.py
@@ -2,26 +2,25 @@ import random
 
 
 class Hashing:
-    def __init__(self, s):
-        self.MOD = 2147483647
-        self.BASE1 = random.randrange(self.MOD)
-        self.BASE2 = random.randrange(self.MOD)
+    def __init__(self, s, mod=2147483647):
+        self.mod = mod
+        self.base1 = base1 = random.randrange(mod)
+        self.base2 = base2 = random.randrange(mod)
 
         self._len = _len = len(s)
         f_hash, s_hash = [0] * (_len + 1), [0] * (_len + 1)
         for i in range(_len):
-            f_hash[i + 1] = (self.BASE1 * f_hash[i] + s[i]) % self.MOD
-            s_hash[i + 1] = (self.BASE2 * s_hash[i] + s[i]) % self.MOD
+            f_hash[i + 1] = (base1 * f_hash[i] + s[i]) % mod
+            s_hash[i + 1] = (base2 * s_hash[i] + s[i]) % mod
         self.f_hash, self.s_hash = f_hash, s_hash
 
     def hashed(self, start, stop):
-        return ((self.f_hash[stop] - pow(self.BASE1, stop - start, self.MOD) * self.f_hash[start]) % self.MOD,
-                (self.s_hash[stop] - pow(self.BASE2, stop - start, self.MOD) * self.s_hash[start]) % self.MOD)
+        return ((self.f_hash[stop] - pow(self.base1, stop - start, self.mod) * self.f_hash[start]) % self.mod,
+                (self.s_hash[stop] - pow(self.base2, stop - start, self.mod) * self.s_hash[start]) % self.mod)
 
     def get_hashes(self, length):
-        pow_base1, pow_base2 = pow(self.BASE1, length, self.MOD), pow(self.BASE2, length, self.MOD)
-        f_hash = [(self.f_hash[i + length] - pow_base1 * self.f_hash[i]) % self.MOD
-                  for i in range(self._len - length + 1)]
-        s_hash = [(self.s_hash[i + length] - pow_base2 * self.s_hash[i]) % self.MOD
-                  for i in range(self._len - length + 1)]
+        mod = self.mod
+        pow_base1, pow_base2 = pow(self.base1, length, mod), pow(self.base2, length, mod)
+        f_hash = [(self.f_hash[i + length] - pow_base1 * self.f_hash[i]) % mod for i in range(self._len - length + 1)]
+        s_hash = [(self.s_hash[i + length] - pow_base2 * self.s_hash[i]) % mod for i in range(self._len - length + 1)]
         return f_hash, s_hash
