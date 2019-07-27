84e41946b7e379e4ab4edab50662532f69077e7d
Björn Martinsson <pajenegod@gmail.com>
2019-07-27 21:47:32 +0200

    Improved speed for KMP and removed the need for a uniq seperator

--- pyrival/snippets/strings/kmp.py
+++ pyrival/snippets/strings/kmp.py
@@ -1,13 +1,24 @@
-def pi(s):
-    p = [0] * len(s)
-    for i in range(1, len(s)):
-        g = p[i - 1]
-        while g and (s[i] != s[g]):
-            g = p[g - 1]
-        p[i] = g + (s[i] == s[g])
-
-    return p
-
-
-def match(s, pat, uniq='\0'):
-    return [i + 1 - c for i, c in enumerate(pi(pat + uniq + s)[len(pat) + 1:]) if c == len(pat)]
+def partial(S):
+    P = [0] * len(S)
+    g = 0
+    for i in range(1, len(S)):
+        while g and (S[g] != S[i]):
+            g = P[g - 1]
+        P[i] = g = g + (S[g] == S[i])
+ 
+    return P
+ 
+def match(S, pattern):
+    P = partial(pattern)
+    g = 0
+    ret = []
+    
+    for i in range(len(S)):
+        while g and pattern[g] != S[i]:
+            g = P[g - 1]
+        g += pattern[g] == S[i]
+        if g == len(P): 
+            ret.append(i + 1 - g)
+            g = P[g - 1]
+    
+    return ret
