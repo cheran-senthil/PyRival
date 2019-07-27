a1e0d00f8bde36d042882db33c717cc85e855c5b
Cheran <cheran.v.senthil@gmail.com>
2019-07-27 20:40:59 +0530

    improve kmp

--- pyrival/snippets/strings/kmp.py
+++ pyrival/snippets/strings/kmp.py
@@ -4,14 +4,14 @@ def pi(s):
         g = p[i - 1]
         while g and (s[i] != s[g]):
             g = p[g - 1]
-        p[i] = g + int(s[i] == s[g])
+        p[i] = g + (s[i] == s[g])
 
     return p
 
 
-def match(s, pat):
+def match(s, pat, uniq='\0'):
     res = []
-    p = pi(pat + '\0' + s)
+    p = pi(pat + uniq + s)
     for i in range(len(p) - len(s), len(p)):
         if p[i] == len(pat):
             res.append(i - 2 * len(pat))
