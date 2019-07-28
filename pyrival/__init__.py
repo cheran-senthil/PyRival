cce539cfcb9a62a820640265f35fac9e1dbe2ca4
Cheran <cheran.v.senthil@gmail.com>
2019-07-28 15:05:08 +0530

    Create string_find method

--- pyrival/__init__.py
+++ pyrival/__init__.py
@@ -12,7 +12,12 @@ from .performance.readnumbers import readnumbers
 from .snippets.algebra import ntt
 from .snippets.algebra.chinese_remainder import chinese_remainder, composite_crt
 from .snippets.algebra.discrete_log import discrete_log
-from .snippets.algebra.factors import all_factors, distinct_factors, pollard_rho, prime_factors
+from .snippets.algebra.factors import (
+    all_factors,
+    distinct_factors,
+    pollard_rho,
+    prime_factors,
+)
 from .snippets.algebra.fft import fft, fft_conv
 from .snippets.algebra.fst import fst, fst_conv
 from .snippets.algebra.gcd import extended_gcd, gcd, gcdm, lcm, lcmm
@@ -22,8 +27,12 @@ from .snippets.algebra.modinv import modinv
 from .snippets.algebra.phi import phi
 from .snippets.algebra.primitive_root import ilog, primitive_root
 from .snippets.algebra.sieve import prime_list, prime_sieve
-from .snippets.combinatorics.combinatorics import (catalan_recursive, euler_recursive, stirling_1_recursive,
-                                                   stirling_2_recursive)
+from .snippets.combinatorics.combinatorics import (
+    catalan_recursive,
+    euler_recursive,
+    stirling_1_recursive,
+    stirling_2_recursive,
+)
 from .snippets.combinatorics.nCr_mod import make_nCr_mod
 from .snippets.combinatorics.partitions import partition
 from .snippets.data_structures.BitArray import bitarray
@@ -38,15 +47,50 @@ from .snippets.data_structures.PersistentSegTree import create, minimum, setter
 from .snippets.data_structures.RangeQuery import RangeQuery
 from .snippets.data_structures.SegmentTree import SegmentTree
 from .snippets.data_structures.SortedList import SortedList
-from .snippets.data_structures.Treap import (TreapHashMap, TreapHashSet, TreapMultiSet, TreapSet, treap_builder,
-                                             treap_ceiling, treap_create_node, treap_erase, treap_floor, treap_higher,
-                                             treap_insert, treap_insert_unique, treap_keys, treap_lower, treap_max,
-                                             treap_merge, treap_min, treap_prior, treap_split)
+from .snippets.data_structures.Treap import (
+    TreapHashMap,
+    TreapHashSet,
+    TreapMultiSet,
+    TreapSet,
+    treap_builder,
+    treap_ceiling,
+    treap_create_node,
+    treap_erase,
+    treap_floor,
+    treap_higher,
+    treap_insert,
+    treap_insert_unique,
+    treap_keys,
+    treap_lower,
+    treap_max,
+    treap_merge,
+    treap_min,
+    treap_prior,
+    treap_split,
+)
 from .snippets.data_structures.Trie import Trie
 from .snippets.data_structures.TwoSat import TwoSat
 from .snippets.geometry.convex_hull import convex_hull
-from .snippets.geometry.lines import collinear, dist, get_2dline, intersect, is_parallel, is_same, rotate
-from .snippets.geometry.vectors import angle, closest_point, cross2d, cross3d, dot, norm_sq, scale, to_vec, translate
+from .snippets.geometry.lines import (
+    collinear,
+    dist,
+    get_2dline,
+    intersect,
+    is_parallel,
+    is_same,
+    rotate,
+)
+from .snippets.geometry.vectors import (
+    angle,
+    closest_point,
+    cross2d,
+    cross3d,
+    dot,
+    norm_sq,
+    scale,
+    to_vec,
+    translate,
+)
 from .snippets.graphs.bellman_ford import bellman_ford
 from .snippets.graphs.bfs import bfs, layers
 from .snippets.graphs.components import connected_components
@@ -63,7 +107,17 @@ from .snippets.graphs.lca import LCA
 from .snippets.graphs.prim import prim
 from .snippets.graphs.scc import scc
 from .snippets.graphs.toposort import kahn, toposort
-from .snippets.linear_algebra.matrix import eye, mat_add, mat_inv, mat_mul, mat_pow, mat_sub, minor, transpose, vec_mul
+from .snippets.linear_algebra.matrix import (
+    eye,
+    mat_add,
+    mat_inv,
+    mat_mul,
+    mat_pow,
+    mat_sub,
+    minor,
+    transpose,
+    vec_mul,
+)
 from .snippets.linear_algebra.multivariable_crt import is_sol, mcrt, pivot
 from .snippets.misc.alphabeta import AlphaBetaNode, alphabeta
 from .snippets.misc.cumsum2d import cumsum2d
@@ -75,10 +129,16 @@ from .snippets.numerical.integrate import fast_quad, quad, rec, simpson
 from .snippets.numerical.interpolate import interpolate
 from .snippets.numerical.iroot import iroot
 from .snippets.numerical.polynomial import diff, divroot, poly
-from .snippets.numerical.search import (binary_search, discrete_binary_search, discrete_ternary_search,
-                                        fractional_binary_search, golden_section_search, ternary_search)
+from .snippets.numerical.search import (
+    binary_search,
+    discrete_binary_search,
+    discrete_ternary_search,
+    fractional_binary_search,
+    golden_section_search,
+    ternary_search,
+)
 from .snippets.strings.hashing import Hashing
-from .snippets.strings.kmp import match, pi
+from .snippets.strings.kmp import match, partial, string_find
 from .snippets.strings.lcs import lcs
 from .snippets.strings.LCSubstr import LCSubstr
 from .snippets.strings.LPSubstr import LPSubstr
@@ -213,7 +273,7 @@ __all__ = [
     "partition",
     "performance",
     "phi",
-    "pi",
+    "partial",
     "pivot",
     "pollard_rho",
     "poly",
@@ -234,6 +294,7 @@ __all__ = [
     "split",
     "stirling_1_recursive",
     "stirling_2_recursive",
+    "string_find",
     "subset_masks",
     "sum_of_subsets",
     "ternary_search",
--- pyrival/snippets/strings/kmp.py
+++ pyrival/snippets/strings/kmp.py
@@ -1,24 +1,35 @@
-def partial(S):
-    P = [0] * len(S)
-    g = 0
-    for i in range(1, len(S)):
-        while g and (S[g] != S[i]):
-            g = P[g - 1]
-        P[i] = g = g + (S[g] == S[i])
+def partial(s):
+    g, pi = 0, [0] * len(s)
+    for i in range(1, len(s)):
+        while g and (s[g] != s[i]):
+            g = pi[g - 1]
+        pi[i] = g = g + (s[g] == s[i])
+
+    return pi
  
-    return P
- 
-def match(S, pattern):
-    P = partial(pattern)
+def match(s, pat):
+    pi = partial(pat)
+
+    g, idx = 0, []
+    for i in range(len(s)):
+        while g and pat[g] != s[i]:
+            g = pi[g - 1]
+        g += pat[g] == s[i]
+        if g == len(pi): 
+            idx.append(i + 1 - g)
+            g = pi[g - 1]
+    
+    return idx
+
+def string_find(s, pat):
+    pi = partial(pat)
+
     g = 0
-    ret = []
+    for i in range(len(s)):
+        while g and pat[g] != s[i]:
+            g = pi[g - 1]
+        g += pat[g] == s[i]
+        if g == len(pi): 
+            return True
     
-    for i in range(len(S)):
-        while g and pattern[g] != S[i]:
-            g = P[g - 1]
-        g += pattern[g] == S[i]
-        if g == len(P): 
-            ret.append(i + 1 - g)
-            g = P[g - 1]
-    
-    return ret
+    return False
