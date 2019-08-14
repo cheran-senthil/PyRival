8a2aaddcd3626103973e4dd08ed87b9024302c7c
Mukundan Senthil <mukundan314@gmail.com>
2019-08-14 18:04:40 +0530

    Split __init__.py into mutiple files

---
+++ pyrival/combinatorics/__init__.py
@@ -0,0 +1,12 @@
+from .combinatorics import catalan_recursive, euler_recursive, stirling_1_recursive, stirling_2_recursive
+from .nCr_mod import make_nCr_mod
+from .partitions import partition
+
+__all__ = [
+    "catalan_recursive",
+    "euler_recursive",
+    "stirling_1_recursive",
+    "stirling_2_recursive",
+    "make_nCr_mod",
+    "partition",
+]
