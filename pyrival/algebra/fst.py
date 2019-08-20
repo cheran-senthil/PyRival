4aa4faefa3090aa4d6782093ba43b5dc3020e44c
Mukundan Senthil <mukundan314@gmail.com>
2019-08-20 12:57:44 +0530

    Fix bug in fst

--- pyrival/algebra/fst.py
+++ pyrival/algebra/fst.py
@@ -7,9 +7,9 @@ def fst(a, oplus=op.and_, inv=False):
         for i in range(0, len(a), 2 * step):
             for j in range(i, i + step):
                 if oplus == op.and_:
-                    a[j], a[j + step] = a[j + step] - a[j], a[j] if inv else a[j + step], a[j] + a[j + step]
+                    a[j], a[j + step] = (a[j + step] - a[j], a[j]) if inv else (a[j + step], a[j] + a[j + step])
                 elif oplus == op.or_:
-                    a[j], a[j + step] = a[j + step], a[j] - a[j + step] if inv else a[j] + a[j + step], a[j]
+                    a[j], a[j + step] = (a[j + step], a[j] - a[j + step]) if inv else (a[j] + a[j + step], a[j])
                 elif oplus == op.xor:
                     a[j], a[j + step] = a[j] + a[j + step], a[j] - a[j + step]
         step *= 2
