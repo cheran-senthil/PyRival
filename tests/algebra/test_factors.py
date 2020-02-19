6ce7a7f5dba18d1d386b29c16c3cb47bb65afcdb
Mukundan Senthil <mukundan314@gmail.com>
2020-02-19 15:57:45 +0530

    Add tests for pollard_rho, prime_factors, distinct_factors and all_factors

---
+++ tests/algebra/test_factors.py
@@ -0,0 +1,37 @@
+import random
+
+import pyrival.algebra
+
+
+def test_pollard_rho():
+    for _ in range(1000):
+        n = random.randint(2, 1000)
+        print(n)
+        assert n % pyrival.algebra.pollard_rho(n) == 0
+
+
+def test_prime_factors():
+    for _ in range(1000):
+        n = random.randint(1, 1000)
+        for f, e in pyrival.algebra.prime_factors(n).items():
+            assert n % (f**e) == 0
+            n //= f**e
+        assert n == 1
+
+
+def test_distinct_factors():
+    for _ in range(1000):
+        n = random.randint(1, 1000)
+        factors = set(pyrival.algebra.distinct_factors(n))
+        for i in range(1, n + 1):
+            assert (n % i == 0) == (i in factors)
+
+
+def test_all_factors():
+    for _ in range(1000):
+        n = random.randint(1, 1000)
+        factors = pyrival.algebra.all_factors(n)
+        assert factors == sorted(factors)
+        factors = set(factors)
+        for i in range(1, n + 1):
+            assert (n % i == 0) == (i in factors)
