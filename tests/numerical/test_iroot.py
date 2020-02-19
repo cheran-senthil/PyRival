27d299c4881391130f89fe42d965faa4005d823f
Mukundan Senthil <mukundan314@gmail.com>
2020-02-19 18:43:03 +0530

    Add tests for iroot

---
+++ tests/numerical/test_iroot.py
@@ -0,0 +1,15 @@
+import random
+
+import pyrival.numerical
+
+
+def test_iroot():
+    for _ in range(10000):
+        k = random.randint(1, 10)
+        n = random.randint(-10000 if k & 1 else 0, 10000)
+        root = pyrival.numerical.iroot(n, k)
+
+        if root < 0:
+            assert (root - 1) ** k < n <= root ** k
+        else:
+            assert root ** k <= n < (root + 1) ** k
