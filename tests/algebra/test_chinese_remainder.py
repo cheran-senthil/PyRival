5e304be9c73655e8ca563dba348665275b401e8e
Mukundan Senthil <mukundan314@gmail.com>
2020-02-20 10:45:14 +0530

    Add tests for chinese_remainder

---
+++ tests/algebra/test_chinese_remainder.py
@@ -0,0 +1,12 @@
+import random
+
+import pyrival.algebra
+
+
+def test_chinese_remainder(primes):
+    for _ in range(1000):
+        l = random.randint(2, 100)
+        p = random.sample(primes, l)
+        a = [random.randint(0, 10000) % p[i] for i in range(l)]
+        x = pyrival.algebra.chinese_remainder(a, p)
+        assert [x % i for i in p] == a
