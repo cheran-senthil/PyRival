e4eb361e932f2491ea7d0e49616eb28324801e19
Mukundan Senthil <mukundan314@gmail.com>
2020-02-20 17:51:08 +0530

    Update combinatorics/__init__.py

--- pyrival/combinatorics/__init__.py
+++ pyrival/combinatorics/__init__.py
@@ -1,11 +1,19 @@
-from .combinatorics import catalan_recursive, euler_recursive, stirling_1_recursive, stirling_2_recursive
+from .combinatorics import (bell, catalan, catalan_recursive, derangements, euler, euler_recursive, multinomial, nCr,
+                            stirling_1_recursive, stirling_2, stirling_2_recursive)
 from .nCr_mod import make_nCr_mod
 from .partitions import partition
 
 __all__ = [
+    "bell",
+    "catalan",
     "catalan_recursive",
+    "derangements",
+    "euler",
     "euler_recursive",
+    "multinomial",
+    "nCr",
     "stirling_1_recursive",
+    "stirling_2",
     "stirling_2_recursive",
     "make_nCr_mod",
     "partition",
