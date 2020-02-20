9ad1250e60978a4d0a6b2eb2da3f4dcfe5981d9e
Mukundan Senthil <mukundan314@gmail.com>
2020-02-20 09:53:07 +0530

    Get full coverage for BitArray tests

--- tests/data_structures/test_BitArray.py
+++ tests/data_structures/test_BitArray.py
@@ -2,8 +2,14 @@ import pyrival.data_structures
 
 
 def test_BitArray():
-    array = pyrival.data_structures.BitArray(524288)
-    for i in range(524288):
+    array = pyrival.data_structures.BitArray(262144)
+
+    for i in range(262144):
         assert array[i] == 0
         array[i] = 1
         assert array[i] == 1
+
+    for i in range(262144):
+        assert array[i] == 1
+        array[i] = 0
+        assert array[i] == 0
