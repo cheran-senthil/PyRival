8cd67db3d13d76adb1f98c2e32b7ecac13f311a9
Mukundan Senthil <mukundan314@gmail.com>
2020-02-20 10:42:55 +0530

    Use fixture to get primes

---
+++ tests/algebra/conftest.py
@@ -0,0 +1,11 @@
+import os
+import pytest
+
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+
+
+@pytest.fixture
+def primes():
+    with open(os.path.join(DATA_DIR, 'primes.txt')) as f:
+        return [int(i) for i in f.read().split(' ')]
--- tests/algebra/test_is_prime.py
+++ tests/algebra/test_is_prime.py
@@ -1,10 +1,10 @@
 import pyrival.algebra
 
 
-def test_is_prime():
-    primes = set(pyrival.algebra.prime_list(100000))
-    for i in range(100000):
-        assert pyrival.algebra.is_prime(i) == (i in primes)
+def test_is_prime(primes):
+    prime_set = set(primes)
+    for i in range(primes[-1] + 1):
+        assert pyrival.algebra.is_prime(i) == (i in prime_set)
 
     assert pyrival.algebra.is_prime(10 ** 8 + 7) == True
     assert pyrival.algebra.is_prime(10 ** 9 + 7) == True
