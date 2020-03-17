309442a5d3d4c29ae248549504d95fc250dc3ceb
Mukundan Senthil <mukundan314@gmail.com>
2020-03-17 20:32:39 +0530

    Reduce range of random numbers generated for order_statistic test

--- tests/misc/test_order_statistic.py
+++ tests/misc/test_order_statistic.py
@@ -5,7 +5,7 @@ import pyrival.misc
 
 def test_order_statistic():
     for _ in range(1000):
-        arr = [random.randint(-1000, 1000) for _ in range(random.randint(0, 100))]
+        arr = [random.randint(-100, 100) for _ in range(random.randint(0, 100))]
         sor = sorted(arr)
 
         for i in range(len(arr)):
