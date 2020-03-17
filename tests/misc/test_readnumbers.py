d3b7d5a9086bc7c64f45d235164e862f382c0371
Mukundan Senthil <mukundan314@gmail.com>
2020-03-17 14:26:40 +0530

    Fix bug in test

--- tests/misc/test_readnumbers.py
+++ tests/misc/test_readnumbers.py
@@ -8,7 +8,7 @@ import pyrival.misc
 def test_readnumbers(monkeypatch):
     nums = [random.randint(-10000, 10000) for _ in range(100000)]
 
-    string = ''.join(str(num) + random.choice([' ', '\n', '\r\n']) for num in nums)[:-1]
+    string = ''.join(str(num) + random.choice([' ', '\n', '\r\n']) for num in nums).rstrip()
     stream = io.TextIOWrapper(io.BytesIO(string.encode('ascii')))
     monkeypatch.setattr(sys, "stdin", stream)
 
