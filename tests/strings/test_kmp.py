afc7e73669d5521f1f07782e2bcdfa5db368e64d
Mukundan Senthil <mukundan314@gmail.com>
2020-03-17 16:32:13 +0530

    Add tests for match and string_find

---
+++ tests/strings/test_kmp.py
@@ -0,0 +1,38 @@
+import random
+
+import pyrival.strings
+
+
+def brute_force_find(s, pat):
+    idx = []
+
+    for i in range(len(s) - len(pat) + 1):
+        found = True
+        for j in range(len(pat)):
+            found = found and (s[i + j] == pat[j])
+        if found:
+            idx.append(i)
+
+    return idx
+
+
+def test_match():
+    for _ in range(10000):
+        s_len = random.randint(1, 50)
+        pat_len = random.randint(1, s_len)
+
+        s = [random.randint(0, 10) for _ in range(s_len)]
+        pat = [random.randint(0, 10) for _ in range(pat_len)]
+
+        assert pyrival.strings.match(s, pat) == brute_force_find(s, pat)
+
+
+def test_string_find():
+    for _ in range(10000):
+        s_len = random.randint(1, 50)
+        pat_len = random.randint(1, s_len)
+
+        s = [random.randint(0, 10) for _ in range(s_len)]
+        pat = [random.randint(0, 10) for _ in range(pat_len)]
+
+        assert pyrival.strings.string_find(s, pat) == (brute_force_find(s, pat) != [])
