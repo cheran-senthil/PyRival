f64c33c7a4aba5c58cc7c76dc9dbdb8e0782013c
Mukundan Senthil <mukundan314@gmail.com>
2020-03-17 22:52:31 +0530

    Add test for composite_crt

--- tests/algebra/test_chinese_remainder.py
+++ tests/algebra/test_chinese_remainder.py
@@ -10,3 +10,12 @@ def test_chinese_remainder(primes):
         a = [random.randint(0, 10000) % p[i] for i in range(l)]
         x = pyrival.algebra.chinese_remainder(a, p)
         assert [x % i for i in p] == a
+
+
+def test_composite_crt(primes):
+    for _ in range(1000):
+        l = random.randint(2, 100)
+        p = random.sample(primes, l)
+        a = [random.randint(0, 10000) % p[i] for i in range(l)]
+        x = pyrival.algebra.composite_crt(a, p)
+        assert [x % i for i in p] == a
