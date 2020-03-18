02ed19ffbcac97636e605950e965a1a2e5a95acf
Mukundan Senthil <mukundan314@gmail.com>
2020-03-18 16:46:24 +0530

    Incease random ranges in tests for functions in factors.py

--- tests/algebra/test_factors.py
+++ tests/algebra/test_factors.py
@@ -5,15 +5,15 @@ import pyrival.algebra
 
 def test_pollard_rho():
     for _ in range(1000):
-        n = random.randint(2, 1000)
+        n = random.randint(2, 10000)
         assert n % pyrival.algebra.pollard_rho(n) == 0
 
 
-def test_prime_factors(primes):
+def test_prime_factors(prime_set):
     for _ in range(1000):
-        n = random.randint(1, 1000)
+        n = random.randint(1, 10000)
         for f, e in pyrival.algebra.prime_factors(n).items():
-            assert f in primes
+            assert f in prime_set
             assert n % (f**e) == 0
             n //= f**e
         assert n == 1
@@ -21,7 +21,7 @@ def test_prime_factors(primes):
 
 def test_distinct_factors():
     for _ in range(1000):
-        n = random.randint(1, 1000)
+        n = random.randint(1, 10000)
         factors = set(pyrival.algebra.distinct_factors(n))
         for i in range(1, n + 1):
             assert (n % i == 0) == (i in factors)
@@ -29,7 +29,7 @@ def test_distinct_factors():
 
 def test_all_factors():
     for _ in range(1000):
-        n = random.randint(1, 1000)
+        n = random.randint(1, 10000)
         factors = pyrival.algebra.all_factors(n)
         assert factors == sorted(factors)
         factors = set(factors)
