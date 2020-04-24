705e76e76faec1a9ec9bd0705ff3af62b1c31928
Mukundan Senthil <mukundan314@gmail.com>
2020-04-24 18:11:59 +0530

    Parametrize RangeQuery tests

--- tests/data_structures/test_RangeQuery.py
+++ tests/data_structures/test_RangeQuery.py
@@ -2,40 +2,29 @@ import random
 
 import pyrival.data_structures
 
+import pytest
 
-def test_RangeQuery_min():
+params = [
+    (float('inf'), min),
+    (float('-inf'), max),
+]
+
+
+@pytest.mark.parametrize("default,func", params)
+def test_RangeQuery(default, func):
     for _ in range(1000):
         l = random.randint(2, 100)
 
         arr = [random.randint(-1000, 1000) for _ in range(l)]
-        rangeQuery = pyrival.data_structures.RangeQuery(arr, min)
+        range_query = pyrival.data_structures.RangeQuery(arr, func)
 
         q = random.randint(0, 100)
         for _ in range(q):
             begin = random.randrange(0, l - 1)
             end = random.randrange(begin + 1, l)
 
-            m = float('inf')
+            m = default
             for i in range(begin, end):
-                m = min(m, arr[i])
+                m = func(m, arr[i])
 
-            assert rangeQuery.query(begin, end) == m
-
-
-def test_RangeQuery_max():
-    for _ in range(1000):
-        l = random.randint(2, 100)
-
-        arr = [random.randint(-1000, 1000) for _ in range(l)]
-        rangeQuery = pyrival.data_structures.RangeQuery(arr, max)
-
-        q = random.randint(0, 100)
-        for _ in range(q):
-            begin = random.randrange(0, l - 1)
-            end = random.randrange(begin + 1, l)
-
-            m = float('-inf')
-            for i in range(begin, end):
-                m = max(m, arr[i])
-
-            assert rangeQuery.query(begin, end) == m
+            assert range_query.query(begin, end) == m
