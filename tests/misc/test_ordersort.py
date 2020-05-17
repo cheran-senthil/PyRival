e4731016501a47e60425f78b2aa9f8003a99f980
Mukundan Senthil <mukundan314@gmail.com>
2020-05-17 14:21:11 +0530

    Update tests for ordersort

--- tests/misc/test_ordersort.py
+++ tests/misc/test_ordersort.py
@@ -7,25 +7,21 @@ def test_ordersort():
     for _ in range(10000):
         l = random.randint(1, 100)
 
-        r = list(range(l))
-        arr = [random.randint(-1000, 1000) for _ in range(l)]
+        arr = [random.randint(0, 1000) for _ in range(l)]
 
-        got = pyrival.misc.ordersort(r, key=arr.__getitem__)
-        expected = sorted(r, key=arr.__getitem__)
+        got = pyrival.misc.ordersort(range(l), arr)
+        expected = sorted(range(l), key=arr.__getitem__)
 
-        for i, j in zip(got, expected):
-            assert arr[i] == arr[j]
+        assert [arr[i] for i in got] == [arr[i] for i in expected]
 
 
 def test_ordersort_reverse():
     for _ in range(10000):
         l = random.randint(1, 100)
 
-        r = list(range(l))
-        arr = [random.randint(-1000, 1000) for _ in range(l)]
+        arr = [random.randint(0, 1000) for _ in range(l)]
 
-        got = pyrival.misc.ordersort(r, key=arr.__getitem__, reverse=True)
-        expected = sorted(r, key=arr.__getitem__, reverse=True)
+        got = pyrival.misc.ordersort(range(l), arr, reverse=True)
+        expected = sorted(range(l), key=arr.__getitem__, reverse=True)
 
-        for i, j in zip(got, expected):
-            assert arr[i] == arr[j]
+        assert [arr[i] for i in got] == [arr[i] for i in expected]
