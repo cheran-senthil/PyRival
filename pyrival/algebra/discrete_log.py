993cdd7aff92f1e55edb149446c781db32c8aa1f
bjorn-martinsson <50366338+bjorn-martinsson@users.noreply.github.com>
2020-06-01 15:47:05 +0200

    Renamed variables and removed unnecessary code

--- pyrival/algebra/discrete_log.py
+++ pyrival/algebra/discrete_log.py
@@ -1,21 +1,22 @@
 def discrete_log(a, b, mod):
-    """returns the smallest x >= 0 s.t. pow(a, x, mod) == b or None if no such x exists"""
+    """
+    Returns smallest x >= 0 s.t. pow(a, x, mod) == b or None if no such x exists. 
+    Note: works even if a and mod are not coprime.
+    """
     n = int(mod**0.5) + 1
 
-    # small_step[x] = maximum j <= n s.t. b * a^j % mod = x
-    small_step, e = {}, 1
+    # tiny_step[x] = maximum j <= n s.t. b * a^j % mod = x
+    tiny_step, e = {}, 1
     for j in range(n + 1):
         if e == b:
             return j
-        small_step[b * e % mod] = j
+        tiny_step[b * e % mod] = j
         e = e * a % mod
 
     # find (i, j) s.t. a^(n * i) % mod = b * a^j % mod
     e = factor = pow(a, n, mod)
     for i in range(2, n + 1):
         e = e * factor % mod
-        j = small_step.get(e, None)
-        if j is not None:
+        if e in tiny_step:
+            j = tiny_step[e]
             return n * i - j if pow(a, n * i - j, mod) == b else None
-
-    return None
