904ef44b58b50cf02498f03e69f88feb9c0b9f70
bjorn-martinsson <50366338+bjorn-martinsson@users.noreply.github.com>
2020-06-01 15:04:12 +0200

    Update discrete_log.py
    Fixed an erroneous comment

--- pyrival/algebra/discrete_log.py
+++ pyrival/algebra/discrete_log.py
@@ -1,21 +1,21 @@
 def discrete_log(a, b, mod):
     """returns the smallest x >= 0 s.t. pow(a, x, mod) == b or None if no such x exists"""
-    m = int(mod**0.5) + 1
+    n = int(mod**0.5) + 1
 
-    # small_step[x] = minimum j <= m s.t. b * a^j % mod = x
+    # small_step[x] = maximum j <= n s.t. b * a^j % mod = x
     small_step, e = {}, 1
-    for j in range(m + 1):
+    for j in range(n + 1):
         if e == b:
             return j
         small_step[b * e % mod] = j
         e = e * a % mod
 
-    # find (i, j) s.t. a^(m * i) % mod = b * a^j % mod
-    e = factor = pow(a, m, mod)
-    for i in range(2, m + 1):
+    # find (i, j) s.t. a^(n * i) % mod = b * a^j % mod
+    e = factor = pow(a, n, mod)
+    for i in range(2, n + 1):
         e = e * factor % mod
         j = small_step.get(e, None)
         if j is not None:
-            return m * i - j if pow(a, m * i - j, mod) == b else None
+            return n * i - j if pow(a, n * i - j, mod) == b else None
 
     return None
