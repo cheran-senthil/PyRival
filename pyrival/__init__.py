da4a60b3de85fc9151b182e87712dbe723670a6d
Bjorn <pajenegod@gmail.com>
2020-06-13 15:51:25 +0200

    Tiny change, hid one variable

--- pyrival/__init__.py
+++ pyrival/__init__.py
@@ -1,6 +1,6 @@
 from .version import version as __version__
 
 import os as _os
-for s in ('algebra', 'combinatorics', 'data_structures', 'geometry', 
+for _s in ('algebra', 'combinatorics', 'data_structures', 'geometry', 
           'graphs', 'linear_algebra', 'numerical', 'strings', 'misc', 'tools'):
-    __path__.append(_os.path.join(_os.path.dirname(__file__), s))
+    __path__.append(_os.path.join(_os.path.dirname(__file__), _s))
