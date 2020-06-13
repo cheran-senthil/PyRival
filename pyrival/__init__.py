af5a66c17966e8a2b6ccc741de9f2b1fea2f0222
Bjorn <pajenegod@gmail.com>
2020-06-13 04:21:41 +0200

    Trying to fix failing tests

--- pyrival/__init__.py
+++ pyrival/__init__.py
@@ -3,4 +3,4 @@ from .version import version as __version__
 import os as _os
 for s in ('algebra', 'combinatorics', 'data_structures', 'geometry', 
           'graphs', 'linear_algebra', 'numerical', 'strings', 'misc', 'tools'):
-    __path__.append(_os.path.join(__path__[0], s))
+    __path__.append(_os.path.join(_os.path.dirname(__file__), s))
