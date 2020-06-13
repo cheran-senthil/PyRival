6f1e0ebb688b1dfa9df61295b314de1fde8c487f
Bjorn <pajenegod@gmail.com>
2020-06-13 15:36:43 +0200

    Fixed failing tests

--- tests/combinatorics/test_combinatorics.py
+++ tests/combinatorics/test_combinatorics.py
@@ -1,12 +1,10 @@
 from pyrival.combinatorics import combinatorics
 
 def test_catalan_recursive(catalan):
-    assert all(combinatorics.catalan_recursive(i) == j for i, j in enumerate(A))
-
+    assert all(combinatorics.catalan_recursive(i) == j for i, j in enumerate(catalan))
 
 def test_derangements(derangements):
-    assert all(combinatorics.derangements(i) == j for i, j in enumerate(A))
-
+    assert all(combinatorics.derangements(i) == j for i, j in enumerate(derangements))
 
 def test_catalan(catalan):
-    assert all(combinatorics.catalan(i) == j for i, j in enumerate(A))
+    assert all(combinatorics.catalan(i) == j for i, j in enumerate(catalan))
