eef3b7e2259f650faa76a79e9648d87f4b2b16e0
Bjorn <pajenegod@gmail.com>
2020-06-16 03:01:25 +0200

    Added template for modulo calculations

---
+++ pyrival/algebra/mod_template.py
@@ -0,0 +1,81 @@
+"""
+This template is useful for problems involving a prime modulo. 
+The template contains a fast function for calculating a * b % MOD, 
+as well as precalculating factorial, inverse factorial, modular inverse
+for all numbers <= maxN in O(maxN) time.
+
+With this template, one can for example quickly calculate n choose k mod MOD,
+or calculate matrix multiplication mod MOD.
+"""
+
+def fast_modder(MOD):
+    """ Returns function modmul(a,b) that quickly calculates a * b % MOD, assuming 0 <= a,b < MOD """
+    import sys, platform
+    impl = platform.python_implementation()
+    maxs = sys.maxsize
+    if 'PyPy' in impl and MOD <= maxs and MOD ** 2 > maxs:
+        import __pypy__
+        intsub = __pypy__.intop.int_sub
+        intmul = __pypy__.intop.int_mul
+        intmulmod = __pypy__.intop.int_mulmod
+        if MOD < 2**30:
+            MODINV = 1.0 / MOD
+            def modmul(a, b):
+                x = intsub(intmul(a,b), intmul(MOD, int(MODINV * a * b)))
+                return x - MOD if x >= MOD else (x if x >= 0 else x + MOD)
+        else:
+            def modmul(a, b):
+                return intmulmod(a, b, MOD)
+    else:
+        def modmul(a, b):
+            return a * b % MOD
+    return modmul
+
+
+""" Precalculate factorial, modular inverse of factorial and modular inverse """
+MOD = 10 ** 9 + 7 # needs to be prime!
+maxN = 10 ** 6
+modmul = fast_modder(MOD)
+
+def mod_precalc():
+    """ Calculates fac, inv_fac and mod_inv for i <= maxN in O(maxN) time """
+    assert maxN <= MOD
+    
+    fac = [1] * maxN
+    for i in range(2, maxN):
+        fac[i] = modmul(fac[i - 1], i)
+
+    inv_fac = [pow(fac[-1], MOD - 2, MOD)] * maxN
+    for i in reversed(range(1, maxN)):
+        inv_fac[i - 1] = modmul(inv_fac[i], i)
+
+    inv_mod = [modmul(fac[i], inv_fac[i - 1]) for i in range(maxN)]
+
+    return fac, inv_fac, inv_mod
+
+fac, inv_fac, inv_mod = mod_precalc()
+
+""" Useful functions involving modulo """
+
+def choose(n,k):
+    """ Calculate n choose k in O(1) time """
+    if k < 0 or k > n:
+        return 0
+    return modmul(modmul(fac[n], invfac[k]), invfac[n-k])
+
+def redu(x):
+    return x if x < MOD else x - MOD
+
+def matrix_modmul(A, B):
+    """ Multiplies matrices A and B, assuming 0 <= A[i][j], B[i][j] < MOD """
+    assert len(A[0]) == len(B)
+    C = []
+    for Ai in A:
+        tmp = [0] * len(B[0])
+        for k in range(len(B)):
+            Aik = Ai[k]
+            Bk = B[k]
+            for j in range(len(Bk)):
+                tmp[j] = redu(tmp[j], modmul(Aik, Bk[j]))
+        C.append(tmp)
+    return C
