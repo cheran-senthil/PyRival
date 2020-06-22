ba8764126d26380aec2f32db5b413530c8ff3225
Mukundan Senthil <mukundan314@gmail.com>
2020-06-22 10:27:26 +0530

    Improve interactive_runner.py

--- pyrival/tools/interactive_runner.py
+++ pyrival/tools/interactive_runner.py
@@ -1,85 +1,101 @@
 import argparse
 import asyncio
+import io
 import pathlib
 import shlex
 import sys
 from asyncio.subprocess import PIPE
 
 
-async def tee(stream, streams, prefix):
-    line = await stream.readline()
-    while line.endswith(b'\n'):
-        for s, p in zip(streams, prefix):
-            s.write(p + line)
-            if hasattr(s, 'flush'):
-                s.flush()
-        line = await stream.readline()
+class PrefixedStream(io.IOBase):
+    def __init__(self, stream, prefix):
+        self.stream = stream
+        self.prefix = prefix
+        self.flush = stream.flush
+
+    def close(self):
+        pass
+
+    def write(self, b):
+        return self.stream.write(self.prefix + b)
+
+
+async def tee(in_stream, out_streams):
+    line = await in_stream.readline()
+    while line.endswith(b"\n"):
+        for out_stream in out_streams:
+            out_stream.write(line)
+            if hasattr(out_stream, "flush"):
+                out_stream.flush()
+        line = await in_stream.readline()
 
     if line:
-        for s, p in zip(streams, prefix):
-            s.write(p + line + b" % No new line\n")
-            if hasattr(s, 'flush'):
-                s.flush()
+        for out_stream in out_streams:
+            out_stream.write(line)
+            if hasattr(out_stream, "flush"):
+                out_stream.flush()
+
+    for out_stream in out_streams:
+        out_stream.close()
 
 
 async def show_exit_code(process, prefix):
-    print(prefix, await process.wait(), sep='')
+    print(prefix, await process.wait(), sep="")
 
 
-async def async_main(argv=sys.argv):
-    parser = argparse.ArgumentParser(pathlib.Path(argv[0]).name)
-    parser.add_argument("program1", help="Command to execute first program")
-    parser.add_argument("program2", help="Command to execute second program")
-    parser.add_argument('--disable-stdout', default=False, action="store_true", help="Do not show stdout")
-    parser.add_argument("--program1-stdout-prefix",
-                        metavar="PREFIX",
-                        default="Program 1 (stdout): ",
-                        help="Prefix to add before the first program's stdout")
-    parser.add_argument("--program1-stderr-prefix",
-                        metavar="PREFIX",
-                        default="Program 1 (stderr): ",
-                        help="Prefix to add before the first program's stderr")
-    parser.add_argument("--program2-stdout-prefix",
-                        metavar="PREFIX",
-                        default="Program 2 (stdout): ",
-                        help="Prefix to add before the second program's stdout")
-    parser.add_argument("--program2-stderr-prefix",
-                        metavar="PREFIX",
-                        default="Program 2 (stderr): ",
-                        help="Prefix to add before the second program's stderr")
+async def async_main(argv=None):
+    argv = sys.argv if argv is None else argv
+
+    parser = argparse.ArgumentParser(pathlib.Path(argv[0]).name, description="A interactive runner")
+    parser.add_argument("command_1", type=str, help="command to execute first program")
+    parser.add_argument("command_2", type=str, help="Command to execute second program")
+    parser.add_argument(
+        "--command1-stdout-prefix",
+        metavar="PREFIX",
+        default="Command 1 (stdout): ",
+        help="Prefix to add before the first command's stdout",
+    )
+    parser.add_argument(
+        "--command1-stderr-prefix",
+        metavar="PREFIX",
+        default="Command 1 (stderr): ",
+        help="Prefix to add before the first command's stderr",
+    )
+    parser.add_argument(
+        "--command2-stdout-prefix",
+        metavar="PREFIX",
+        default="Command 2 (stdout): ",
+        help="Prefix to add before the second command's stdout",
+    )
+    parser.add_argument(
+        "--command2-stderr-prefix",
+        metavar="PREFIX",
+        default="Command 2 (stderr): ",
+        help="Prefix to add before the second command's stderr",
+    )
 
     args = parser.parse_args(argv[1:])
 
-    process_1 = await asyncio.create_subprocess_exec(*shlex.split(args.program1), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    process_2 = await asyncio.create_subprocess_exec(*shlex.split(args.program2), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    proc_1 = await asyncio.create_subprocess_exec(*shlex.split(args.command_1), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    proc_2 = await asyncio.create_subprocess_exec(*shlex.split(args.command_2), stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-    program1_stdout_prefix = args.program1_stdout_prefix.encode("utf-8")
-    program1_stderr_prefix = args.program1_stderr_prefix.encode("utf-8")
-    program2_stdout_prefix = args.program2_stdout_prefix.encode("utf-8")
-    program2_stderr_prefix = args.program2_stderr_prefix.encode("utf-8")
-
-    process_1_stdout_tee = [process_2.stdin]
-    process_1_stdout_tee_prefixes = [b""]
-    process_2_stdout_tee = [process_1.stdin]
-    process_2_stdout_tee_prefixes = [b""]
-
-    if not args.disable_stdout:
-        process_1_stdout_tee.append(sys.stdout.buffer)
-        process_1_stdout_tee_prefixes.append(program1_stdout_prefix)
-        process_2_stdout_tee.append(sys.stdout.buffer)
-        process_2_stdout_tee_prefixes.append(program2_stdout_prefix)
+    proc_1_stdout = PrefixedStream(sys.stdout.buffer, args.command1_stdout_prefix.encode("utf-8"))
+    proc_2_stdout = PrefixedStream(sys.stdout.buffer, args.command2_stdout_prefix.encode("utf-8"))
+    proc_1_stderr = PrefixedStream(sys.stderr.buffer, args.command1_stderr_prefix.encode("utf-8"))
+    proc_2_stderr = PrefixedStream(sys.stderr.buffer, args.command2_stderr_prefix.encode("utf-8"))
 
     await asyncio.gather(
-        tee(process_1.stdout, process_1_stdout_tee, process_1_stdout_tee_prefixes),
-        tee(process_2.stdout, process_2_stdout_tee, process_2_stdout_tee_prefixes),
-        tee(process_1.stderr, [sys.stdout.buffer], [program1_stderr_prefix]),
-        tee(process_2.stderr, [sys.stdout.buffer], [program2_stderr_prefix]),
-        show_exit_code(process_1, "Program 1 Exited with Code: "),
-        show_exit_code(process_2, "Program 2 Exited with Code: "),
+        tee(proc_1.stdout, [proc_2.stdin, proc_1_stdout]),
+        tee(proc_2.stdout, [proc_1.stdin, proc_2_stdout]),
+        tee(proc_1.stderr, [proc_1_stderr]),
+        tee(proc_2.stderr, [proc_2_stderr]),
+        show_exit_code(proc_1, "Program 1 Exited with Code: "),
+        show_exit_code(proc_2, "Program 2 Exited with Code: "),
     )
 
 
-def main(argv=sys.argv):
+def main(argv=None):
+    argv = sys.argv if argv is None else argv
     asyncio.run(async_main(argv))
 
 
