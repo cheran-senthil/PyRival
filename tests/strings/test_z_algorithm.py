ac0b9eb29c4d6c4f136bfbc5717ca7c6cc6b4234
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-17 04:16:37 +0600

    Add tests for TLE cases

--- tests/strings/test_z_algorithm.py
+++ tests/strings/test_z_algorithm.py
@@ -1,13 +1,15 @@
-import string
+from random import choice
 from itertools import product
+from string import ascii_lowercase
 
 from pyrival.strings.z_algorithm import z_function
 
 
-def brute_force(text):
-    z_array = [0] * len(text)
+def brute_force(text, limit=2**30):
+    k = min(limit, len(text))
+    z_array = [0] * k
 
-    for i in range(len(text)):
+    for i in range(k):
         j, k = 0, i
         while k < len(text) and text[k] == text[j]:
             j, k = j + 1, k + 1
@@ -21,8 +23,16 @@ def generate_words(char_set, length):
         yield ''.join(c for c in word)
 
 
-def test_z_algorithm():
+def test_z_algorithm__corner_cases():
     for c in range(1, 4):
         for length in range(1, 11):
-            for word in generate_words(string.ascii_lowercase[:c], length):
+            for word in generate_words(ascii_lowercase[:c], length):
                 assert brute_force(word) == z_function(word)
+
+
+def test_z_algorithm__large_cases():
+    for c in range(1, 10):
+        length = 10**5
+        text = ''.join(choice(ascii_lowercase[:c]) for _ in range(length))
+        z_array = z_function(text)
+        assert z_array[:100] == brute_force(text, 100)
