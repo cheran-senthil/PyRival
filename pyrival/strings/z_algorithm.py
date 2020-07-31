7e38c375ff5c19cef0ce07f13419ff36d2f84c8b
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-31 15:24:21 +0600

    Improve z algorithm implementation

--- pyrival/strings/z_algorithm.py
+++ pyrival/strings/z_algorithm.py
@@ -7,18 +7,20 @@ def z_function(text):
     """
 
     n = len(text)
-    z_array = [0] * n
-    l, r = 0, 0
+    z_array = [0 for _ in range(n)]
+    l = r = 0
 
     for i in range(1, n):
-        z = max(0, min(z_array[i - l], r - i))
-        while i + z < n and text[z] == text[i + z]:
-            z += 1
+        z = z_array[i - l]
+        if i + z >= r:
+            z = max(r - i, 0)
+            while i + z < n and text[z] == text[i + z]:
+                z += 1
 
-        if i + z > r:
             l, r = i, i + z
 
         z_array[i] = z
 
     z_array[0] = n
     return z_array
+
