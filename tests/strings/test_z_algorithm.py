5b23292ab700a872e4c1a399830938863cdaedab
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-31 16:08:47 +0600

    Improve tests for z algorithm

--- tests/strings/test_z_algorithm.py
+++ tests/strings/test_z_algorithm.py
@@ -1,21 +1,18 @@
-from random import choice
 from itertools import product
+from random import choice, randint
 from string import ascii_lowercase
 
 from pyrival.strings.z_algorithm import z_function
 
 
-def brute_force(text, limit=2**30):
-    k = min(limit, len(text))
-    z_array = [0] * k
+def get_lcp(text, i):
+    n = len(text)
 
-    for i in range(k):
-        j, k = 0, i
-        while k < len(text) and text[k] == text[j]:
-            j, k = j + 1, k + 1
-            z_array[i] += 1
+    z = 0
+    while i + z < n and text[i + z] == text[z]:
+        z += 1
 
-    return z_array
+    return z
 
 
 def generate_words(char_set, length):
@@ -25,14 +22,26 @@ def generate_words(char_set, length):
 
 def test_z_algorithm__corner_cases():
     for c in range(1, 4):
-        for length in range(1, 11):
-            for word in generate_words(ascii_lowercase[:c], length):
-                assert brute_force(word) == z_function(word)
+        for n in range(1, 11):
+            for text in generate_words(ascii_lowercase[:c], n):
+                assert z_function(text) == [get_lcp(text, i) for i in range(n)]
 
 
 def test_z_algorithm__large_cases():
-    for c in range(1, 10):
-        length = 10**5
-        text = ''.join(choice(ascii_lowercase[:c]) for _ in range(length))
+    for c in range(1, 4):
+        n = 10**5
+        text = ''.join(choice(ascii_lowercase[:c]) for _ in range(n))
+
         z_array = z_function(text)
-        assert z_array[:100] == brute_force(text, 100)
+        for _ in range(100):
+            i = randint(0, n - 1)
+            assert z_array[i] == get_lcp(text, i)
+
+    n = 10**6
+    f = lambda x: 'a' if bin(x).count('1') % 2 == 0 else 'b'
+    text = ''.join(f(i) for i in range(n))
+
+    z_array = z_function(text)
+    for _ in range(100):
+        i = randint(0, n - 1)
+        assert z_array[i] == get_lcp(text, i)
