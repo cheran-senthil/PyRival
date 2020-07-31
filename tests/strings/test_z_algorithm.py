e2c39daa49fb27041a190bbfad7ef6748746968c
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-31 16:09:58 +0600

    Make helper methods private and reorder methods

--- tests/strings/test_z_algorithm.py
+++ tests/strings/test_z_algorithm.py
@@ -5,26 +5,11 @@ from string import ascii_lowercase
 from pyrival.strings.z_algorithm import z_function
 
 
-def get_lcp(text, i):
-    n = len(text)
-
-    z = 0
-    while i + z < n and text[i + z] == text[z]:
-        z += 1
-
-    return z
-
-
-def generate_words(char_set, length):
-    for word in product(char_set, repeat=length):
-        yield ''.join(c for c in word)
-
-
 def test_z_algorithm__corner_cases():
     for c in range(1, 4):
         for n in range(1, 11):
-            for text in generate_words(ascii_lowercase[:c], n):
-                assert z_function(text) == [get_lcp(text, i) for i in range(n)]
+            for text in _generate_words(ascii_lowercase[:c], n):
+                assert z_function(text) == [_get_lcp(text, i) for i in range(n)]
 
 
 def test_z_algorithm__large_cases():
@@ -35,7 +20,7 @@ def test_z_algorithm__large_cases():
         z_array = z_function(text)
         for _ in range(100):
             i = randint(0, n - 1)
-            assert z_array[i] == get_lcp(text, i)
+            assert z_array[i] == _get_lcp(text, i)
 
     n = 10**6
     f = lambda x: 'a' if bin(x).count('1') % 2 == 0 else 'b'
@@ -44,4 +29,19 @@ def test_z_algorithm__large_cases():
     z_array = z_function(text)
     for _ in range(100):
         i = randint(0, n - 1)
-        assert z_array[i] == get_lcp(text, i)
+        assert z_array[i] == _get_lcp(text, i)
+
+
+def _get_lcp(text, i):
+    n = len(text)
+
+    z = 0
+    while i + z < n and text[i + z] == text[z]:
+        z += 1
+
+    return z
+
+
+def _generate_words(char_set, length):
+    for word in product(char_set, repeat=length):
+        yield ''.join(c for c in word)
