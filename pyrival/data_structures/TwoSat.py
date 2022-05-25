18ea8986a9ad2c7663b58e105de27df1035c9764
bjorn-martinsson <pajenegod@gmail.com>
2022-05-25 18:33:52 +0200

    Fixed faulty SCC implementation in 2Sat

--- pyrival/data_structures/TwoSat.py
+++ pyrival/data_structures/TwoSat.py
@@ -1,86 +1,52 @@
-def scc(graph):
-    """
-    Finds what strongly connected components each node
-    is a part of in a directed graph,
-    it also finds a weak topological ordering of the nodes
-    """
-    n = len(graph)
-    comp = [-1] * n
-    top_order = []
-
-    Q = []
-    stack = []
-    new_node = None
-    for root in range(n):
-        if comp[root] >= 0:
-            continue
-
-        # Do a dfs while keeping track of depth
-        Q.append(root)
-        root_depth = len(top_order)
-        while Q:
-            node = Q.pop()
-            if node >= 0:
-                if comp[node] >= 0:
-                    continue
-                # First time
-
-                # Index the node
-                comp[node] = len(top_order) + len(stack)
-                stack.append(node)
-
-                # Do a dfs
-                Q.append(~node)
-                Q += graph[node]
-            else:
-                # Second time
-                node = ~node
-
-                # calc low link
-                low = index = comp[node]
-                for nei in graph[node]:
-                    if root_depth <= comp[nei]:
-                        low = min(low, comp[nei])
-
-                # low link same as index, so create SCC
-                if low == index:
-                    while new_node != node:
-                        new_node = stack.pop()
-                        comp[new_node] = index
-                        top_order.append(new_node)
-                else:
-                    comp[node] = low
-
-    top_order.reverse()
-    return comp, top_order
-
-
+def find_SCC(graph):
+    SCC, S, P = [], [], []
+    depth = [0] * len(graph)
+ 
+    stack = list(range(len(graph)))
+    while stack:
+        node = stack.pop()
+        if node < 0:
+            d = depth[~node] - 1
+            if P[-1] > d:
+                SCC.append(S[d:])
+                del S[d:], P[-1]
+                for node in SCC[-1]:
+                    depth[node] = -1
+        elif depth[node] > 0:
+            while P[-1] > depth[node]:
+                P.pop()
+        elif depth[node] == 0:
+            S.append(node)
+            P.append(len(S))
+            depth[node] = len(S)
+            stack.append(~node)
+            stack += graph[node]
+    return SCC[::-1]
+ 
 class TwoSat:
     def __init__(self, n):
         self.n = n
         self.graph = [[] for _ in range(2 * n)]
-
+ 
     def _imply(self, x, y):
         self.graph[x].append(y if y >= 0 else 2 * self.n + y)
-
+ 
     def either(self, x, y):
         """either x or y must be True"""
         self._imply(~x, y)
         self._imply(~y, x)
-
+ 
     def set(self, x):
         """x must be True"""
         self._imply(~x, x)
-
+ 
     def solve(self):
-        comp, top_order = scc(self.graph)
-        for x in range(self.n):
-            if comp[x] == comp[~x]:
+        SCC = find_SCC(self.graph)
+        order = [0] * (2 * self.n)
+        for i, comp in enumerate(SCC):
+            for x in comp:
+                order[x] = i
+        for i in range(self.n):
+            if order[i] == order[~i]:
                 return False, None
-
-        self.values = [None] * self.n
-        for x in reversed(top_order):
-            y = x if x < self.n else (2 * self.n - 1 - x)
-            if self.values[y] is None:
-                self.values[y] = x < self.n
-        return True, self.values
+        return True, [+(order[i] > order[~i]) for i in range(self.n)]
