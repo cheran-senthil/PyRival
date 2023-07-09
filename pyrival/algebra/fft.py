dc477c8477624db9909f4f385768ed0697876174
bjorn-martinsson <pajenegod@gmail.com>
2023-07-09 21:44:07 +0200

    New fft and ntt

--- pyrival/algebra/fft.py
+++ pyrival/algebra/fft.py
@@ -1,46 +1,42 @@
-import cmath
+# FFT implementation based on https://codeforces.com/blog/entry/117947
 
-MOD = 10**9 + 7
+rt = [1]
+def fft(P):
+    n = len(P)
+    P = list(P)
+    assert n and (n - 1) & n == 0
+    
+    while 2 * len(rt) < n:
+        import cmath
+        root = cmath.exp(2j * cmath.pi / (4 * len(rt)))
+        rt.extend([r * root for r in rt])
 
-
-def fft(a, inv=False):
-    n = len(a)
-    w = [cmath.rect(1, (-2 if inv else 2) * cmath.pi * i / n) for i in range(n >> 1)]
+    k = n
+    while k > 1:
+        for i in range(0, n, k):
+            r = rt[i//k]
+            for j1 in range(i, i + k//2):
+                j2 = j1 + k//2
+                z = r * P[j2]
+                P[j2] = P[j1] - z
+                P[j1] = P[j1] + z
+        k //= 2
+    
     rev = [0] * n
-    for i in range(n):
-        rev[i] = rev[i >> 1] >> 1
-        if i & 1:
-            rev[i] |= n >> 1
-        if i < rev[i]:
-            a[i], a[rev[i]] = a[rev[i]], a[i]
+    for i in range(1, n):
+        rev[i] = rev[i // 2] // 2 + (i & 1) * n // 2
+    return [P[r] for r in rev]
 
-    step = 2
-    while step <= n:
-        half, diff = step >> 1, n // step
-        for i in range(0, n, step):
-            pw = 0
-            for j in range(i, i + half):
-                v = a[j + half] * w[pw]
-                a[j + half] = a[j] - v
-                a[j] += v
-                pw += diff
-        step <<= 1
+def ifft(P):
+    n = len(P)
+    return fft([P[-i]/n for i in range(n)])
 
-    if inv:
-        for i in range(n):
-            a[i] /= n
+def fft_conv(P, Q):
+    m = len(P) + len(Q) - 1
+    n = 1 << m.bit_length()
 
+    P = P + [0] * (n - len(P))
+    Q = Q + [0] * (n - len(Q))
+    P, Q = fft(P), fft(Q)
 
-def fft_conv(a, b):
-    s = len(a) + len(b) - 1
-    n = 1 << s.bit_length()
-    a.extend([0.0] * (n - len(a)))
-    b.extend([0.0] * (n - len(b)))
-
-    fft(a), fft(b)
-    for i in range(n):
-        a[i] *= b[i]
-    fft(a, True)
-
-    a = [a[i].real for i in range(s)]
-    return a
+    return ifft([p*q for p,q in zip(P, Q)])[:m]
