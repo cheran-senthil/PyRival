8e98ac0ed1393544a07d537c3b6c0dc7a230bbe9
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2023-12-14 20:49:50 +0530

    Modified Code for Xor basis

--- pyrival/linear_algebra/xor_basis.py
+++ pyrival/linear_algebra/xor_basis.py
@@ -2,36 +2,28 @@ class XorBasis:
     " Linear basis for xor "
         
     def __init__(self, bit_length=31):
-        self.bit_length=bit_length
-        self.basis_size=0
-        self.val = [0]*self.bit_length
+        self.basis = []
+        self.bit_length = bit_length
     
     def is_redundant(self, x):
         " Returns true if x can be represented as xor of some already inserted elements"
         
-        for i in range(self.bit_length):
-            if (x>>i)&1:
-                if self.val[i]==0:
-                    return False
-                x^=self.val[i]
-        return True
+        return self._reduce(x)==0
     
     def insert(self, x):
         "Adds x to the basis if it is not redundant. Returns true if x is added to the basis"
-        for i in range(self.bit_length):
-            if (x>>i)&1:
-                if self.val[i]==0:
-                    self.val[i]=x
-                    self.basis_size+=1
-                    return True
-                x^=self.val[i]
-        return False
+        x = self._reduce(x)
+        if x:
+            self.basis.append(x)
+        return x!=0
+    
+    def _reduce(self, x):
+        for b in self.basis:
+            x = min(x, x ^ b)
+        return x
     
     def __len__(self):
-        return self.basis_size
-    
-    def __getitem__(self, i):
-        return self.val[i]
+        return len(self.basis)
     
     def __contains__(self, x):
         return self.is_redundant(x)
@@ -39,7 +31,6 @@ class XorBasis:
     def __repr__(self):
         " Prints the basis in descending order "
         print("Basis:")
-        for i in range(self.bit_length-1, -1, -1):
-            if self.val[i]!=0:
-                print(bin(self.val[i])[2:].zfill(self.bit_length))
+        for b in self.basis:
+            print(bin(b)[2:].zfill(self.bit_length))
         return ""
