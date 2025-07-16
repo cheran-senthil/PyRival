d7086f116dfeb5ca8be7c6b2d91af1071f68e807
bjorn-martinsson <pajenegod@gmail.com>
2025-07-16 16:17:34 +0200

    Fix another typo

--- pyrival/algebra/multiplicative_function.py
+++ pyrival/algebra/multiplicative_function.py
@@ -13,7 +13,7 @@ def compute_multiplicative_function(g, n, u=-2):
         if DP[p] == u: # if p prime
             k = 1
             upper = (n - 1)//p 
-            while upper: # Overflow-safe check for (big-1)//p**k>0
+            while upper: # Overflow-safe check for (n - 1)//p**k>0
                 pk = p**k
                 f_pk = g(p, k)
                 for j in range(1, upper + 1):
