85e3f8b2158aa04bd9325dd29e84c8ca503cd9c9
bjorn-martinsson <pajenegod@gmail.com>
2025-07-16 18:09:43 +0200

    Switch to n inclusive

--- pyrival/algebra/multiplicative_function.py
+++ pyrival/algebra/multiplicative_function.py
@@ -1,19 +1,19 @@
-# Computes f(x) for x = 1,...,n-1 in O(n log log n) time, 
+# Computes f(x) for x = 1,...,n in O(n log log n) time, 
 # where f is a multiplicative function.
 # Input:
 #  function g: g(p,k) = f(p**k) where p is prime and k>0
-#  n: size of output
+#  n: How many values to compute
 #  u: Used as uninitialized value. Can be anything expect
-#  that u != f(x) for x = 1,..,n-1.
+#     that u != f(x) for x = 1,..,n.
  
 def compute_multiplicative_function(g, n, u=-2):
-    DP = [u] * n
+    DP = [u] * (n + 1)
     DP[1] = 1
-    for p in range(2, n):
+    for p in range(2, n + 1):
         if DP[p] == u: # if p prime
             k = 1
-            upper = (n - 1)//p 
-            while upper: # Overflow-safe check for (n - 1)//p**k > 0
+            upper = n // p 
+            while upper: # Overflow-safe check for n // p**k > 0
                 pk = p**k
                 f_pk = g(p, k)
                 for j in range(1, upper + 1):
@@ -21,6 +21,7 @@ def compute_multiplicative_function(g, n, u=-2):
                 k += 1
                 upper //= p
     return DP
+
 
 # Examples
 
@@ -40,7 +41,7 @@ mobius = compute_multiplicative_function(mobius_g, n)
 divcount_g = lambda p,k: k + 1
 divcount = compute_multiplicative_function(divcount_g, n)
 # Is prime iff divisor count == 2
-primes = [i for i in range(n) if divcount[i] == 2]
+primes = [i for i in range(n + 1) if divcount[i] == 2]
 
 # Compute sum of all divisors of x
 divsum_g = lambda p,k: (p**(k + 1) - 1) // (p - 1)
