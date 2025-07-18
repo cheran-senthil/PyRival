022050b584aaa7b792b5d47e2f6f85d114960102
bjorn-martinsson <pajenegod@gmail.com>
2025-07-19 01:17:05 +0200

    Remove walrus

--- pyrival/data_structures/RangeMinimumQuery.py
+++ pyrival/data_structures/RangeMinimumQuery.py
@@ -16,7 +16,8 @@ class RangeMinimumQuery:
  
     def query(self, start, stop):
         """min of data[start, stop)"""
-        data = self._data[depth := (stop - start).bit_length() - 1]
+        depth = (stop - start).bit_length() - 1
+        data = self._data[depth]
         return min(data[start], data[stop - (1 << depth)])
 
 
@@ -41,10 +42,10 @@ class RangeMinimumQuery2:
                 mask &= mask - 1
             A[i] = mask = 2 * (mask & ~(-1<<62)) + 1
         self.B = [data[i + (A[i] & ~(-1<<32)).bit_length() - 1] for i in range(n)]
-        self.RMQ = RangeMinimumQuery(self.B[::32])
+        self._query = RangeMinimumQuery(self.B[::32]).query
     
     def query(self, start, stop):
         """min of data[start, stop)"""
         if stop - start < 64:
             return self.data[start + (self.A[start] & ~(-1 << stop - start)).bit_length() - 1]
-        return min(self.B[start], self.B[stop - 32], self.RMQ.query((start + 31) >> 5, stop >> 5))
+        return min(self.B[start], self.B[stop - 32], self._query((start + 31) >> 5, stop >> 5))
