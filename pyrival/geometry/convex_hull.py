72f16bae5acfbd19f6554d63a09bbe6e882c54a7
bjorn-martinsson <50366338+bjorn-martinsson@users.noreply.github.com>
2025-11-17 16:16:41 +0100

    Update convex_hull.py
    Fixed bug

--- pyrival/geometry/convex_hull.py
+++ pyrival/geometry/convex_hull.py
@@ -8,7 +8,7 @@ def convex_hull(P):
     cross = lambda a,b,c: (a[0] - b[0]) * (c[1] - b[1]) - (a[1] - b[1]) * (c[0] - b[0])
     H = []
     for p in P + P[::-1]:
-        while len(H) > 1 and cross(H[-2], H[-1], p) <= 0 and not H[-2] == p != H[-1]:
+        while len(H) > 1 and cross(H[-2], H[-1], p) <= 0 and not H[-2] < H[-1] > p:
             H.pop()
         H.append(p)
     return H[:-1]
