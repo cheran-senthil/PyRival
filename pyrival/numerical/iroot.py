51a24bd869252b8ec6fece9108d86f6a0aa9caa0
Mukundan314 <mukundan314@gmail.com>
2026-03-10 19:48:41 +0530

    Improve initial guess for iroot

--- pyrival/numerical/iroot.py
+++ pyrival/numerical/iroot.py
@@ -3,7 +3,8 @@ def iroot(n, k=2):
         return 0
     if n < 0:
         return -iroot(-n, k) if k & 1 else None
-    u, s = n, n + 1
+    u = 1 << -(-n.bit_length() // k)
+    s = u + 1
     while u < s:
         s = u
         t = (k - 1) * s + n // s**(k - 1)
